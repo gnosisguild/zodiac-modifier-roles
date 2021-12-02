78eef5ee0aa478fb3b441779535434f993d8489f
nginnever <ginneversource@gmail.com>
2021-12-02 12:24:38 -0800

    multisend restructure

--- contracts/Roles.sol
+++ contracts/Roles.sol
@@ -21,6 +21,7 @@ contract Roles is Modifier {
         bool scoped;
         bool delegateCallAllowed;
         bool sendAllowed;
+        bool isMultiSend;
         mapping(bytes4 => Function) functions;
     }
 
@@ -400,6 +401,46 @@ contract Roles is Modifier {
         return (roles[role].targetAddresses[targetAddress].delegateCallAllowed);
     }
 
+    function splitMultiSend(bytes memory transactions) internal returns (address[] memory, uint256[] memory, bytes[] memory, Enum.Operation[] memory) {
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            let length := mload(transactions)
+            let i := 0x20
+            for {
+                // Pre block is not used in "while mode"
+            } lt(i, length) {
+                // Post block is not used in "while mode"
+            } {
+                // First byte of the data is the operation.
+                // We shift by 248 bits (256 - 8 [operation byte]) it right since mload will always load 32 bytes (a word).
+                // This will also zero out unused data.
+                let operation := shr(0xf8, mload(add(transactions, i)))
+                // We offset the load address by 1 byte (operation byte)
+                // We shift it right by 96 bits (256 - 160 [20 address bytes]) to right-align the data and zero out unused data.
+                let to := shr(0x60, mload(add(transactions, add(i, 0x01))))
+                // We offset the load address by 21 byte (operation byte + 20 address bytes)
+                let value := mload(add(transactions, add(i, 0x15)))
+                // We offset the load address by 53 byte (operation byte + 20 address bytes + 32 value bytes)
+                let dataLength := mload(add(transactions, add(i, 0x35)))
+                // We offset the load address by 85 byte (operation byte + 20 address bytes + 32 value bytes + 32 data length bytes)
+                let data := add(transactions, add(i, 0x55))
+                // let success := 0
+                // switch operation
+                //     case 0 {
+                //         success := call(gas(), to, value, data, dataLength, 0, 0)
+                //     }
+                //     case 1 {
+                //         success := delegatecall(gas(), to, data, dataLength, 0, 0)
+                //     }
+                // if eq(success, 0) {
+                //     revert(0, 0)
+                // }
+                // Next entry starts at 85 byte + data length
+                i := add(i, add(0x55, dataLength))
+            }
+        }
+    }
+
     function checkParameters(
         uint16 role,
         address targetAddress,
@@ -485,9 +526,9 @@ contract Roles is Modifier {
         address targetAddress,
         uint256 value,
         bytes memory data,
-        Enum.Operation operation
+        Enum.Operation operation,
+        uint16 role
     ) internal {
-        uint16 role = defaultRoles[msg.sender];
         if (data.length != 0 && data.length < 4) {
             revert FunctionSignatureTooShort();
         }
@@ -497,7 +538,6 @@ contract Roles is Modifier {
         ) {
             revert DelegateCallNotAllowed();
         }
-
         if (!roles[role].targetAddresses[targetAddress].allowed) {
             revert TargetAddressNotAllowed();
         }
@@ -541,7 +581,12 @@ contract Roles is Modifier {
         bytes calldata data,
         Enum.Operation operation
     ) public override moduleOnly returns (bool success) {
-        checkTransaction(to, value, data, operation);
+        uint16 role = defaultRoles[msg.sender];
+        if (roles[role].targetAddresses[to].isMultiSend) {
+            
+        } else {
+            checkTransaction(to, value, data, operation, role);
+        }
         return exec(to, value, data, operation);
     }
 
@@ -562,7 +607,12 @@ contract Roles is Modifier {
         moduleOnly
         returns (bool success, bytes memory returnData)
     {
-        checkTransaction(to, value, data, operation);
+        uint16 role = defaultRoles[msg.sender];
+        if (roles[role].targetAddresses[to].isMultiSend) {
+            
+        } else {
+            checkTransaction(to, value, data, operation, role);
+        }
         return execAndReturnData(to, value, data, operation);
     }
 
