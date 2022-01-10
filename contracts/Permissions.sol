cb12d209519d8a2c9ea48823d8b053802e43563b
Cristóvão <cristovao.honorato@gmail.com>
2022-01-10 01:43:14 +0100

    yarn run fmt:sol

--- contracts/Permissions.sol
+++ contracts/Permissions.sol
@@ -347,7 +347,7 @@ library Permissions {
 
         // set compValue
         key = keyForCompValues(targetAddress, functionSig, paramIndex);
-        
+
         role.compValuesOneOf[key] = new bytes32[](compValues.length);
         for (uint256 i = 0; i < compValues.length; i++) {
             role.compValuesOneOf[key][i] = maybeCompressCompValue(
