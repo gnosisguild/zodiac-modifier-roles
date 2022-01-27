6d36c6de727324ee888ecd2dbc8e30d60df155d4
Colin Spence <udappia@gmail.com>
2022-01-27 10:59:00 -0700

    Styles action buttons and tableRows;

--- packages/app/src/components/RoleList.tsx
+++ packages/app/src/components/RoleList.tsx
@@ -1,14 +1,44 @@
 import React  from "react"
 import { useGetRolesForRolesModifier } from "../hooks/useSubgraph"
-import { Button, Box, Table, TableHead, TableBody, TableRow, TableCell, makeStyles} from "@material-ui/core"
+import { Box, Button, IconButton, Table, TableHead, TableBody, TableRow, TableCell, makeStyles} from "@material-ui/core"
+import KeyboardArrowRightSharp from '@material-ui/icons/KeyboardArrowRightSharp'
+import DeleteOutlineSharp from '@material-ui/icons/DeleteOutlineSharp'
 import clsx from "clsx"
 
 const useStyles = makeStyles((theme) => ({
   root: {
     marginTop: theme.spacing(2),
   },
+  arrowIcon: {
+    color: 'rgba(217, 212, 173, 1)',
+  },
+  iconButton: {
+    borderRadius: 8,
+    color: 'rgba(217, 212, 173, 1)',
+    width: 34,
+    height: 34,
+  },
+  deleteButton: {
+    backgroundColor: 'rgba(217, 212, 173, 0.1)',
+    border: '1px solid rgba(217, 212, 173, 0.3)',
+    '&:hover': {
+      opacity: 0.8,
+    }
+  },
+  deleteIcon: {
+    width: 16,
+  },
   memberCell: {
     width: '50%',
+  },
+  tableButton: {
+    backgroundColor: 'rgba(217, 212, 173, 0.1)',
+    border: '1px solid rgba(217, 212, 173, 0.3)',
+    borderRadius: 8,
+    color: 'rgba(217, 212, 173, 1)',
+    '&:before': {
+      content: 'none',
+    }
   },
   tableCell: {
     borderBottom: '1px solid rgba(217, 212, 173, 0.3)',
@@ -20,6 +50,12 @@ const useStyles = makeStyles((theme) => ({
     fontSize: 14,
     letterSpacing: 1,
     textTransform: 'uppercase',
+  },
+  tableRow: {
+    cursor: 'pointer',
+    '&:hover $tableCell': {
+      backgroundColor: 'rgba(217, 212, 173, 0.02)',
+    }
   },
 }))
 
@@ -49,7 +85,7 @@ const RoleList = ({ modifierAddress }: Props): React.ReactElement => {
         </TableHead>
         <TableBody>
           {roles.map((role) => (
-            <TableRow key={role.id} >
+            <TableRow key={role.id} className={classes.tableRow}>
               <TableCell className={classes.tableCell}>
                 {`Role #${role.id}`}
               </TableCell>
@@ -58,13 +94,19 @@ const RoleList = ({ modifierAddress }: Props): React.ReactElement => {
               </TableCell>
               <TableCell className={classes.tableCell}>
                 <Box display="flex" justifyContent="flex-end">
-                  <Button>
-                    Edit
-                  </Button>
-                  <Button>
-                    Delete
-                  </Button>
-                  <div>^</div>
+                  <Box sx={{mr: 1}}>
+                    <Button aria-label="Edit Role" className={classes.tableButton}>
+                      Edit
+                    </Button>
+                  </Box>
+                  <Box sx={{mr: 1}}>
+                    <IconButton size="small" aria-label="Delete Role" className={clsx(classes.iconButton, classes.deleteButton)}>
+                      <DeleteOutlineSharp className={classes.deleteIcon} />
+                    </IconButton>
+                  </Box>
+                  <IconButton size="small" aria-label="View Role" className={classes.iconButton}>
+                    <KeyboardArrowRightSharp className={classes.arrowIcon} />
+                  </IconButton>
                 </Box>
               </TableCell>
             </TableRow>
