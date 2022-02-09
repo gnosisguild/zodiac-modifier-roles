e4a184ab57725ed41b910020bf73a81a96a78de1
Asgeir <2156509+asgeir-eth@users.noreply.github.com>
2022-02-09 11:26:32 +0000

    App: Formating

--- packages/app/src/utils/address.ts
+++ packages/app/src/utils/address.ts
@@ -3,9 +3,9 @@
  * @param address Full address to truncate
  * @returns Truncated address
  */
-export function truncateEthAddress  (address: string) {
-  const truncateRegex = /^(0x[a-zA-Z0-9]{4})[a-zA-Z0-9]+([a-zA-Z0-9]{4})$/;
-  const match = address.match(truncateRegex);
-  if (!match) return address;
-  return `${match[1]}…${match[2]}`;
+export function truncateEthAddress(address: string) {
+  const truncateRegex = /^(0x[a-zA-Z0-9]{4})[a-zA-Z0-9]+([a-zA-Z0-9]{4})$/
+  const match = address.match(truncateRegex)
+  if (!match) return address
+  return `${match[1]}…${match[2]}`
 }
