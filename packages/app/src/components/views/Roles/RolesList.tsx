8d8b99e9e3d825305bba320610b5589885d70752
Carlos Febres <carlosfebres@dorg.tech>
2022-02-10 06:22:47 -0400

    [app] display roles filter text field

--- packages/app/src/components/views/Roles/RolesList.tsx
+++ packages/app/src/components/views/Roles/RolesList.tsx
@@ -1,25 +1,39 @@
-import { Box, Button } from "@material-ui/core"
-import { Link, useParams } from "react-router-dom"
-import AddIcon from "@material-ui/icons/Add"
-import RoleTable from "../Role/RoleTable"
 import React from "react"
+import { Box, Button, InputAdornment, makeStyles } from "@material-ui/core"
+import { Link, useParams } from "react-router-dom"
+import RoleTable from "../Role/RoleTable"
+import { TextField } from "../../commons/input/TextField"
+
+import AddIcon from "@material-ui/icons/Add"
+import SearchIcon from "@material-ui/icons/Search"
+
+const useStyles = makeStyles((theme) => ({
+  search: {
+    maxWidth: 300,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+}))
 
 export const RolesList = () => {
+  const classes = useStyles()
   const { module } = useParams()
   return (
     <>
-      <Box display="flex" justifyContent="flex-end">
-        {/* <TextField
-            placeholder="Filter by Members"
-            className={classes.search}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <SearchIcon />
-                </InputAdornment>
-              ),
-            }}
-          /> */}
+      <Box display="flex">
+        <TextField
+          placeholder="Filter by Members or Targets"
+          className={classes.search}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <div className={classes.grow} />
         <Link to={`/${module}/roles/new`}>
           <Button startIcon={<AddIcon />} variant="contained" color="secondary">
             Create a role
