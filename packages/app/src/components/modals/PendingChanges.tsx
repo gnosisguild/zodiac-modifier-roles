c6456abdffe23dadcba7e77d7b979c5af337a065
Asgeir <2156509+asgeir-eth@users.noreply.github.com>
2022-02-28 12:23:23 +0000

    [app] [app] Pending adding and removing members displayed

--- packages/app/src/components/modals/PendingChanges.tsx
+++ packages/app/src/components/modals/PendingChanges.tsx
@@ -2,13 +2,48 @@ import React, { useContext, useState } from "react"
 import { Box, makeStyles, Typography } from "@material-ui/core"
 import Modal from "../commons/Modal"
 import { useAbi } from "../../hooks/useAbi"
+import AddCircleIcon from "@material-ui/icons/AddCircle"
+import HighlightOffIcon from "@material-ui/icons/HighlightOff"
 import { RoleContext } from "../views/Role/RoleContext"
-import { Target } from "../../typings/role"
+import { EntityStatus, Target } from "../../typings/role"
+import { truncateEthAddress } from "../../utils/address"
 
 const useStyles = makeStyles((theme) => ({
+  memberContainer: {
+    alignItems: "center",
+    display: "flex",
+    fontFamily: "Roboto Mono, monospace",
+    justifyContent: "space-between",
+    marginBottom: 10,
+    padding: theme.spacing(1),
+    position: "relative",
+    "&::before": {
+      content: '" "',
+      border: "1px solid rgba(217, 212, 173, 0.3)",
+      inset: -4,
+      pointerEvents: "none",
+      position: "absolute",
+      zIndex: 1,
+    },
+  },
+  memberIconContainer: {
+    alignItems: "center",
+    background: "rgba(217, 212, 173, 0.1)",
+    borderRadius: 999,
+    display: "flex",
+    height: 20,
+    justifyContent: "center",
+    marginRight: theme.spacing(1),
+    minWidth: 20,
+  },
+  memberIcon: {},
   label: {
     color: theme.palette.text.primary,
     marginBottom: theme.spacing(0.5),
+  },
+  address: {
+    alignItems: "center",
+    display: "flex",
   },
 }))
 
@@ -44,9 +79,41 @@ type MembersProps = {
 }
 const Members = ({ members }: MembersProps): React.ReactElement => {
   const classes = useStyles()
+  console.log(members)
+
+  const memberRow = (change: "add" | "remove") => (member: string) => {
+    if (change === "add") {
+      return (
+        <Box className={classes.memberContainer}>
+          <Box className={classes.address}>
+            <Box className={classes.memberIconContainer}>
+              <AddCircleIcon color="action" className={classes.memberIcon} width={16} height={16} />
+            </Box>
+            {truncateEthAddress(member)}
+          </Box>
+        </Box>
+      )
+    }
+
+    if (change === "remove") {
+      return (
+        <Box className={classes.memberContainer}>
+          <Box className={classes.address}>
+            <Box className={classes.memberIconContainer}>
+              <HighlightOffIcon color="error" className={classes.memberIcon} width={16} height={16} />
+            </Box>
+            {truncateEthAddress(member)}
+          </Box>
+        </Box>
+      )
+    }
+  }
+
   return (
     <Box sx={{ mt: 2 }}>
       <Typography className={classes.label}>Members</Typography>
+      {members.add.map(memberRow("add"))}
+      {members.remove.map(memberRow("remove"))}
     </Box>
   )
 }
