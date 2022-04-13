7d50fa9d8ad30fab9d639d275c7f405f16056ade
Colin Spence <udappia@gmail.com>
2022-04-13 13:37:45 -0600

    Updates zodiac-ui-components

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { ThemeProvider } from "styled-components"
-import { Button, CssBaseline, ThemeProvider as MUIThemeProvider } from "@material-ui/core"
+import { CssBaseline, ThemeProvider as MUIThemeProvider } from "@material-ui/core"
 import { zodiacMuiTheme } from "zodiac-ui-components"
 import { theme as gnosisStyledComponentsTheme } from "@gnosis.pm/safe-react-components"
 import App from "./components/App"
