cada308ad98ec75c436e3132d31dc95dfc80a65d
fabianschu <48454910+fabianschu@users.noreply.github.com>
2022-05-02 09:48:39 +0200

    removes subgraph & fe code

--- packages/app/src/components/commons/layout/Column.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { Box, BoxProps } from "@material-ui/core"
-
-type ColumnProps = Omit<BoxProps, "display" | "flexDirection">
-
-export const Column = (props: ColumnProps) => {
-  return <Box display="flex" flexDirection="column" {...props} />
-}
