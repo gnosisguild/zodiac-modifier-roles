dbea50db6019bcc10e9b3e4a04bd73fa6699e190
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-05-17 16:21:51 +0200

    encode apply preset transactions and multisend support

--- src/sdk/applyPreset.ts
+++ src/sdk/applyPreset.ts
@@ -1,9 +1,11 @@
-import { Contract, Signer } from "ethers";
+import { Contract, PopulatedTransaction, Signer } from "ethers";
+import { encodeMulti, MetaTransaction, OperationType } from "ethers-multisend";
 import { defaultAbiCoder, solidityPack } from "ethers/lib/utils";
 
 import ROLES_ABI from "../../build/artifacts/contracts/Roles.sol/Roles.json";
 import { Roles } from "../../typechain-types";
 
+import encodeApplyPreset from "./encodeApplyPreset";
 import { AVATAR_ADDRESS_PLACEHOLDER } from "./placeholders";
 import {
   AllowFunction,
@@ -29,364 +31,31 @@ const applyPreset = async (
   roleId: number,
   preset: RolePreset,
   signer: Signer,
-  avatar?: string
+  avatar?: string,
+  multiSend?: string
 ): Promise<void> => {
-  sanityCheck(preset);
-
-  const contract = new Contract(address, ROLES_ABI.abi, signer) as Roles;
-
-  const avatarAddress = avatar || (await contract.avatar());
-  const filledPreset = fillPlaceholders(preset, avatarAddress);
-  console.log(`Using ${avatarAddress} for avatar address placeholders.`);
-
+  const txs = await encodeApplyPreset(address, roleId, preset, avatar);
   nonce = await signer.getTransactionCount();
 
-  await makeAllowTargetCalls(roleId, filledPreset, contract);
+  if (multiSend) {
+    const multiTx = encodeMulti(txs.map(asMetaTransaction));
+    await signer.sendTransaction({ ...multiTx, nonce: nonce++ });
+  } else {
+    await Promise.all(
+      txs.map((tx) => signer.sendTransaction({ ...tx, nonce: nonce++ }))
+    );
+  }
+};
 
-  await makeScopeTargetCalls(roleId, filledPreset, contract);
-
-  await makeScopeAllowFunctionCalls(roleId, filledPreset, contract);
-
-  await makeScopeSignatureCalls(roleId, filledPreset, contract);
-
-  await makeScopeParameterAsOneOfCalls(roleId, filledPreset, contract);
+const asMetaTransaction = (
+  populatedTransaction: PopulatedTransaction
+): MetaTransaction => {
+  return {
+    to: populatedTransaction.to || "",
+    data: populatedTransaction.data || "",
+    value: populatedTransaction.value?.toHexString() || "0",
+    operation: OperationType.Call,
+  };
 };
 
 export default applyPreset;
-
-const fillPlaceholders = (preset: RolePreset, avatarAddress: string) => ({
-  ...preset,
-  allowFunctions: preset.allowFunctions.map((allowFunction) => ({
-    ...allowFunction,
-    params: (allowFunction.params || []).map(
-      (param) =>
-        param && {
-          ...param,
-          value: fillPlaceholdersValue(param.value, avatarAddress),
-        }
-    ),
-  })),
-});
-
-const fillPlaceholdersValue = (
-  value: ScopeParam["value"],
-  avatarAddress: string
-) => {
-  const encodedAddress = defaultAbiCoder.encode(["address"], [avatarAddress]);
-
-  if (value === AVATAR_ADDRESS_PLACEHOLDER) {
-    return encodedAddress;
-  }
-  if (Array.isArray(value)) {
-    return value.map((entry) =>
-      entry === AVATAR_ADDRESS_PLACEHOLDER ? encodedAddress : entry
-    );
-  }
-
-  return value;
-};
-
-type RolesPresetFilled = ReturnType<typeof fillPlaceholders>;
-
-const ExecutionOptionLabel = {
-  [ExecutionOptions.None]: "call",
-  [ExecutionOptions.DelegateCall]: "call, delegatecall",
-  [ExecutionOptions.Send]: "call, send",
-  [ExecutionOptions.Both]: "call, delegatecall, send",
-};
-
-const ComparisonLabel = {
-  [Comparison.EqualTo]: "",
-  [Comparison.GreaterThan]: ">",
-  [Comparison.LessThan]: "<",
-};
-
-async function makeAllowTargetCalls(
-  roleId: number,
-  preset: RolesPresetFilled,
-  contract: Roles
-) {
-  await Promise.all(
-    preset.allowTargets.map(
-      async ({ targetAddress, options = ExecutionOptions.None }) => {
-        await contract.allowTarget(roleId, targetAddress, options, {
-          nonce: nonce++,
-        });
-        console.log(
-          `✔️ Allow ${ExecutionOptionLabel[options]} to any function of ${targetAddress}`
-        );
-      }
-    )
-  );
-}
-
-async function makeScopeTargetCalls(
-  roleId: number,
-  preset: RolesPresetFilled,
-  contract: Roles
-) {
-  // Every single function scoping, either: functionAllow, functionScope, parameterScope and parameterScopeAsOneOf
-  // requires a preceding scopeTarget
-  await Promise.all(
-    preset.allowFunctions
-      .flatMap((af) => af.targetAddresses)
-      .map(async (targetAddress) => {
-        await contract.scopeTarget(roleId, targetAddress, {
-          nonce: nonce++,
-        });
-        // console.log(`✔️ Allow calls to select functions of ${targetAddress}`);
-      })
-  );
-}
-
-async function makeScopeAllowFunctionCalls(
-  roleId: number,
-  preset: RolesPresetFilled,
-  contract: Roles
-) {
-  await Promise.all(
-    preset.allowFunctions.map(async (allowFunction) => {
-      if (needsScopeAllowFunctionCall(allowFunction)) {
-        const {
-          targetAddresses,
-          functionSig,
-          options = ExecutionOptions.None,
-        } = allowFunction;
-
-        await Promise.all(
-          targetAddresses.map((targetAddress) =>
-            contract.scopeAllowFunction(
-              roleId,
-              targetAddress,
-              functionSig,
-              options,
-              {
-                nonce: nonce++,
-              }
-            )
-          )
-        );
-
-        console.log(
-          `✔️ Allow ${
-            ExecutionOptionLabel[options]
-          } to ${functionSig} function of:\n${logList(targetAddresses)}`
-        );
-      }
-    })
-  );
-}
-
-async function makeScopeSignatureCalls(
-  roleId: number,
-  preset: RolesPresetFilled,
-  contract: Roles
-) {
-  preset.allowFunctions
-    .filter(needsScopeSignatureCall)
-    .map(async (allowFunction, i) => {
-      const {
-        targetAddresses,
-        functionSig,
-        options = ExecutionOptions.None,
-        params = [],
-      } = allowFunction;
-
-      // Note we exclude oneOf parameters. These will be set independently later
-      const paramsWithoutOneOf = params.map((param) =>
-        param?.comparison !== Comparison.OneOf ? param : undefined
-      );
-
-      // extract arguments
-      const isParamScoped = paramsWithoutOneOf.map(Boolean);
-      const paramType = paramsWithoutOneOf.map(
-        (entry) => entry?.type || ParameterType.Static
-      );
-      const paramComp = paramsWithoutOneOf.map(
-        (entry) => entry?.comparison || Comparison.EqualTo
-      );
-      const compValue = paramsWithoutOneOf.map(
-        (entry) => (entry?.value as string) || "0x"
-      );
-
-      await Promise.all(
-        targetAddresses.map((targetAddress) =>
-          contract.scopeFunction(
-            roleId,
-            targetAddress,
-            functionSig,
-            isParamScoped,
-            paramType,
-            paramComp,
-            compValue,
-            options,
-            {
-              nonce: nonce++,
-            }
-          )
-        )
-      );
-      console.log(
-        `✔️ Allow ${
-          ExecutionOptionLabel[options]
-        } to ${functionSig} function with params (${logParams(
-          paramsWithoutOneOf
-        )}) of:\n${logList(targetAddresses)}`
-      );
-    });
-}
-
-async function makeScopeParameterAsOneOfCalls(
-  roleId: number,
-  preset: RolesPresetFilled,
-  contract: Roles
-) {
-  await Promise.all(
-    preset.allowFunctions.map(async (allowFunction) => {
-      const {
-        targetAddresses,
-        functionSig,
-        options = ExecutionOptions.None,
-        params,
-      } = allowFunction;
-
-      // If there are other scoped params, we've already set the ExecutionOptions in makeScopeSignatureCalls
-      const hasPerformedScopeSignatureCalls =
-        countParams(allowFunction).scopedCount > 0;
-      const needsScopeFunctionExecutionOptions =
-        options != ExecutionOptions.None && !hasPerformedScopeSignatureCalls;
-
-      await Promise.all(
-        params.map(async (param, paramIndex) => {
-          // skip any params that are not scoped as oneOf
-          if (param?.comparison !== Comparison.OneOf) return;
-
-          await Promise.all(
-            targetAddresses.map(async (targetAddress) => {
-              if (needsScopeFunctionExecutionOptions) {
-                await contract.scopeFunctionExecutionOptions(
-                  roleId,
-                  targetAddress,
-                  functionSig,
-                  options,
-                  {
-                    nonce: nonce++,
-                  }
-                );
-              }
-
-              await contract.scopeParameterAsOneOf(
-                roleId,
-                targetAddress,
-                functionSig,
-                paramIndex,
-                param.type,
-                param.value as string[],
-                {
-                  nonce: nonce++,
-                }
-              );
-            })
-          );
-
-          if (hasPerformedScopeSignatureCalls) {
-            console.log(
-              `✔️ Narrow earlier allowance of ${
-                ExecutionOptionLabel[options]
-              } to ${functionSig} function: must now use params (${logParams(
-                params
-              )}) of:\n${logList(targetAddresses)}`
-            );
-          } else {
-            console.log(
-              `✔️ Allow ${
-                ExecutionOptionLabel[options]
-              } to ${functionSig} function with params (${logParams(
-                params
-              )}) of:\n${logList(targetAddresses)}`
-            );
-          }
-        })
-      );
-    })
-  );
-}
-
-function needsScopeAllowFunctionCall(allowFunction: AllowFunction): boolean {
-  const { scopedCount, scopedOneOfCount } = countParams(allowFunction);
-  return scopedCount + scopedOneOfCount === 0;
-}
-
-function needsScopeSignatureCall(allowFunction: AllowFunction): boolean {
-  const { scopedCount } = countParams(allowFunction);
-  return scopedCount > 0;
-}
-
-function countParams({ params = [] }: AllowFunction) {
-  const scopedParams = params.filter(Boolean) as ScopeParam[];
-  return {
-    scopedCount: scopedParams.filter(
-      (entry) => entry.comparison !== Comparison.OneOf
-    ).length,
-    scopedOneOfCount: scopedParams.filter(
-      (entry) => entry.comparison === Comparison.OneOf
-    ).length,
-  };
-}
-
-const logList = (addresses: string[]) =>
-  addresses.map((address) => `  - ${address}`).join("\n");
-
-const logParams = (params: (ScopeParam | undefined)[]) =>
-  params
-    .map((param) => {
-      if (!param) return "any";
-      if (param.comparison === Comparison.OneOf)
-        return `${(param.value as string[]).join(" | ")}`;
-      return `${ComparisonLabel[param.comparison]}${param.value as string}`;
-    })
-    .join(", ");
-
-const sanityCheck = (preset: RolePreset) => {
-  assertNoWildcardScopedIntersection(preset);
-  assertNoDuplicateAllowFunction(preset);
-};
-
-const assertNoWildcardScopedIntersection = (preset: RolePreset) => {
-  const wildcardTargets = preset.allowTargets.map((f) => f.targetAddress);
-  const scopedTargets = new Set(
-    preset.allowFunctions.flatMap((af) => af.targetAddresses)
-  );
-
-  const intersection = [
-    ...new Set(wildcardTargets.filter((x) => scopedTargets.has(x))),
-  ];
-  if (intersection.length > 0) {
-    throw new Error(
-      `The following addresses appear under allowTargets and allowFunctions: ${intersection.join(
-        ", "
-      )}`
-    );
-  }
-};
-
-const assertNoDuplicateAllowFunction = (preset: RolePreset) => {
-  const allowFunctions = preset.allowFunctions.flatMap((af) =>
-    af.targetAddresses.map((ta) => `${ta}.${af.functionSig}`)
-  );
-  const counts = allowFunctions.reduce(
-    (result, item) => ({ ...result, [item]: (result[item] || 0) + 1 }),
-    {} as Record<string, number>
-  );
-  const duplicates = [
-    ...new Set(allowFunctions.filter((item) => counts[item] > 1)),
-  ];
-
-  if (duplicates.length > 0) {
-    throw new Error(
-      `The following functions appear multiple times under allowFunctions: ${duplicates.join(
-        ", "
-      )}`
-    );
-  }
-};
---
+++ src/sdk/encodeApplyPreset.ts
@@ -0,0 +1,401 @@
+import { Contract, PopulatedTransaction } from "ethers";
+import { encodeMulti, MetaTransaction, OperationType } from "ethers-multisend";
+import { defaultAbiCoder } from "ethers/lib/utils";
+
+import ROLES_ABI from "../../build/artifacts/contracts/Roles.sol/Roles.json";
+import { Roles } from "../../typechain-types";
+
+import { AVATAR_ADDRESS_PLACEHOLDER } from "./placeholders";
+import {
+  AllowFunction,
+  Comparison,
+  ExecutionOptions,
+  ParameterType,
+  RolePreset,
+  ScopeParam,
+} from "./types";
+
+/**
+ * Updates a role, setting all permissions of the given preset
+ *
+ * @param address The address of the roles modifier
+ * @param roleId ID of the role to update
+ * @param preset: Permissions preset to apply
+ */
+const encodeApplyPreset = async (
+  /// address of the roles modifier
+  address: string,
+  roleId: number,
+  preset: RolePreset,
+  avatar?: string
+): Promise<PopulatedTransaction[]> => {
+  sanityCheck(preset);
+
+  const contract = new Contract(address, ROLES_ABI.abi) as Roles;
+
+  const avatarAddress = avatar || (await contract.avatar());
+  const filledPreset = fillPlaceholders(preset, avatarAddress);
+  console.log(`Using ${avatarAddress} for avatar address placeholders.`);
+
+  return await Promise.all([
+    ...populateAllowTargetCalls(roleId, filledPreset, contract),
+    ...populateScopeTargetCalls(roleId, filledPreset, contract),
+    ...populateScopeAllowFunctionCalls(roleId, filledPreset, contract),
+    ...populateScopeSignatureCalls(roleId, filledPreset, contract),
+    ...populateScopeParameterAsOneOfCalls(roleId, filledPreset, contract),
+  ]);
+};
+
+export default encodeApplyPreset;
+
+export const encodeApplyPresetMultisend = async (
+  /// address of the roles modifier
+  address: string,
+  roleId: number,
+  preset: RolePreset,
+  avatar?: string,
+  multiSendAddress?: string,
+  chunkSize = 75
+): Promise<MetaTransaction[]> => {
+  const txs = await encodeApplyPreset(address, roleId, preset, avatar);
+  return chunkArray(txs, chunkSize).map((chunk) =>
+    encodeMulti(chunk.map(asMetaTransaction), multiSendAddress)
+  );
+};
+
+const chunkArray = (array: any[], chunkSize: number) => {
+  const result = [];
+  while (array.length) {
+    result.push(array.splice(0, chunkSize));
+  }
+  return result;
+};
+
+const asMetaTransaction = (
+  populatedTransaction: PopulatedTransaction
+): MetaTransaction => {
+  return {
+    to: populatedTransaction.to || "",
+    data: populatedTransaction.data || "",
+    value: populatedTransaction.value?.toHexString() || "0",
+    operation: OperationType.Call,
+  };
+};
+
+const fillPlaceholders = (preset: RolePreset, avatarAddress: string) => ({
+  ...preset,
+  allowFunctions: preset.allowFunctions.map((allowFunction) => ({
+    ...allowFunction,
+    params: (allowFunction.params || []).map(
+      (param) =>
+        param && {
+          ...param,
+          value: fillPlaceholdersValue(param.value, avatarAddress),
+        }
+    ),
+  })),
+});
+
+const fillPlaceholdersValue = (
+  value: ScopeParam["value"],
+  avatarAddress: string
+) => {
+  const encodedAddress = defaultAbiCoder.encode(["address"], [avatarAddress]);
+
+  if (value === AVATAR_ADDRESS_PLACEHOLDER) {
+    return encodedAddress;
+  }
+  if (Array.isArray(value)) {
+    return value.map((entry) =>
+      entry === AVATAR_ADDRESS_PLACEHOLDER ? encodedAddress : entry
+    );
+  }
+
+  return value;
+};
+
+type RolesPresetFilled = ReturnType<typeof fillPlaceholders>;
+
+const ExecutionOptionLabel = {
+  [ExecutionOptions.None]: "call",
+  [ExecutionOptions.DelegateCall]: "call, delegatecall",
+  [ExecutionOptions.Send]: "call, send",
+  [ExecutionOptions.Both]: "call, delegatecall, send",
+};
+
+const ComparisonLabel = {
+  [Comparison.EqualTo]: "",
+  [Comparison.GreaterThan]: ">",
+  [Comparison.LessThan]: "<",
+};
+
+function populateAllowTargetCalls(
+  roleId: number,
+  preset: RolesPresetFilled,
+  contract: Roles
+) {
+  return preset.allowTargets.map(
+    ({ targetAddress, options = ExecutionOptions.None }) => {
+      console.log(
+        `✔️ Allow ${ExecutionOptionLabel[options]} to any function of ${targetAddress}`
+      );
+      return contract.populateTransaction.allowTarget(
+        roleId,
+        targetAddress,
+        options
+      );
+    }
+  );
+}
+
+function populateScopeTargetCalls(
+  roleId: number,
+  preset: RolesPresetFilled,
+  contract: Roles
+) {
+  // Every single function scoping, either: functionAllow, functionScope, parameterScope and parameterScopeAsOneOf
+  // requires a preceding scopeTarget
+  return preset.allowFunctions
+    .flatMap((af) => af.targetAddresses)
+    .map((targetAddress) => {
+      return contract.populateTransaction.scopeTarget(roleId, targetAddress);
+    });
+}
+
+function populateScopeAllowFunctionCalls(
+  roleId: number,
+  preset: RolesPresetFilled,
+  contract: Roles
+) {
+  return preset.allowFunctions
+    .filter(needsScopeAllowFunctionCall)
+    .flatMap((allowFunction) => {
+      const {
+        targetAddresses,
+        functionSig,
+        options = ExecutionOptions.None,
+      } = allowFunction;
+
+      console.log(
+        `✔️ Allow ${
+          ExecutionOptionLabel[options]
+        } to ${functionSig} function of:\n${logList(targetAddresses)}`
+      );
+
+      return targetAddresses.map((targetAddress) =>
+        contract.populateTransaction.scopeAllowFunction(
+          roleId,
+          targetAddress,
+          functionSig,
+          options
+        )
+      );
+    });
+}
+
+function populateScopeSignatureCalls(
+  roleId: number,
+  preset: RolesPresetFilled,
+  contract: Roles
+) {
+  return preset.allowFunctions
+    .filter(needsScopeSignatureCall)
+    .flatMap((allowFunction) => {
+      const {
+        targetAddresses,
+        functionSig,
+        options = ExecutionOptions.None,
+        params = [],
+      } = allowFunction;
+
+      // Note we exclude oneOf parameters. These will be set independently later
+      const paramsWithoutOneOf = params.map((param) =>
+        param?.comparison !== Comparison.OneOf ? param : undefined
+      );
+
+      // extract arguments
+      const isParamScoped = paramsWithoutOneOf.map(Boolean);
+      const paramType = paramsWithoutOneOf.map(
+        (entry) => entry?.type || ParameterType.Static
+      );
+      const paramComp = paramsWithoutOneOf.map(
+        (entry) => entry?.comparison || Comparison.EqualTo
+      );
+      const compValue = paramsWithoutOneOf.map(
+        (entry) => (entry?.value as string) || "0x"
+      );
+
+      console.log(
+        `✔️ Allow ${
+          ExecutionOptionLabel[options]
+        } to ${functionSig} function with params (${logParams(
+          paramsWithoutOneOf
+        )}) of:\n${logList(targetAddresses)}`
+      );
+
+      return targetAddresses.map((targetAddress) =>
+        contract.populateTransaction.scopeFunction(
+          roleId,
+          targetAddress,
+          functionSig,
+          isParamScoped,
+          paramType,
+          paramComp,
+          compValue,
+          options
+        )
+      );
+    });
+}
+
+function populateScopeParameterAsOneOfCalls(
+  roleId: number,
+  preset: RolesPresetFilled,
+  contract: Roles
+) {
+  return preset.allowFunctions.flatMap((allowFunction) => {
+    const {
+      targetAddresses,
+      functionSig,
+      options = ExecutionOptions.None,
+      params,
+    } = allowFunction;
+
+    // If there are other scoped params, we've already set the ExecutionOptions in makeScopeSignatureCalls
+    const hasPerformedScopeSignatureCalls =
+      countParams(allowFunction).scopedCount > 0;
+    const needsScopeFunctionExecutionOptions =
+      options != ExecutionOptions.None && !hasPerformedScopeSignatureCalls;
+
+    return params
+      .filter((param) => param?.comparison === Comparison.OneOf) // skip any params that are not scoped as oneOf
+      .flatMap((param, paramIndex) => {
+        return targetAddresses.flatMap((targetAddress) => {
+          if (!param)
+            throw new Error("invariant violation: param is undefined");
+          const result = [];
+
+          if (needsScopeFunctionExecutionOptions) {
+            result.push(
+              contract.populateTransaction.scopeFunctionExecutionOptions(
+                roleId,
+                targetAddress,
+                functionSig,
+                options
+              )
+            );
+          }
+
+          result.push(
+            contract.populateTransaction.scopeParameterAsOneOf(
+              roleId,
+              targetAddress,
+              functionSig,
+              paramIndex,
+              param.type,
+              param.value as string[]
+            )
+          );
+
+          if (hasPerformedScopeSignatureCalls) {
+            console.log(
+              `✔️ Narrow earlier allowance of ${
+                ExecutionOptionLabel[options]
+              } to ${functionSig} function: must now use params (${logParams(
+                params
+              )}) of:\n${logList(targetAddresses)}`
+            );
+          } else {
+            console.log(
+              `✔️ Allow ${
+                ExecutionOptionLabel[options]
+              } to ${functionSig} function with params (${logParams(
+                params
+              )}) of:\n${logList(targetAddresses)}`
+            );
+          }
+
+          return result;
+        });
+      });
+  });
+}
+
+function needsScopeAllowFunctionCall(allowFunction: AllowFunction): boolean {
+  const { scopedCount, scopedOneOfCount } = countParams(allowFunction);
+  return scopedCount + scopedOneOfCount === 0;
+}
+
+function needsScopeSignatureCall(allowFunction: AllowFunction): boolean {
+  const { scopedCount } = countParams(allowFunction);
+  return scopedCount > 0;
+}
+
+function countParams({ params = [] }: AllowFunction) {
+  const scopedParams = params.filter(Boolean) as ScopeParam[];
+  return {
+    scopedCount: scopedParams.filter(
+      (entry) => entry.comparison !== Comparison.OneOf
+    ).length,
+    scopedOneOfCount: scopedParams.filter(
+      (entry) => entry.comparison === Comparison.OneOf
+    ).length,
+  };
+}
+
+const logList = (addresses: string[]) =>
+  addresses.map((address) => `  - ${address}`).join("\n");
+
+const logParams = (params: (ScopeParam | undefined)[]) =>
+  params
+    .map((param) => {
+      if (!param) return "any";
+      if (param.comparison === Comparison.OneOf)
+        return `${(param.value as string[]).join(" | ")}`;
+      return `${ComparisonLabel[param.comparison]}${param.value as string}`;
+    })
+    .join(", ");
+
+const sanityCheck = (preset: RolePreset) => {
+  assertNoWildcardScopedIntersection(preset);
+  assertNoDuplicateAllowFunction(preset);
+};
+
+const assertNoWildcardScopedIntersection = (preset: RolePreset) => {
+  const wildcardTargets = preset.allowTargets.map((f) => f.targetAddress);
+  const scopedTargets = new Set(
+    preset.allowFunctions.flatMap((af) => af.targetAddresses)
+  );
+
+  const intersection = [
+    ...new Set(wildcardTargets.filter((x) => scopedTargets.has(x))),
+  ];
+  if (intersection.length > 0) {
+    throw new Error(
+      `The following addresses appear under allowTargets and allowFunctions: ${intersection.join(
+        ", "
+      )}`
+    );
+  }
+};
+
+const assertNoDuplicateAllowFunction = (preset: RolePreset) => {
+  const allowFunctions = preset.allowFunctions.flatMap((af) =>
+    af.targetAddresses.map((ta) => `${ta}.${af.functionSig}`)
+  );
+  const counts = allowFunctions.reduce(
+    (result, item) => ({ ...result, [item]: (result[item] || 0) + 1 }),
+    {} as Record<string, number>
+  );
+  const duplicates = [
+    ...new Set(allowFunctions.filter((item) => counts[item] > 1)),
+  ];
+
+  if (duplicates.length > 0) {
+    throw new Error(
+      `The following functions appear multiple times under allowFunctions: ${duplicates.join(
+        ", "
+      )}`
+    );
+  }
+};
