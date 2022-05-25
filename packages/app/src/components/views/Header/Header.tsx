558e535d32200405a05c8a02a2723e87418cc06a
Asgeir <asgeir@cluda.com>
2022-05-25 15:51:23 +0200

    [app] Small fixes to the header component

--- packages/app/src/components/views/Header/Header.tsx
+++ packages/app/src/components/views/Header/Header.tsx
@@ -6,6 +6,7 @@ import { HeaderAddressBox } from "./HeaderAddressBox"
 import { useRootSelector } from "../../../store"
 import { getRolesModifierAddress } from "../../../store/main/selectors"
 import { ConnectWalletBox } from "./ConnectWalletBox"
+import { useNavigate } from "react-router-dom"
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,12 +29,16 @@ const useStyles = makeStyles((theme) => ({
 
 export const Header = () => {
   const classes = useStyles()
+  const navigate = useNavigate()
 
   const module = useRootSelector(getRolesModifierAddress)
 
   return (
     <Box className={classes.root}>
-      <HeaderBox icon={<img src={RolesModuleLogo} alt="Roles App Logo" className={classes.img} />}>
+      <HeaderBox
+        icon={<img src={RolesModuleLogo} alt="Roles App Logo" className={classes.img} />}
+        onClick={() => navigate(`/${module}`)}
+      >
         <Typography variant="h5" className={classes.title}>
           Roles
         </Typography>
