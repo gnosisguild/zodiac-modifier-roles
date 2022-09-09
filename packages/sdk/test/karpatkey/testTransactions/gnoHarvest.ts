c4dc78af0becc7d583233f647605a0feea8cea7f
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-09-09 16:45:11 +0200

    fix elk farming addresses

--- packages/sdk/test/karpatkey/testTransactions/gnoHarvest.ts
+++ packages/sdk/test/karpatkey/testTransactions/gnoHarvest.ts
@@ -1,37 +1,72 @@
 export default [
   {
     from: "0x458cd345b4c05e8df39d0a07220feb4ec19f5e6f",
-    data: "0x6a627842000000000000000000000000b721cc32160ab0da2614cc6ab16ed822aeebc101",
-    to: "0xabc000d88f23bb45525e447528dbf656a9d55bf5",
-  },
-  {
-    from: "0x458cd345b4c05e8df39d0a07220feb4ec19f5e6f",
-    data: "0x84e9bd7e000000000000000000000000458cd345b4c05e8df39d0a07220feb4ec19f5e6f",
-    to: "0xb721cc32160ab0da2614cc6ab16ed822aeebc101",
-  },
-  {
-    from: "0x458cd345b4c05e8df39d0a07220feb4ec19f5e6f",
-    data: "0x84e9bd7e000000000000000000000000458cd345b4c05e8df39d0a07220feb4ec19f5e6f",
-    to: "0xa6df868420232698c1d0bf9aa8677d4873307a6a",
-  },
-  {
-    from: "0x458cd345b4c05e8df39d0a07220feb4ec19f5e6f",
-    data: "0x18fccc760000000000000000000000000000000000000000000000000000000000000004000000000000000000000000458cd345b4c05e8df39d0a07220feb4ec19f5e6f",
-    to: "0xddcbf776df3de60163066a5dddf2277cb445e0f3",
-  },
-  {
-    from: "0x458cd345b4c05e8df39d0a07220feb4ec19f5e6f",
-    data: "0x18fccc760000000000000000000000000000000000000000000000000000000000000000000000000000000000000000458cd345b4c05e8df39d0a07220feb4ec19f5e6f",
-    to: "0xdf667dea9f6857634aaaf549ca40e06f04845c03",
-  },
-  {
-    from: "0x458cd345b4c05e8df39d0a07220feb4ec19f5e6f",
     data: "0x3d18b912",
-    to: "0xDb59EF120FF1FA5013Bb5047e513162003034723",
+    to: "0x9614aE1c4F2aa5834fA99c0273613ccC6c991F56",
   },
   {
     from: "0x458cd345b4c05e8df39d0a07220feb4ec19f5e6f",
     data: "0x2459a699",
-    to: "0xDb59EF120FF1FA5013Bb5047e513162003034723",
+    to: "0x9614aE1c4F2aa5834fA99c0273613ccC6c991F56",
+  },
+  {
+    from: "0x458cd345b4c05e8df39d0a07220feb4ec19f5e6f",
+    data: "0x3d18b912",
+    to: "0xDe3f38FD15f566416Fe3941b57e1b0A50a9487e1",
+  },
+  {
+    from: "0x458cd345b4c05e8df39d0a07220feb4ec19f5e6f",
+    data: "0x2459a699",
+    to: "0xDe3f38FD15f566416Fe3941b57e1b0A50a9487e1",
+  },
+  {
+    from: "0x458cd345b4c05e8df39d0a07220feb4ec19f5e6f",
+    data: "0x3d18b912",
+    to: "0xc35EcbcA23597747a11E34e733EFe54c2D774F1a",
+  },
+  {
+    from: "0x458cd345b4c05e8df39d0a07220feb4ec19f5e6f",
+    data: "0x2459a699",
+    to: "0xc35EcbcA23597747a11E34e733EFe54c2D774F1a",
+  },
+  {
+    from: "0x458cd345b4c05e8df39d0a07220feb4ec19f5e6f",
+    data: "0x3d18b912",
+    to: "0x400445e4FFD2AdE6435B36c9A827B50a496960A2",
+  },
+  {
+    from: "0x458cd345b4c05e8df39d0a07220feb4ec19f5e6f",
+    data: "0x2459a699",
+    to: "0x400445e4FFD2AdE6435B36c9A827B50a496960A2",
+  },
+  {
+    from: "0x458cd345b4c05e8df39d0a07220feb4ec19f5e6f",
+    data: "0x3d18b912",
+    to: "0x5942A302f2bdceb43C2934B42c584f4ee5f7B027",
+  },
+  {
+    from: "0x458cd345b4c05e8df39d0a07220feb4ec19f5e6f",
+    data: "0x2459a699",
+    to: "0x5942A302f2bdceb43C2934B42c584f4ee5f7B027",
+  },
+  {
+    from: "0x458cd345b4c05e8df39d0a07220feb4ec19f5e6f",
+    data: "0x3d18b912",
+    to: "0xa88072Bb6fDfd51c4e9158E95a9b8D2C200c7291",
+  },
+  {
+    from: "0x458cd345b4c05e8df39d0a07220feb4ec19f5e6f",
+    data: "0x2459a699",
+    to: "0xa88072Bb6fDfd51c4e9158E95a9b8D2C200c7291",
+  },
+  {
+    from: "0x458cd345b4c05e8df39d0a07220feb4ec19f5e6f",
+    data: "0x3d18b912",
+    to: "0x94A59d8eB6D09F64aA9f4Deda8Bf1272deBC07f8",
+  },
+  {
+    from: "0x458cd345b4c05e8df39d0a07220feb4ec19f5e6f",
+    data: "0x2459a699",
+    to: "0x94A59d8eB6D09F64aA9f4Deda8Bf1272deBC07f8",
   },
 ]
