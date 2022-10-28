bf24f6f28ab01041b537424092e175c0a5955dac
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-10-28 12:09:15 +0200

    some fixes

--- packages/sdk/src/presets/helpers/curve/regular.ts
+++ packages/sdk/src/presets/helpers/curve/regular.ts
@@ -1,16 +1,21 @@
-import { Pool } from "./types"
-import { PresetAllowEntry } from '../../../types'
+import { PresetAllowEntry } from "../../../types"
+import { AVATAR_ADDRESS_PLACEHOLDER } from "../../placeholders"
 import { staticEqual } from "../utils"
-import {
-  AVATAR_ADDRESS_PLACEHOLDER,
-} from "../../placeholders"
+
+import { Pool } from "./types"
 
 const CRV_MINTER_ADDRESS = "0xd061D61a4d941c39E5453435B6345Dc261C2fcE0"
 
 export const allowRegularPool = (pool: Pool): PresetAllowEntry[] => {
   const poolFunctions: PresetAllowEntry[] = [
-    //Gotta make sure the token address is not "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE" 
-    { tokens: pool.tokens.filter(token => token.toLowerCase() !== '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'), spenders: [pool.address] },
+    //Gotta make sure the token address is not "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
+    {
+      tokens: pool.tokens.filter(
+        (token) =>
+          token.toLowerCase() !== "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+      ),
+      spenders: [pool.address],
+    },
     {
       targetAddresses: [pool.address],
       signature: "add_liquidity(uint256[${pool.tokens.length}],uint256)",
@@ -25,26 +30,25 @@ export const allowRegularPool = (pool: Pool): PresetAllowEntry[] => {
     },
     {
       targetAddresses: [pool.address],
-      signature: "remove_liquidity_imbalance(uint256[${pool.tokens.length}],uint256)",
+      signature:
+        "remove_liquidity_imbalance(uint256[${pool.tokens.length}],uint256)",
     },
     {
       targetAddresses: [pool.address],
       signature: "exchange(int128,int128,uint256,uint256)",
     },
-  ];
+  ]
 
   if (pool.meta === true) {
-    poolFunctions.push(
-      {
-        targetAddresses: [pool.address],
-        signature: "exchange_underlying(int128,int128,uint256,uint256)",
-      }
-    )
+    poolFunctions.push({
+      targetAddresses: [pool.address],
+      signature: "exchange_underlying(int128,int128,uint256,uint256)",
+    })
   }
 
   const result = poolFunctions
 
-  if ('gauge' in pool) {
+  if ("gauge" in pool) {
     const gaugeFunctions = [
       { tokens: [pool.token], spenders: [pool.gauge.address] },
       {
@@ -61,16 +65,14 @@ export const allowRegularPool = (pool: Pool): PresetAllowEntry[] => {
         signature: "mint(address)",
         params: { [0]: staticEqual(AVATAR_ADDRESS_PLACEHOLDER) },
       },
-    ];
+    ]
 
     if (pool.gauge.type !== "LiquidityGauge") {
-      gaugeFunctions.push(
-        {
-          targetAddresses: [pool.gauge.address],
-          signature: "claim_rewards(address)",
-          params: { [0]: staticEqual(AVATAR_ADDRESS_PLACEHOLDER) },
-        }
-      )
+      gaugeFunctions.push({
+        targetAddresses: [pool.gauge.address],
+        signature: "claim_rewards(address)",
+        params: { [0]: staticEqual(AVATAR_ADDRESS_PLACEHOLDER) },
+      })
     }
 
     result.push(...gaugeFunctions)
@@ -82,7 +84,8 @@ export const allowRegularPool = (pool: Pool): PresetAllowEntry[] => {
       { tokens: [...pool.zap.basePool.tokens], spenders: [pool.zap.address] },
       {
         targetAddresses: [pool.zap.address],
-        signature: "add_liquidity(uint256[${pool.tokens.length+pool.zap.basePool.tokens.length-1}],uint256)",
+        signature:
+          "add_liquidity(uint256[${pool.tokens.length+pool.zap.basePool.tokens.length-1}],uint256)",
       },
       {
         targetAddresses: [pool.zap.address],
@@ -90,16 +93,17 @@ export const allowRegularPool = (pool: Pool): PresetAllowEntry[] => {
       },
       {
         targetAddresses: [pool.zap.address],
-        signature: "remove_liquidity(uint256,uint256[${pool.tokens.length+pool.zap.basePool.tokens.length-1}])",
+        signature:
+          "remove_liquidity(uint256,uint256[${pool.tokens.length+pool.zap.basePool.tokens.length-1}])",
       },
       {
         targetAddresses: [pool.zap.address],
-        signature: "signature': 'remove_liquidity_imbalance(uint256[${pool.tokens.length+pool.zap.basePool.tokens.length-1}],uint256)",
+        signature:
+          "signature': 'remove_liquidity_imbalance(uint256[${pool.tokens.length+pool.zap.basePool.tokens.length-1}],uint256)",
       },
-    ];
+    ]
 
     result.push(...zapFunctions)
-
   }
 
   return result
--- packages/sdk/src/presets/helpers/curve/types.ts
+++ packages/sdk/src/presets/helpers/curve/types.ts
@@ -45,8 +45,7 @@ import pools from "./pools"
 //   | CryptoFactoryPool["name"]
 //   | CryptoFactoryPoolAddresses
 
-
 type ArrayElement<ArrayType extends readonly unknown[]> =
-  ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
+  ArrayType extends readonly (infer ElementType)[] ? ElementType : never
 
 export type Pool = ArrayElement<typeof pools>
