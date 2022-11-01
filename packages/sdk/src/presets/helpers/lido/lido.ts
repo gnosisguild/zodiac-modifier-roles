1d65866cb658fe398a59fadcc05057d6f9e0e02d
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-11-01 10:50:29 +0100

    fixed some syntax and typing errors

--- packages/sdk/src/presets/helpers/lido/lido.ts
+++ packages/sdk/src/presets/helpers/lido/lido.ts
@@ -4,11 +4,11 @@ const STETH = "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84"
 const WSTETH = "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0"
 
 export const allowLido = (): PresetAllowEntry[] => {
-    return [
-        { targetAddresses: [WSTETH], signature: "wrap(uint256)" },
-        { targetAddresses: [WSTETH], signature: "unwrap(uint256)" },
-        { targetAddresses: [STETH], signature: "submit(address)" },
-        { tokens: [STETH], spenders: [WSTETH] },
-        { tokens: [WSTETH], spenders: [WSTETH] },
-    ]
+  return [
+    { targetAddresses: [WSTETH], signature: "wrap(uint256)" },
+    { targetAddresses: [WSTETH], signature: "unwrap(uint256)" },
+    { targetAddresses: [STETH], signature: "submit(address)" },
+    { tokens: [STETH], spenders: [WSTETH] },
+    { tokens: [WSTETH], spenders: [WSTETH] },
+  ]
 }
