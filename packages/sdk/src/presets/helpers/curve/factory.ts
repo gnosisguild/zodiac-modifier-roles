34961eb2cb4df8ee647c43196edee8bb84377e1c
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-11-01 10:46:19 +0100

    adjust curve factory helper to new api

--- packages/sdk/src/presets/helpers/curve/factory.ts
+++ packages/sdk/src/presets/helpers/curve/factory.ts
@@ -1,5 +1,6 @@
 import { PresetAllowEntry } from "../../../types"
 import { AVATAR_ADDRESS_PLACEHOLDER } from "../../placeholders"
+import { allowErc20Approve } from "../erc20"
 import { staticEqual } from "../utils"
 
 import { Pool } from "./types"
@@ -7,109 +8,105 @@ import { Pool } from "./types"
 const CRV_MINTER_ADDRESS = "0xd061D61a4d941c39E5453435B6345Dc261C2fcE0"
 
 export const allowFactoryPool = (pool: Pool): PresetAllowEntry[] => {
-    const poolFunctions: PresetAllowEntry[] = [
-        //Gotta make sure the token address is not "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
-        {
-            tokens: (pool.tokens as readonly string[]).filter(
-                (token) =>
-                    token.toLowerCase() !== "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
-            ),
-            spenders: [pool.address],
-        },
-        {
-            targetAddresses: [pool.address],
-            signature: "add_liquidity(uint256[${pool.tokens.length}],uint256)",
-        },
-        {
-            targetAddresses: [pool.address],
-            signature: "remove_liquidity_one_coin(uint256,int128,uint256)",
-        },
-        {
-            targetAddresses: [pool.address],
-            signature: "remove_liquidity(uint256,uint256[${pool.tokens.length}])",
-        },
-        {
-            targetAddresses: [pool.address],
-            signature:
-                "remove_liquidity_imbalance(uint256[${pool.tokens.length}],uint256)",
-        },
-        {
-            targetAddresses: [pool.address],
-            signature: "exchange(int128,int128,uint256,uint256)",
-        },
+  const result: PresetAllowEntry[] = [
+    //Gotta make sure the token address is not "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"
+    ...allowErc20Approve(
+      (pool.tokens as readonly string[]).filter(
+        (token) =>
+          token.toLowerCase() !== "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"
+      ),
+      [pool.address]
+    ),
+    {
+      targetAddress: pool.address,
+      signature: "add_liquidity(uint256[${pool.tokens.length}],uint256)",
+    },
+    {
+      targetAddress: pool.address,
+      signature: "remove_liquidity_one_coin(uint256,int128,uint256)",
+    },
+    {
+      targetAddress: pool.address,
+      signature: "remove_liquidity(uint256,uint256[${pool.tokens.length}])",
+    },
+    {
+      targetAddress: pool.address,
+      signature:
+        "remove_liquidity_imbalance(uint256[${pool.tokens.length}],uint256)",
+    },
+    {
+      targetAddress: pool.address,
+      signature: "exchange(int128,int128,uint256,uint256)",
+    },
+  ]
+
+  if (pool.meta) {
+    result.push({
+      targetAddress: pool.address,
+      signature: "exchange_underlying(int128,int128,uint256,uint256)",
+    })
+  }
+
+  if ("gauge" in pool) {
+    const gaugeFunctions: PresetAllowEntry[] = [
+      ...allowErc20Approve([pool.token], [pool.gauge.address]),
+      {
+        targetAddress: pool.gauge.address,
+        signature: "deposit(uint256)",
+      },
+      {
+        targetAddress: pool.gauge.address,
+        signature: "withdraw(uint256)",
+      },
+      {
+        targetAddress: CRV_MINTER_ADDRESS,
+        signature: "mint(address)",
+        params: { [0]: staticEqual(AVATAR_ADDRESS_PLACEHOLDER) },
+      },
     ]
 
-    if (pool.meta === true) {
-        poolFunctions.push({
-            targetAddresses: [pool.address],
-            signature: "exchange_underlying(int128,int128,uint256,uint256)",
-        })
+    if (pool.gauge.type !== "LiquidityGauge") {
+      gaugeFunctions.push({
+        targetAddress: pool.gauge.address,
+        signature: "claim_rewards(address)",
+        params: { [0]: staticEqual(AVATAR_ADDRESS_PLACEHOLDER) },
+      })
     }
 
-    const result = poolFunctions
+    result.push(...gaugeFunctions)
+  }
 
-    if ("gauge" in pool) {
-        const gaugeFunctions = [
-            { tokens: [pool.token], spenders: [pool.gauge.address] },
-            {
-                targetAddresses: [pool.gauge.address],
-                signature: "deposit(uint256)",
-            },
-            {
-                targetAddresses: [pool.gauge.address],
-                signature: "withdraw(uint256)",
-            },
-            //Define minter address as constant
-            {
-                targetAddresses: [CRV_MINTER_ADDRESS],
-                signature: "mint(address)",
-                params: { [0]: staticEqual(AVATAR_ADDRESS_PLACEHOLDER) },
-            },
-        ]
-
-        if (pool.gauge.type !== "LiquidityGauge") {
-            gaugeFunctions.push({
-                targetAddresses: [pool.gauge.address],
-                signature: "claim_rewards(address)",
-                params: { [0]: staticEqual(AVATAR_ADDRESS_PLACEHOLDER) },
-            })
-        }
-
-        result.push(...gaugeFunctions)
+  if ("zap" in pool) {
+    if ("basePool" in pool.zap) {
+      //Ask Nico about the approvals which are missing in Python
+      result.push(
+        ...allowErc20Approve([...pool.zap.basePool.tokens], [pool.zap.address])
+      )
     }
+    const zapFunctions = [
+      {
+        targetAddress: pool.zap.address,
+        signature:
+          "add_liquidity(uint256[${pool.tokens.length+pool.zap.basePool.tokens.length-1}],uint256)",
+      },
+      {
+        targetAddress: pool.zap.address,
+        signature: "remove_liquidity_one_coin(uint256,int128,uint256)",
+      },
+      {
+        targetAddress: pool.zap.address,
+        signature:
+          "remove_liquidity(uint256,uint256[${pool.tokens.length+pool.zap.basePool.tokens.length-1}])",
+      },
+      {
+        targetAddress: pool.zap.address,
+        signature:
+          "signature': 'remove_liquidity_imbalance(uint256[${pool.tokens.length+pool.zap.basePool.tokens.length-1}],uint256)",
+      },
+    ]
 
-    if ("zap" in pool) {
-        if ("basePool" in pool.zap) {
-            //Ask Nico about the approvals which are missing in Python
-            result.push({
-                tokens: [...pool.zap.basePool.tokens],
-                spenders: [pool.zap.address],
-            })
-        }
-        const zapFunctions = [
-            {
-                targetAddresses: [pool.zap.address],
-                signature:
-                    "add_liquidity(uint256[${pool.tokens.length+pool.zap.basePool.tokens.length-1}],uint256)",
-            },
-            {
-                targetAddresses: [pool.zap.address],
-                signature: "remove_liquidity_one_coin(uint256,int128,uint256)",
-            },
-            {
-                targetAddresses: [pool.zap.address],
-                signature:
-                    "remove_liquidity(uint256,uint256[${pool.tokens.length+pool.zap.basePool.tokens.length-1}])",
-            },
-            {
-                targetAddresses: [pool.zap.address],
-                signature:
-                    "signature': 'remove_liquidity_imbalance(uint256[${pool.tokens.length+pool.zap.basePool.tokens.length-1}],uint256)",
-            },
-        ]
+    result.push(...zapFunctions)
+  }
 
-        result.push(...zapFunctions)
-    }
-
-    return result
+  return result
 }
