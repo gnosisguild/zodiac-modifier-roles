c1fbb0e7b798ac4a327ec9abfe934f1e36725220
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-11-01 10:50:29 +0100

    fixed some syntax and typing errors

--- packages/sdk/src/presets/helpers/curve/factory.ts
+++ packages/sdk/src/presets/helpers/curve/factory.ts
@@ -77,35 +77,39 @@ export const allowFactoryPool = (pool: Pool): PresetAllowEntry[] => {
   }
 
   if ("zap" in pool) {
-    if ("basePool" in pool.zap) {
-      //Ask Nico about the approvals which are missing in Python
-      result.push(
-        ...allowErc20Approve([...pool.zap.basePool.tokens], [pool.zap.address])
-      )
-    }
-    const zapFunctions = [
-      {
-        targetAddress: pool.zap.address,
-        signature:
-          "add_liquidity(uint256[${pool.tokens.length+pool.zap.basePool.tokens.length-1}],uint256)",
-      },
+    const zapFunctions: PresetAllowEntry[] = [
       {
         targetAddress: pool.zap.address,
         signature: "remove_liquidity_one_coin(uint256,int128,uint256)",
       },
-      {
-        targetAddress: pool.zap.address,
-        signature:
-          "remove_liquidity(uint256,uint256[${pool.tokens.length+pool.zap.basePool.tokens.length-1}])",
-      },
-      {
-        targetAddress: pool.zap.address,
-        signature:
-          "signature': 'remove_liquidity_imbalance(uint256[${pool.tokens.length+pool.zap.basePool.tokens.length-1}],uint256)",
-      },
     ]
-
     result.push(...zapFunctions)
+
+    if ("basePool" in pool.zap) {
+      const basePoolFunctions: PresetAllowEntry[] = [
+        //Ask Nico about the approvals which are missing in Python
+        ...allowErc20Approve([...pool.zap.basePool.tokens], [pool.zap.address]),
+        {
+          targetAddress: pool.zap.address,
+          signature: `add_liquidity(uint256[${
+            pool.tokens.length + pool.zap.basePool.tokens.length - 1
+          }],uint256)`,
+        },
+        {
+          targetAddress: pool.zap.address,
+          signature: `remove_liquidity(uint256,uint256[${
+            pool.tokens.length + pool.zap.basePool.tokens.length - 1
+          }])`,
+        },
+        {
+          targetAddress: pool.zap.address,
+          signature: `remove_liquidity_imbalance(uint256[${
+            pool.tokens.length + pool.zap.basePool.tokens.length - 1
+          }],uint256)`,
+        },
+      ]
+      result.push(...basePoolFunctions)
+    }
   }
 
   return result
