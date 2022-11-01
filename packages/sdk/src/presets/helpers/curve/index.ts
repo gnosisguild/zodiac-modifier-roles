58116dc075b180f60c8e4810d8c7910c4209b66d
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-11-01 11:07:48 +0100

    more fixes and refactorings for curve pool helpers

--- packages/sdk/src/presets/helpers/curve/index.ts
+++ packages/sdk/src/presets/helpers/curve/index.ts
@@ -1,5 +1,9 @@
+import { PresetAllowEntry } from "../../../types"
+
+import { allowGauge } from "./gauge"
 import pools from "./pools"
 import { allowRegularPool } from "./regular"
+import { allowZap } from "./zap"
 
 const findPool = (poolNameOrAddress: string) =>
   pools.find(
@@ -14,12 +18,24 @@ const allowCurvePool = (poolNameOrTokenAddress: string) => {
     throw new Error(`Pool not found: ${poolNameOrTokenAddress}`)
   }
 
+  const result: PresetAllowEntry[] = []
   switch (pool.type) {
     case "regular":
-      return allowRegularPool(pool)
+      result.push(...allowRegularPool(pool))
+      break
     default:
       throw new Error(`Not yet implemented: ${pool.type}`)
   }
+
+  if ("gauge" in pool) {
+    result.push(...allowGauge(pool))
+  }
+
+  if ("zap" in pool) {
+    result.push(...allowZap(pool))
+  }
+
+  return result
 }
 
 export default allowCurvePool
