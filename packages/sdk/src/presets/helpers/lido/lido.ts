134a1918db5d01616bdbc15239ddfa32aca0bf82
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-11-01 11:09:44 +0100

    adjust lido helper to new api

--- packages/sdk/src/presets/helpers/lido/lido.ts
+++ packages/sdk/src/presets/helpers/lido/lido.ts
@@ -1,14 +1,14 @@
 import { PresetAllowEntry } from "../../../types"
+import { allowErc20Approve } from "../erc20"
 
 const STETH = "0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84"
 const WSTETH = "0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0"
 
 export const allowLido = (): PresetAllowEntry[] => {
   return [
-    { targetAddresses: [WSTETH], signature: "wrap(uint256)" },
-    { targetAddresses: [WSTETH], signature: "unwrap(uint256)" },
-    { targetAddresses: [STETH], signature: "submit(address)" },
-    { tokens: [STETH], spenders: [WSTETH] },
-    { tokens: [WSTETH], spenders: [WSTETH] },
+    { targetAddress: WSTETH, signature: "wrap(uint256)" },
+    { targetAddress: WSTETH, signature: "unwrap(uint256)" },
+    { targetAddress: STETH, signature: "submit(address)" },
+    ...allowErc20Approve([STETH, WSTETH], [WSTETH]),
   ]
 }
