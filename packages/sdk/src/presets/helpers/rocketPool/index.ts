56ea5aab86b19e31fe7f9389933957926a86bedf
sgzerbo <98768518+sgzerbo@users.noreply.github.com>
2022-11-15 11:39:47 -0300

    Added Aura helper functions and fixes

--- packages/sdk/src/presets/helpers/rocketPool/index.ts
+++ packages/sdk/src/presets/helpers/rocketPool/index.ts
@@ -1,7 +1,7 @@
 import { ExecutionOptions, PresetAllowEntry } from "../../../types"
 
 const RETH = "0xae78736Cd615f374D3085123A210448E74Fc6393"
-const ROCKETDEPOSITPOOL = "0x2cac916b2A963Bf162f076C0a8a4a8200BCFBfb4"
+const ROCKET_DEPOSIT_POOL = "0x2cac916b2A963Bf162f076C0a8a4a8200BCFBfb4"
 
 export const allowRocketPool = (): PresetAllowEntry[] => {
     return [
