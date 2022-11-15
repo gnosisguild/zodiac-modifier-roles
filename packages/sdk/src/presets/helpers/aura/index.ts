b96f0e779a9a9c7422e450bb7e39711a092525ee
sgzerbo <98768518+sgzerbo@users.noreply.github.com>
2022-11-15 11:39:47 -0300

    Added Aura helper functions and fixes

---
+++ packages/sdk/src/presets/helpers/aura/index.ts
@@ -0,0 +1,49 @@
+import { PresetAllowEntry } from "../../../types"
+import { allowErc20Approve } from "../erc20"
+
+import pools from "./pools"
+
+const BOOSTER_ADDRESS = "0x7818A1DA7BD1E64c199029E86Ba244a9798eEE10"
+const REWARD_POOL_DEPOSIT_WRAPPER_ADDRESS = "0xB188b1CB84Fb0bA13cb9ee1292769F903A9feC59"
+
+const findPool = (poolNameOrTokenAddress: string) =>
+    pools.find(
+        (pool) =>
+            pool.name === poolNameOrTokenAddress || pool.token === poolNameOrTokenAddress
+    )
+
+export const allowAuraPool = (poolNameOrTokenAddress: string): PresetAllowEntry[] => {
+    const pool = findPool(poolNameOrTokenAddress)
+
+    if (!pool) {
+        throw new Error(`Pool not found: ${poolNameOrTokenAddress}`)
+    }
+
+    const result: PresetAllowEntry[] = [
+        ...allowErc20Approve([pool.token], [BOOSTER_ADDRESS]),
+        {
+            targetAddress: BOOSTER_ADDRESS,
+            signature: "deposit(uint256,uint256,bool)",
+        },
+        {
+            targetAddress: pool.rewarder,
+            signature: "withdrawAndUnwrap(uint256,bool)",
+        },
+        {
+            targetAddress: pool.rewarder,
+            signature: "getReward()",
+        }
+    ]
+
+    //Only boosted pools do not have the "tokens" key, though there's only one boosted pool at the moment
+    if ("tokens" in pool) {
+        result.push(
+            ...allowErc20Approve(pool.tokens, REWARD_POOL_DEPOSIT_WRAPPER_ADDRESS),
+            {
+                targetAddress: REWARD_POOL_DEPOSIT_WRAPPER_ADDRESS,
+                signature: "depositSingle(address,address,uint256,bytes32,tuple)",
+            }
+        )
+    }
+    return result
+}
