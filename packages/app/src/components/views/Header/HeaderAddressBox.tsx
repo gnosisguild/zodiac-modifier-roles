8287f5f04dcf433915b8c32e3a75878f03e1fc14
Colin Spence <udappia@gmail.com>
2022-12-06 13:01:50 -0700

    Adds Zodiac link in header box

--- packages/app/src/components/views/Header/HeaderAddressBox.tsx
+++ packages/app/src/components/views/Header/HeaderAddressBox.tsx
@@ -1,8 +1,12 @@
 import { HeaderBox } from "./HeaderBox"
-import { makeStyles, Typography } from "@material-ui/core"
+import { Box, Link, makeStyles, Typography } from "@material-ui/core"
+import { ArrowForward } from "@material-ui/icons"
 import { EthHashInfo } from "@gnosis.pm/safe-react-components"
 import { truncateEthAddress } from "../../../utils/address"
 import classNames from "classnames"
+import { useRootSelector } from "../../../store"
+import { getChainId, getRolesModifierAddress } from "../../../store/main/selectors"
+import { getNetwork, NETWORKS } from "../../../utils/networks"
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -22,6 +26,19 @@ const useStyles = makeStyles((theme) => ({
   title: {
     marginLeft: theme.spacing(1),
   },
+  zodiacArrow: {
+    color: "currentColor",
+    marginLeft: theme.spacing(0.5),
+    width: theme.spacing(2),
+  },
+  zodiacLink: {
+    color: "rgba(255,255,255,0.8)",
+    display: "flex",
+    alignItems: "center",
+    "&:hover": {
+      color: "white",
+    },
+  },
   addressFont: {
     fontFamily: "Roboto Mono",
   },
@@ -36,8 +53,14 @@ interface HeaderAddressBoxProps {
   onClick?: () => void
 }
 
+const networkConfigs = NETWORKS.map(getNetwork)
+
 export const HeaderAddressBox = ({ address, emptyText, onClick }: HeaderAddressBoxProps) => {
   const classes = useStyles()
+  const network = useRootSelector(getChainId)
+  const rolesModifierAddress = useRootSelector(getRolesModifierAddress)
+
+  const networkShortname = networkConfigs.find((chain) => chain.chainId === network)?.shortName
   return (
     <HeaderBox
       icon={
@@ -54,12 +77,26 @@ export const HeaderAddressBox = ({ address, emptyText, onClick }: HeaderAddressB
       }
       onClick={onClick}
     >
-      <Typography
-        variant="body1"
-        className={classNames(classes.title, { [classes.empty]: !address, [classes.addressFont]: address })}
-      >
-        {address ? truncateEthAddress(address) : emptyText}
-      </Typography>
+      <Box className={classes.title}>
+        <Typography
+          variant="body1"
+          className={classNames({ [classes.empty]: !address, [classes.addressFont]: address })}
+        >
+          {address ? truncateEthAddress(address) : emptyText}
+        </Typography>
+        {address && (
+          <Link
+            target="_blank"
+            rel="noredirect"
+            href={`https://app.safe.global/${networkShortname}:${rolesModifierAddress}/apps?appUrl=https%3A%2F%2Fzodiac.gnosisguild.org%2F`}
+            className={classes.zodiacLink}
+            underline="always"
+          >
+            View in Zodiac
+            <ArrowForward className={classes.zodiacArrow} />
+          </Link>
+        )}
+      </Box>
     </HeaderBox>
   )
 }
