31fe9a6d7341cd90277bc2ab827881923850a29b
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-12-20 14:57:40 +0100

    preserve param names

--- packages/sdk/src/presets/allow.ts
+++ packages/sdk/src/presets/allow.ts
@@ -13,19 +13,15 @@ import { TupleScopings } from "./types"
 const mainnetProvider = ethers.getDefaultProvider("mainnet")
 const defaultSigner = ethers.Wallet.createRandom().connect(mainnetProvider)
 
-const sdk = getMainnetSdk(defaultSigner)
-
 interface CallOptions {
   send?: boolean
   delegatecall?: boolean
 }
 
-// const makeAllowKit = <T extends EthersFunctions, U extends { [key in keyof T]: any }>(conditions: T, input: any):
-
 type MapParams<T extends any[]> = ((...b: T) => void) extends (
   ...args: [...infer I, any]
 ) => void
-  ? [...TupleScopings<I>, CallOptions]
+  ? [...params: TupleScopings<I>, options?: CallOptions]
   : []
 
 const makeAllowFunction = <
@@ -84,26 +80,19 @@ const makeAllowContract = <C extends BaseContract>(
   return Object.assign(allowEntireContract, allowFunctions)
 }
 
-const allowUniswap = makeAllowContract(sdk.uniswap.nftPositions)
-
-allowUniswap.mint(
-  { token0: "0x6B175474E89094C44Da98b954EedeAC495" },
-  { send: true, delegatecall: false }
-) // YAY!
-
 type EthSdk = {
   [key: string]: EthSdk | BaseContract
 }
 
-type AllowSdk<S extends EthSdk> = {
+type AllowKit<S extends EthSdk> = {
   [Key in keyof S]: S[Key] extends BaseContract
     ? AllowContract<S[Key]>
     : S[Key] extends EthSdk // somehow it cannot infer that it cannot be a BaseContract here, so we use an extra conditional
-    ? AllowSdk<S[Key]>
+    ? AllowKit<S[Key]>
     : never
 }
 
-const mapSdkRecursive = <S extends EthSdk>(sdk: S): AllowSdk<S> => {
+const mapSdkRecursive = <S extends EthSdk>(sdk: S): AllowKit<S> => {
   return Object.keys(sdk).reduce((acc, key) => {
     if (sdk[key] instanceof BaseContract) {
       acc[key] = makeAllowContract(sdk[key] as BaseContract)
@@ -118,22 +107,11 @@ export const allow = {
   mainnet: mapSdkRecursive(getMainnetSdk(defaultSigner)),
 }
 
+allow.mainnet.uniswap.nftPositions.mint({ amount0Desired: { oneOf: [1, 2] } })
+
 const execOptions = (options: CallOptions = {}): ExecutionOptions => {
   if (options.send && options.delegatecall) return ExecutionOptions.Both
   if (options.delegatecall) return ExecutionOptions.DelegateCall
   if (options.send) return ExecutionOptions.Send
   return ExecutionOptions.None
 }
-
-// sdk.uniswap.nftPositions.functions.burn(1, { send: true, delegatecall: false })
-
-// // allow.uniswap.nftPositions.wholeTarget({ send: true, delegatecall: false })
-// sdk.uniswap.nftPositions.mint(eq(1), oneOf([1, 2, 3], undefined,  { send: true, delegatecall: false })
-
-// type Scoping<T> = { eq: T } | { oneOf: T[] }
-// const eq = <T>(value: T): Scoping<T> => ({ eq: value })
-
-// const allowFunction = <T>(
-//   ...scopings: (Scoping<T> | undefined)[],
-//   options: CallOptions = {}
-// ): PresetFunction => ({})
