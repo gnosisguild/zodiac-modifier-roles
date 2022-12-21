090840348d8229b8e97fb1691f0c0888b80acfcd
Jan-Felix <jan-felix.schwarz@gnosis.io>
2022-12-21 16:41:05 +0100

    typesafe placeholders

--- packages/sdk/src/presets/placeholders.ts
+++ packages/sdk/src/presets/placeholders.ts
@@ -1,8 +1,23 @@
+// legacy placeholder format
 export const AVATAR_ADDRESS_PLACEHOLDER = Symbol("AVATAR_ADDRESS_PLACEHOLDER")
 
 export const OMNI_BRIDGE_DATA_PLACEHOLDER = Symbol(
   "OMNI_BRIDGE_DATA_PLACEHOLDER"
 )
+
 export const OMNI_BRIDGE_RECEIVER_PLACEHOLDER = Symbol(
   "OMNI_BRIDGE_RECEIVER_PLACEHOLDER"
 )
+
+// new placeholder format
+export const AVATAR_ADDRESS = {
+  string: Symbol("AVATAR_ADDRESS"),
+}
+
+export const OMNI_BRIDGE_DATA = {
+  byteslike: Symbol("OMNI_BRIDGE_DATA"),
+}
+
+export const OMNI_BRIDGE_RECEIVER = {
+  string: Symbol("OMNI_BRIDGE_RECEIVER"),
+}
