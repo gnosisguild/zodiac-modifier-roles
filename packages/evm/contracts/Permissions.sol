85e8308ba6df408fc08b6f2e44c7dd6a38965519
cristovaoth <cristovao.honorato@gmail.com>
2022-12-31 00:05:39 +0000

    Extract functionConfig mask manipulation functions

--- packages/evm/contracts/Permissions.sol
+++ packages/evm/contracts/Permissions.sol
@@ -2,45 +2,9 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 import "@gnosis.pm/safe-contracts/contracts/common/Enum.sol";
+import "./Types.sol";
 
-enum ParameterType {
-    Static,
-    Dynamic,
-    Dynamic32
-}
-
-enum Comparison {
-    EqualTo,
-    GreaterThan,
-    LessThan,
-    OneOf
-}
-
-enum ExecutionOptions {
-    None,
-    Send,
-    DelegateCall,
-    Both
-}
-
-enum Clearance {
-    None,
-    Target,
-    Function
-}
-
-struct TargetAddress {
-    Clearance clearance;
-    ExecutionOptions options;
-}
-
-struct Role {
-    mapping(address => bool) members;
-    mapping(address => TargetAddress) targets;
-    mapping(bytes32 => uint256) functions;
-    mapping(bytes32 => bytes32) compValues;
-    mapping(bytes32 => bytes32[]) compValuesOneOf;
-}
+import "./FunctionConfig.sol";
 
 library Permissions {
     uint256 internal constant SCOPE_MAX_PARAMS = 48;
@@ -274,9 +238,8 @@ library Permissions {
                 revert FunctionNotAllowed();
             }
 
-            (ExecutionOptions options, bool isWildcarded, ) = unpackFunction(
-                scopeConfig
-            );
+            (ExecutionOptions options, bool isWildcarded, ) = FunctionConfig
+                .unpack(scopeConfig);
 
             checkExecutionOptions(value, operation, options);
 
@@ -329,14 +292,14 @@ library Permissions {
         bytes memory data
     ) internal view {
         bytes4 functionSig = bytes4(data);
-        (, , uint256 length) = unpackFunction(scopeConfig);
+        (, , uint256 length) = FunctionConfig.unpack(scopeConfig);
 
         for (uint256 i = 0; i < length; i++) {
             (
                 bool isScoped,
                 ParameterType paramType,
                 Comparison paramComp
-            ) = unpackParameter(scopeConfig, i);
+            ) = FunctionConfig.unpackParameter(scopeConfig, i);
 
             if (!isScoped) {
                 continue;
@@ -456,14 +419,14 @@ library Permissions {
         ExecutionOptions options
     ) external {
         /*
-         * packLeft(
+         * pack(
          *    0           -> start from a fresh scopeConfig
          *    options     -> externally provided options
          *    true        -> mark the function as wildcarded
          *    0           -> length
          * )
          */
-        uint256 scopeConfig = packLeft(0, options, true, 0);
+        uint256 scopeConfig = FunctionConfig.pack(0, options, true, 0);
         role.functions[
             keyForFunctions(targetAddress, functionSig)
         ] = scopeConfig;
@@ -534,16 +497,16 @@ library Permissions {
         }
 
         /*
-         * packLeft(
+         * pack(
          *    0           -> start from a fresh scopeConfig
          *    options     -> externally provided options
          *    false       -> mark the function as not wildcarded
          *    length      -> length
          * )
          */
-        uint256 scopeConfig = packLeft(0, options, false, length);
+        uint256 scopeConfig = FunctionConfig.pack(0, options, false, length);
         for (uint256 i = 0; i < length; i++) {
-            scopeConfig = packRight(
+            scopeConfig = FunctionConfig.packParameterRaw(
                 scopeConfig,
                 i,
                 isScoped[i],
@@ -592,7 +555,10 @@ library Permissions {
         bytes32 key = keyForFunctions(targetAddress, functionSig);
 
         //set scopeConfig
-        uint256 scopeConfig = packOptions(role.functions[key], options);
+        uint256 scopeConfig = FunctionConfig.packOptions(
+            role.functions[key],
+            options
+        );
 
         role.functions[
             keyForFunctions(targetAddress, functionSig)
@@ -635,7 +601,7 @@ library Permissions {
 
         // set scopeConfig
         bytes32 key = keyForFunctions(targetAddress, functionSig);
-        uint256 scopeConfig = packParameter(
+        uint256 scopeConfig = FunctionConfig.packParameter(
             role.functions[key],
             index,
             true, // isScoped
@@ -692,7 +658,7 @@ library Permissions {
 
         // set scopeConfig
         bytes32 key = keyForFunctions(targetAddress, functionSig);
-        uint256 scopeConfig = packParameter(
+        uint256 scopeConfig = FunctionConfig.packParameter(
             role.functions[key],
             index,
             true, // isScoped
@@ -741,7 +707,7 @@ library Permissions {
 
         // set scopeConfig
         bytes32 key = keyForFunctions(targetAddress, functionSig);
-        uint256 scopeConfig = packParameter(
+        uint256 scopeConfig = FunctionConfig.packParameter(
             role.functions[key],
             index,
             false, // isScoped
@@ -907,144 +873,6 @@ library Permissions {
         for (uint256 j = start; j < end; j++) {
             result[j - start] = data[j];
         }
-    }
-
-    /*
-     * pack/unpack are bit helpers for scopeConfig
-     */
-    function packParameter(
-        uint256 scopeConfig,
-        uint256 index,
-        bool isScoped,
-        ParameterType paramType,
-        Comparison paramComp
-    ) internal pure returns (uint256) {
-        (ExecutionOptions options, , uint256 prevLength) = unpackFunction(
-            scopeConfig
-        );
-
-        uint256 nextLength = index + 1 > prevLength ? index + 1 : prevLength;
-
-        return
-            packLeft(
-                packRight(scopeConfig, index, isScoped, paramType, paramComp),
-                options,
-                false, // isWildcarded=false
-                nextLength
-            );
-    }
-
-    function packOptions(
-        uint256 scopeConfig,
-        ExecutionOptions options
-    ) internal pure returns (uint256) {
-        uint256 optionsMask = 3 << 254;
-
-        scopeConfig &= ~optionsMask;
-        scopeConfig |= uint256(options) << 254;
-
-        return scopeConfig;
-    }
-
-    function packLeft(
-        uint256 scopeConfig,
-        ExecutionOptions options,
-        bool isWildcarded,
-        uint256 length
-    ) internal pure returns (uint256) {
-        // LEFT SIDE
-        // 2   bits -> options
-        // 1   bits -> isWildcarded
-        // 5   bits -> unused
-        // 8   bits -> length
-        // RIGHT SIDE
-        // 48  bits -> isScoped
-        // 96  bits -> paramType (2 bits per entry 48*2)
-        // 96  bits -> paramComp (2 bits per entry 48*2)
-
-        // Wipe the LEFT SIDE clean. Start from there
-        scopeConfig = (scopeConfig << 16) >> 16;
-
-        // set options -> 256 - 2 = 254
-        scopeConfig |= uint256(options) << 254;
-
-        // set isWildcarded -> 256 - 2 - 1 = 253
-        if (isWildcarded) {
-            scopeConfig |= 1 << 253;
-        }
-
-        // set Length -> 48 + 96 + 96 = 240
-        scopeConfig |= length << 240;
-
-        return scopeConfig;
-    }
-
-    function packRight(
-        uint256 scopeConfig,
-        uint256 index,
-        bool isScoped,
-        ParameterType paramType,
-        Comparison paramComp
-    ) internal pure returns (uint256) {
-        // LEFT SIDE
-        // 2   bits -> options
-        // 1   bits -> isWildcarded
-        // 5   bits -> unused
-        // 8   bits -> length
-        // RIGHT SIDE
-        // 48  bits -> isScoped
-        // 96  bits -> paramType (2 bits per entry 48*2)
-        // 96  bits -> paramComp (2 bits per entry 48*2)
-        uint256 isScopedMask = 1 << (index + 96 + 96);
-        uint256 paramTypeMask = 3 << (index * 2 + 96);
-        uint256 paramCompMask = 3 << (index * 2);
-
-        if (isScoped) {
-            scopeConfig |= isScopedMask;
-        } else {
-            scopeConfig &= ~isScopedMask;
-        }
-
-        scopeConfig &= ~paramTypeMask;
-        scopeConfig |= uint256(paramType) << (index * 2 + 96);
-
-        scopeConfig &= ~paramCompMask;
-        scopeConfig |= uint256(paramComp) << (index * 2);
-
-        return scopeConfig;
-    }
-
-    function unpackFunction(
-        uint256 scopeConfig
-    )
-        internal
-        pure
-        returns (ExecutionOptions options, bool isWildcarded, uint256 length)
-    {
-        uint256 isWildcardedMask = 1 << 253;
-
-        options = ExecutionOptions(scopeConfig >> 254);
-        isWildcarded = scopeConfig & isWildcardedMask != 0;
-        length = (scopeConfig << 8) >> 248;
-    }
-
-    function unpackParameter(
-        uint256 scopeConfig,
-        uint256 index
-    )
-        internal
-        pure
-        returns (bool isScoped, ParameterType paramType, Comparison paramComp)
-    {
-        uint256 isScopedMask = 1 << (index + 96 + 96);
-        uint256 paramTypeMask = 3 << (index * 2 + 96);
-        uint256 paramCompMask = 3 << (index * 2);
-
-        isScoped = (scopeConfig & isScopedMask) != 0;
-        paramType = ParameterType(
-            (scopeConfig & paramTypeMask) >> (index * 2 + 96)
-        );
-        paramComp = Comparison((scopeConfig & paramCompMask) >> (index * 2));
     }
 
     function keyForFunctions(
