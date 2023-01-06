7773f7e23421a5499d67bd8f6aeb772ee26d38c6
sgzerbo <98768518+sgzerbo@users.noreply.github.com>
2023-01-06 19:41:33 -0300

    Balancer production presets

---
+++ packages/sdk/src/presets/helpers/stakewise/stakewise.ts
@@ -0,0 +1,7 @@
+import { PresetAllowEntry } from "../../../types"
+import { AVATAR_ADDRESS_PLACEHOLDER } from "../../placeholders"
+import { staticEqual } from "../utils"
+
+export const allowStakewise = (): PresetAllowEntry[] => {
+  return []
+}
