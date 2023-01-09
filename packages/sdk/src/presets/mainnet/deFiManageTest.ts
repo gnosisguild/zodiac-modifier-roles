3f361c44cbc8ccd77c6178b7c53cf3211036a792
sgzerbo <98768518+sgzerbo@users.noreply.github.com>
2023-01-09 17:20:19 -0300

    Updated tests

--- packages/sdk/src/presets/mainnet/deFiManageTest.ts
+++ packages/sdk/src/presets/mainnet/deFiManageTest.ts
@@ -2,14 +2,17 @@ import { ExecutionOptions, RolePreset } from "../../types"
 import { allowCurvePool } from "../helpers/curve"
 import { allowErc20Approve } from "../helpers/erc20"
 import { allowLido } from "../helpers/lido"
-import { staticEqual } from "../helpers/utils"
+import { dynamic32Equal, staticEqual } from "../helpers/utils"
 import {
   AVATAR_ADDRESS_PLACEHOLDER,
   OMNI_BRIDGE_RECEIVER_PLACEHOLDER,
 } from "../placeholders"
 
+const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+const cUSDC = "0x39AA39c021dfbaE8faC545936693aC917d5E7563"
+
 const preset: RolePreset = {
   network: 1,
-  allow: [...allowLido()],
+  allow: [...allowErc20Approve([USDC], [cUSDC])],
 }
 export default preset
---
+++ packages/sdk/test/karpatkey/testTransactions/testManage.ts
@@ -0,0 +1,7 @@
+export default [
+  {
+    from: "0xcfBE92a0482e0d7D1e6501Ecf56d532B2853014F",
+    to: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+    data: "0x095ea7b300000000000000000000000039aa39c021dfbae8fac545936693ac917d5e7563ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",
+  },
+]
