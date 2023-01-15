84321fd3842f3511aa40f70416303d442e964218
cristovaoth <cristovao.honorato@gmail.com>
2023-01-15 17:44:24 +0000

    Expand scopeConfig to support the more ParameterTypes (bump mask representation for type from 2 to 3)

---
+++ packages/evm/contracts/test/MockScopeConfig.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.6.0 <0.9.0;
+
+import "../ScopeConfig.sol";
+
+contract MockScopeConfig {
+    function pack(
+        uint256 scopeConfig,
+        ExecutionOptions options,
+        bool isWildcarded,
+        uint256 length
+    ) public pure returns (uint256) {
+        return ScopeConfig.pack(scopeConfig, options, isWildcarded, length);
+    }
+
+    function packParameter(
+        uint256 scopeConfig,
+        uint256 index,
+        bool isScoped,
+        ParameterType paramType,
+        Comparison paramComp
+    ) public pure returns (uint256) {
+        return
+            ScopeConfig.packParameter(
+                scopeConfig,
+                index,
+                isScoped,
+                paramType,
+                paramComp
+            );
+    }
+
+    function unpack(
+        uint256 scopeConfig
+    )
+        public
+        pure
+        returns (ExecutionOptions options, bool isWildcarded, uint256 length)
+    {
+        return ScopeConfig.unpack(scopeConfig);
+    }
+
+    function unpackParameter(
+        uint256 scopeConfig,
+        uint256 index
+    )
+        public
+        pure
+        returns (bool isScoped, ParameterType paramType, Comparison paramComp)
+    {
+        return ScopeConfig.unpackParameter(scopeConfig, index);
+    }
+}
