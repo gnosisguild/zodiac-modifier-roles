bfe18d62deaab0549bd919591f8cac930d1b1335
cristovaoth <cristovao.honorato@gmail.com>
2023-03-12 18:00:00 +0100

    Use write-once storage to pack scopeConfigs data

--- packages/evm/contracts/test/MockScopeConfig.sol
+++ packages/evm/contracts/test/MockScopeConfig.sol
@@ -4,47 +4,48 @@ pragma solidity >=0.6.0 <0.9.0;
 import "../bitmaps/ScopeConfig.sol";
 
 contract MockScopeConfig {
-    // function pack(
-    //     uint256 scopeConfig,
-    //     ExecutionOptions options,
-    //     bool isWildcarded,
-    //     uint256 length
-    // ) public pure returns (uint256) {
-    //     return ScopeConfig2.pack(scopeConfig, options, isWildcarded, length);
-    // }
-    // function packParameter(
-    //     uint256 scopeConfig,
-    //     uint256 index,
-    //     bool isScoped,
-    //     ParameterType paramType,
-    //     Comparison paramComp
-    // ) public pure returns (uint256) {
-    //     return
-    //         ScopeConfig.packParameter(
-    //             scopeConfig,
-    //             index,
-    //             isScoped,
-    //             paramType,
-    //             paramComp
-    //         );
-    // }
-    // function unpack(
-    //     uint256 scopeConfig
-    // )
-    //     public
-    //     pure
-    //     returns (ExecutionOptions options, bool isWildcarded, uint256 length)
-    // {
-    //     return ScopeConfig.unpack(scopeConfig);
-    // }
-    // function unpackParameter(
-    //     uint256 scopeConfig,
-    //     uint256 index
-    // )
-    //     public
-    //     pure
-    //     returns (bool isScoped, ParameterType paramType, Comparison paramComp)
-    // {
-    //     return ScopeConfig.unpackParameter(scopeConfig, index);
-    // }
+    function packParameter(
+        uint256 index,
+        ParameterConfigFlat calldata parameter,
+        bool isHashed
+    ) public pure returns (bytes memory buffer) {
+        buffer = new bytes(1024);
+        ScopeConfig.packParameter(buffer, index, parameter, isHashed);
+    }
+
+    function unpackParameter(
+        bytes memory buffer,
+        uint256 index
+    )
+        public
+        pure
+        returns (ParameterType _type, Comparison comp, bool isHashed)
+    {
+        ParameterConfig memory p = ScopeConfig.unpackParameter(buffer, index);
+        return (p._type, p.comp, p.isHashed);
+    }
+
+    function packHeader(
+        uint256 length,
+        bool isWildcarded,
+        ExecutionOptions options,
+        address pointer
+    ) public pure returns (bytes32 result) {
+        return ScopeConfig.packHeader(length, isWildcarded, options, pointer);
+    }
+
+    function unpackHeader(
+        bytes32 header
+    )
+        public
+        pure
+        returns (
+            uint256 _length,
+            bool isWildcarded,
+            ExecutionOptions options,
+            address pointer
+        )
+    {
+        return ScopeConfig.unpackHeader(header);
+    }
 }
---
+++ packages/evm/contracts/test/TestLoaderGas.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity >=0.8.0;
+
+import "../PermissionLoader.sol";
+
+contract TestLoaderGas is PermissionLoader {
+    uint16 public constant roleId = 12345;
+    bytes32 public constant key = bytes32(bytes4(0xabcdef00));
+
+    function store(
+        ParameterConfigFlat[] calldata parameters,
+        ExecutionOptions options
+    ) public {
+        _store(roles[roleId], key, parameters, options);
+    }
+
+    function load() public {
+        ParameterConfig memory result = _load(roles[roleId], key);
+        assert(result.children.length > 0);
+    }
+
+    function storeNaive(
+        ParameterConfigFlat[] calldata parameters,
+        ExecutionOptions options
+    ) public {
+        Role storage role = roles[roleId];
+        role.scopeConfig[key] = bytes32(parameters.length);
+        uint8[] memory config = new uint8[](parameters.length);
+
+        for (uint i; i < parameters.length; ++i) {
+            ParameterConfigFlat memory parameter = parameters[i];
+
+            if (shouldPack(parameter.comp)) {
+                config[i] = 1;
+                bytes32 compValue = parameter.compValue.length > 32
+                    ? keccak256(parameter.compValue)
+                    : bytes32(parameter.compValue);
+                role.scopeConfig[_key(key, i)] = compValue;
+            }
+        }
+        role.scopeConfig[key] = bytes32(abi.encodePacked(config));
+    }
+
+    function loadNaive() public {
+        bytes32[] memory result = new bytes32[](32);
+
+        Role storage role = roles[roleId];
+        bytes32 value = role.scopeConfig[key];
+
+        for (uint256 i; value != 0; ++i) {
+            if (value & bytes32(bytes1(0xff)) != 0) {
+                result[i] = role.scopeConfig[_key(key, i)];
+            }
+            value = value >> 8;
+        }
+    }
+
+    function shouldPack(Comparison comparison) private returns (bool) {
+        return
+            comparison == Comparison.EqualTo ||
+            comparison == Comparison.GreaterThan ||
+            comparison == Comparison.LessThan ||
+            comparison == Comparison.WithinAllowance ||
+            comparison == Comparison.Bitmask;
+    }
+}
