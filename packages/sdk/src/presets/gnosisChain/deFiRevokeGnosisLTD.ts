76ea275fa5202450892bb5bc70ef0310a60df655
nledo <nicolas@karpatkey.com>
2023-03-21 15:41:10 -0300

    Added allowErc20Revoke

---
+++ packages/sdk/src/presets/gnosisChain/deFiRevokeGnosisLTD.ts
@@ -0,0 +1,23 @@
+import { ZERO_ADDRESS } from "./addresses"
+import { allowErc20Approve } from "../helpers/erc20"
+import {
+    dynamic32Equal,
+    dynamic32OneOf,
+    staticEqual,
+    dynamicOneOf,
+    subsetOf,
+    dynamicEqual,
+    staticOneOf,
+} from "../helpers/utils"
+import { AVATAR } from "../placeholders"
+import { RolePreset } from "../types"
+import { allow } from "../allow"
+
+
+const preset = {
+    network: 100,
+    allow: [],
+    placeholders: { AVATAR },
+} satisfies RolePreset
+
+export default preset
--- packages/sdk/src/presets/helpers/erc20.ts
+++ packages/sdk/src/presets/helpers/erc20.ts
@@ -11,12 +11,25 @@ export const allowErc20Approve = (tokens: string[], spenders: string[]) =>
       spenders.length === 1
         ? staticEqual(spenders[0], "address")
         : {
-            type: ParameterType.Static,
-            comparison: Comparison.OneOf,
-            value: spenders.map((spender) =>
-              defaultAbiCoder.encode(["address"], [spender])
-            ),
-          },
+          type: ParameterType.Static,
+          comparison: Comparison.OneOf,
+          value: spenders.map((spender) =>
+            defaultAbiCoder.encode(["address"], [spender])
+          ),
+        },
+    ],
+  })
+
+export const allowErc20Revoke = (tokens: string[]) =>
+  forAllTargetAddresses(tokens, {
+    signature: "approve(address,uint256)",
+    params: [
+      undefined,
+      {
+        type: ParameterType.Static,
+        comparison: Comparison.EqualTo,
+        value: [defaultAbiCoder.encode(["uint256"], [0])],
+      },
     ],
   })
 
@@ -27,11 +40,11 @@ export const allowErc20Transfer = (tokens: string[], recipients: string[]) =>
       recipients.length === 1
         ? staticEqual(recipients[0], "address")
         : {
-            type: ParameterType.Static,
-            comparison: Comparison.OneOf,
-            value: recipients.map((recipient) =>
-              defaultAbiCoder.encode(["address"], [recipient])
-            ),
-          },
+          type: ParameterType.Static,
+          comparison: Comparison.OneOf,
+          value: recipients.map((recipient) =>
+            defaultAbiCoder.encode(["address"], [recipient])
+          ),
+        },
     ],
   })
