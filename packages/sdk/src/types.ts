b9a4b5525193a78bac7f3739b6a0944976a0475a
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-03-22 13:31:04 +0100

    update types

--- packages/sdk/src/types.ts
+++ packages/sdk/src/types.ts
@@ -2,30 +2,65 @@ import SUBGRAPH from "./subgraph"
 
 export type NetworkId = keyof typeof SUBGRAPH
 
-export enum ParameterType {
-  Static,
-  Dynamic,
-  Dynamic32,
-}
-
-export enum Comparison {
-  EqualTo,
-  GreaterThan,
-  LessThan,
-  OneOf,
-}
-
-export enum Clearance {
-  None,
-  Target,
-  Function,
-}
-
 export enum ExecutionOptions {
   None = 0,
   Send = 1,
   DelegateCall = 2,
   Both = 3,
+}
+
+export enum Clearance {
+  None = 0,
+  Target = 1,
+  Function = 2,
+}
+
+export enum ParameterType {
+  None = 0,
+  Static = 1,
+  Dynamic = 2,
+  Tuple = 3,
+  Array = 4,
+  AbiEncoded = 5,
+}
+
+export enum Operator {
+  // 00:    EMPTY EXPRESSION (default, always passes)
+  //          paramType: Static / Dynamic
+  //          🚫 children
+  //          🚫 compValue
+  Pass = 0,
+  // ------------------------------------------------------------
+  // 01-04: BOOLEAN EXPRESSIONS
+  //          paramType: None
+  //          ✅ children
+  //          🚫 compValue
+  And = 1,
+  Or = 2,
+  Xor = 3,
+  Not = 4,
+  // ------------------------------------------------------------
+  // 05-16: COMPLEX EXPRESSIONS
+  //          paramType: AbiEncoded / Tuple / Array,
+  //          ✅ children
+  //          🚫 compValue
+  Matches = 5,
+  ArraySome = 6,
+  ArrayEvery = 7,
+  ArraySubset = 8,
+
+  // ------------------------------------------------------------
+  // 17-31: COMPARISON EXPRESSIONS
+  //          paramType: Static / Dynamic / Tuple / Array / AbiEncoded
+  //          🚫 children
+  //          ✅ compValue
+  EqualTo = 17,
+  GreaterThan = 18,
+  LessThan = 19,
+  Bitmask = 20,
+  WithinAllowance = 29,
+  EtherWithinAllowance = 30,
+  CallWithinAllowance = 31,
 }
 
 export interface RolePermissions {
@@ -35,89 +70,27 @@ export interface RolePermissions {
 export interface Target {
   address: string
   clearance: Clearance
-  executionOptions: ExecutionOptions
+  options: ExecutionOptions
   functions: Function[]
 }
 
 export interface Function {
-  sighash: string
-  executionOptions: ExecutionOptions
+  selector: string
+  options: ExecutionOptions
   wildcarded: boolean
-  parameters: Parameter[]
+  condition?: Condition
 }
 
-export interface Parameter {
-  index: number
-  type: ParameterType
-  comparison: Comparison
-  comparisonValue: string[]
+export interface Condition {
+  paramType: ParameterType
+  operator: Operator
+  compValue?: string
+  children?: Condition[]
 }
 
-interface AllowTargetCall {
-  call: "allowTarget"
-  targetAddress: string
-  options: ExecutionOptions
+export interface ConditionFlat {
+  parent: number
+  paramType: ParameterType
+  operator: Operator
+  compValue?: string
 }
-
-interface ScopeTargetCall {
-  call: "scopeTarget"
-  targetAddress: string
-}
-
-interface ScopeAllowFunctionCall {
-  call: "scopeAllowFunction"
-  targetAddress: string
-  functionSig: string
-  options: ExecutionOptions
-}
-
-interface ScopeFunctionCall {
-  call: "scopeFunction"
-  targetAddress: string
-  functionSig: string
-  isParamScoped: boolean[]
-  paramType: ParameterType[]
-  paramComp: (
-    | Comparison.EqualTo
-    | Comparison.GreaterThan
-    | Comparison.LessThan
-  )[]
-  compValue: string[]
-  options: ExecutionOptions
-}
-
-interface ScopeParameterAsOneOfCall {
-  call: "scopeParameterAsOneOf"
-  targetAddress: string
-  functionSig: string
-  paramIndex: number
-  type: ParameterType
-  value: string[]
-}
-
-interface UnscopeParameterCall {
-  call: "unscopeParameter"
-  targetAddress: string
-  functionSig: string
-  paramIndex: number
-}
-
-interface ScopeRevokeFunctionCall {
-  call: "scopeRevokeFunction"
-  targetAddress: string
-  functionSig: string
-}
-
-interface RevokeTargetCall {
-  call: "revokeTarget"
-  targetAddress: string
-}
-
-export type Call =
-  | AllowTargetCall
-  | ScopeTargetCall
-  | ScopeAllowFunctionCall
-  | ScopeFunctionCall
-  | ScopeParameterAsOneOfCall
-  | ScopeRevokeFunctionCall
-  | RevokeTargetCall
