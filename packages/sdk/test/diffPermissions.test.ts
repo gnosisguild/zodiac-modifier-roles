8b0f258967ccbe3c21f0cc98036a4ca905a2e5be
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-03-23 13:57:59 +0100

    fix test

--- packages/sdk/test/diffPermissions.test.ts
+++ packages/sdk/test/diffPermissions.test.ts
@@ -1,130 +1,104 @@
 import { expect } from "chai"
 
-import diffPermissions from "../src/diffPermissions"
-import { Clearance, ExecutionOptions, Role } from "../src/types"
+import { diffPermissions } from "../src/diffPermissions"
+import {
+  Clearance,
+  ExecutionOptions,
+  Operator,
+  ParameterType,
+  Target,
+} from "../src/types"
 
 describe("diffPermissions", () => {
   it("should correctly diff target-cleared targets", () => {
-    const a: Role = {
-      key: "a",
-      members: [],
-      targets: [
-        {
-          address: "0x1",
-          clearance: Clearance.Target,
-          executionOptions: ExecutionOptions.None,
-          functions: [],
-        },
-        {
-          address: "0x2",
-          clearance: Clearance.Target,
-          executionOptions: ExecutionOptions.None,
-          functions: [],
-        },
-      ],
-    }
+    const a: Target[] = [
+      {
+        address: "0x1",
+        clearance: Clearance.Target,
+        executionOptions: ExecutionOptions.None,
+        functions: [],
+      },
+      {
+        address: "0x2",
+        clearance: Clearance.Target,
+        executionOptions: ExecutionOptions.None,
+        functions: [],
+      },
+    ]
 
-    const b: Role = {
-      key: "b",
-      members: [],
-      targets: [
-        {
-          address: "0x1",
-          clearance: Clearance.Target,
-          executionOptions: ExecutionOptions.None,
-          functions: [],
-        },
-        {
-          address: "0x2",
-          clearance: Clearance.Target,
-          executionOptions: ExecutionOptions.Both,
-          functions: [],
-        },
-      ],
-    }
+    const b: Target[] = [
+      {
+        address: "0x1",
+        clearance: Clearance.Target,
+        executionOptions: ExecutionOptions.None,
+        functions: [],
+      },
+      {
+        address: "0x2",
+        clearance: Clearance.Target,
+        executionOptions: ExecutionOptions.Both,
+        functions: [],
+      },
+    ]
 
-    expect(diffPermissions(a, b)).to.deep.equal({
-      targets: [
-        {
-          address: "0x2",
-          clearance: Clearance.Target,
-          executionOptions: ExecutionOptions.None,
-          functions: [],
-        },
-      ],
-    })
-    expect(diffPermissions(b, a)).to.deep.equal({
-      targets: [
-        {
-          address: "0x2",
-          clearance: Clearance.Target,
-          executionOptions: ExecutionOptions.Both,
-          functions: [],
-        },
-      ],
-    })
+    expect(diffPermissions(a, b)).to.deep.equal([
+      {
+        address: "0x2",
+        clearance: Clearance.Target,
+        executionOptions: ExecutionOptions.None,
+        functions: [],
+      },
+    ])
+    expect(diffPermissions(b, a)).to.deep.equal([
+      {
+        address: "0x2",
+        clearance: Clearance.Target,
+        executionOptions: ExecutionOptions.Both,
+        functions: [],
+      },
+    ])
   })
 
-  it("should diff functions based all their properties including parameters", () => {
-    const a: Role = {
-      key: "a",
-      members: [],
-      targets: [
-        {
-          address: "0x9C58BAcC331c9aa871AFD802DB6379a98e80CEdb",
-          clearance: 2,
-          executionOptions: 0,
-          functions: [
-            {
-              selector: "0x095ea7b3",
-              executionOptions: 0,
-              wildcarded: false,
-              parameters: [
-                {
-                  index: 0,
-                  type: 0,
-                  comparison: 3,
-                  comparisonValue: [
-                    "0x000000000000000000000000ddcbf776df3de60163066a5dddf2277cb445e0f3",
-                  ],
-                },
-              ],
+  it("should diff functions based all their properties including conditions", () => {
+    const a: Target[] = [
+      {
+        address: "0x9C58BAcC331c9aa871AFD802DB6379a98e80CEdb",
+        clearance: 2,
+        executionOptions: 0,
+        functions: [
+          {
+            selector: "0x095ea7b3",
+            executionOptions: 0,
+            wildcarded: false,
+            condition: {
+              paramType: ParameterType.None,
+              operator: Operator.Pass,
             },
-          ],
-        },
-      ],
-    }
+          },
+        ],
+      },
+    ]
 
-    const b: Role = {
-      key: "b",
-      members: [],
-      targets: [
-        {
-          address: "0x9C58BAcC331c9aa871AFD802DB6379a98e80CEdb",
-          clearance: 2,
-          executionOptions: 0,
-          functions: [
-            {
-              selector: "0x095ea7b3",
-              executionOptions: 0,
-              wildcarded: false,
-              parameters: [
-                {
-                  index: 1,
-                  type: 0,
-                  comparison: 1,
-                  comparisonValue: [
-                    "0x000000000000000000000000ddcbf776df3de60163066a5dddf2277cb445e0f3",
-                  ],
-                },
-              ],
+    const b: Target[] = [
+      {
+        address: "0x9C58BAcC331c9aa871AFD802DB6379a98e80CEdb",
+        clearance: 2,
+        executionOptions: 0,
+        functions: [
+          {
+            selector: "0x095ea7b3",
+            executionOptions: 0,
+            wildcarded: false,
+            condition: {
+              paramType: ParameterType.None,
+              operator: Operator.Pass,
             },
-          ],
-        },
-      ],
-    }
+          },
+        ],
+      },
+    ]
 
-    expect(diffPermissions(a, a)).to.deep.equal({ targets: [] })
+    expect(diffPermissions(a, a)).to.deep.equal([])
     expect(diffPermissions(a, b)).to.deep.equal(a)
   })
 })
