680e03ee1426b6bb58f3fc57a66b2de759bd7c1e
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-03-24 16:59:13 +0100

    rework erc20 helpers

--- packages/sdk/src/presets/helpers/erc20.ts
+++ packages/sdk/src/presets/helpers/erc20.ts
@@ -1,10 +1,8 @@
-import { defaultAbiCoder } from "ethers/lib/utils"
-
-import { Comparison, ParameterType } from "../../types"
 import { Placeholder } from "../types"
 
 import { equalTo, oneOf } from "./basic"
 import { forAll } from "./batch"
+import { matches } from "./matches"
 
 export const allowErc20Approve = (
   tokens: string[],
@@ -12,24 +10,42 @@ export const allowErc20Approve = (
 ) =>
   forAll(tokens, {
     signature: "approve(address,uint256)",
-    condition:
-      spenders.length === 1
-        ? equalTo(spenders[0], "address")
-        : oneOf(spenders, "address"),
+    condition: matches(
+      [
+        spenders.length === 1
+          ? equalTo(spenders[0], "address")
+          : oneOf(spenders, "address"),
+      ],
+      ["address"]
+    ),
+  })
+
+export const allowErc20Revoke = (
+  tokens: string[],
+  spenders: (string | Placeholder<string>)[]
+) =>
+  forAll(tokens, {
+    signature: "approve(address,uint256)",
+    condition: matches(
+      [
+        spenders.length === 1
+          ? equalTo(spenders[0], "address")
+          : oneOf(spenders, "address"),
+        equalTo(0, "uint256"),
+      ],
+      ["address", "uint256"]
+    ),
   })
 
 export const allowErc20Transfer = (tokens: string[], recipients: string[]) =>
   forAll(tokens, {
     signature: "transfer(address,uint256)",
-    params: [
-      recipients.length === 1
-        ? staticEqual(recipients[0], "address")
-        : {
-            type: ParameterType.Static,
-            comparison: Comparison.OneOf,
-            value: recipients.map((recipient) =>
-              defaultAbiCoder.encode(["address"], [recipient])
-            ),
-          },
-    ],
+    condition: matches(
+      [
+        recipients.length === 1
+          ? equalTo(recipients[0], "address")
+          : oneOf(recipients, "address"),
+      ],
+      ["address"]
+    ),
   })
