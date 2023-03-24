ebb5955368b6dcd75de6d89664f787b80aa6337f
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-03-24 17:01:18 +0100

    clear out defi protocol helpers
    these should be migrated from v1 and live in karpatkey repos in the future

--- packages/sdk/src/presets/helpers/stakewise/stakewise.ts
+++
@@ -1,7 +0,0 @@
-import { PresetAllowEntry } from "../../../types"
-import { AVATAR } from "../../placeholders"
-import { staticEqual } from "../basic"
-
-export const allowStakewise = (): PresetAllowEntry[] => {
-  return []
-}
