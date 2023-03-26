19bf88e0500070090c45832e5a175d1fc3545939
cristovaoth <cristovao.honorato@gmail.com>
2023-03-26 18:41:45 +0100

    Operator Matches dedicated tests. Refactoring test setups to an aux file

--- packages/evm/test/operators/00Pass.spec.ts
+++ packages/evm/test/operators/00Pass.spec.ts
@@ -1,78 +1,11 @@
 import { expect } from "chai";
-import hre from "hardhat";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
-
-import {
-  Operator,
-  ExecutionOptions,
-  ParameterType,
-  deployRolesMod,
-} from "../utils";
-import { ConditionFlatStruct } from "../../typechain-types/contracts/Integrity";
+import { Operator, ParameterType } from "../utils";
+import { setupOneParamStatic } from "./setup";
 
 describe("Operator - Pass", async () => {
-  const ROLE_KEY =
-    "0x0000000000000000000000000000000000000000000000000000000000000001";
-
-  async function setup() {
-    const [owner, invoker] = await hre.ethers.getSigners();
-
-    const Avatar = await hre.ethers.getContractFactory("TestAvatar");
-    const avatar = await Avatar.deploy();
-
-    const TestContract = await hre.ethers.getContractFactory("TestContract");
-    const testContract = await TestContract.deploy();
-
-    const roles = await deployRolesMod(
-      hre,
-      owner.address,
-      avatar.address,
-      avatar.address
-    );
-    await roles.enableModule(invoker.address);
-
-    await roles.connect(owner).assignRoles(invoker.address, [ROLE_KEY], [true]);
-    await roles.connect(owner).setDefaultRole(invoker.address, ROLE_KEY);
-
-    const SELECTOR = testContract.interface.getSighash(
-      testContract.interface.getFunction("fnWithSingleParam")
-    );
-
-    async function invoke(a: number) {
-      return roles
-        .connect(invoker)
-        .execTransactionFromModule(
-          testContract.address,
-          0,
-          (await testContract.populateTransaction.fnWithSingleParam(a))
-            .data as string,
-          0
-        );
-    }
-
-    await roles.connect(owner).scopeTarget(ROLE_KEY, testContract.address);
-
-    async function scopeFunction(conditions: ConditionFlatStruct[]) {
-      await roles
-        .connect(owner)
-        .scopeFunction(
-          ROLE_KEY,
-          testContract.address,
-          SELECTOR,
-          conditions,
-          ExecutionOptions.None
-        );
-    }
-
-    return {
-      roles,
-      scopeFunction,
-      invoke,
-    };
-  }
-
   it("evaluates a Pass", async () => {
-    const { scopeFunction, invoke } = await loadFixture(setup);
+    const { scopeFunction, invoke } = await loadFixture(setupOneParamStatic);
 
     await scopeFunction([
       {
