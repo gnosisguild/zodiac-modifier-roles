4cd4eb1eb7f82ad8733aa27e41734267b836080c
cristovaoth <cristovao.honorato@gmail.com>
2023-03-26 22:31:47 +0100

    ArrayEvery Operator, dedicated tests

--- packages/evm/test/operators/07ArrayEvery.spec.ts
+++ packages/evm/test/operators/07ArrayEvery.spec.ts
@@ -6,8 +6,8 @@ import { defaultAbiCoder } from "ethers/lib/utils";
 import { setupOneParamArrayOfStaticTuple } from "./setup";
 import { Operator, ParameterType } from "../utils";
 
-describe("Operator - ArraySome", async () => {
-  it("can't set more than one child rules for ArraySome", async () => {
+describe("Operator - ArrayEvery", async () => {
+  it("can't set up more than one child rule for ArrayEvery", async () => {
     const { scopeFunction } = await loadFixture(
       setupOneParamArrayOfStaticTuple
     );
@@ -23,7 +23,7 @@ describe("Operator - ArraySome", async () => {
         {
           parent: 0,
           paramType: ParameterType.Array,
-          operator: Operator.ArraySome,
+          operator: Operator.ArrayEvery,
           compValue: "0x",
         },
         {
@@ -66,7 +66,7 @@ describe("Operator - ArraySome", async () => {
     ).to.be.reverted;
   });
 
-  it("evaluates an ArraySome condition", async () => {
+  it("evaluates an ArrayEvery condition", async () => {
     const { roles, invoke, scopeFunction } = await loadFixture(
       setupOneParamArrayOfStaticTuple
     );
@@ -81,7 +81,7 @@ describe("Operator - ArraySome", async () => {
       {
         parent: 0,
         paramType: ParameterType.Array,
-        operator: Operator.ArraySome,
+        operator: Operator.ArrayEvery,
         compValue: "0x",
       },
       {
@@ -93,8 +93,8 @@ describe("Operator - ArraySome", async () => {
       {
         parent: 2,
         paramType: ParameterType.Static,
-        operator: Operator.EqualTo,
-        compValue: defaultAbiCoder.encode(["uint256"], [1234]),
+        operator: Operator.LessThan,
+        compValue: defaultAbiCoder.encode(["uint256"], [1000]),
       },
       {
         parent: 2,
@@ -104,19 +104,28 @@ describe("Operator - ArraySome", async () => {
       },
     ]);
 
-    await expect(invoke([])).to.be.reverted;
-    await expect(invoke([{ a: 1234, b: true }])).to.not.be.reverted;
+    await expect(invoke([])).to.not.be.reverted;
+    await expect(invoke([{ a: 999, b: true }])).to.not.be.reverted;
     await expect(
       invoke([
-        { a: 1234, b: true },
-        { a: 1234, b: false },
+        { a: 999, b: true },
+        { a: 100, b: true },
       ])
     ).to.not.be.reverted;
 
-    await expect(invoke([{ a: 1234, b: false }])).to.be.revertedWithCustomError(
-      roles,
-      "NoArrayElementPasses"
-    );
+    await expect(
+      invoke([
+        { a: 999, b: false },
+        { a: 100, b: true },
+      ])
+    ).to.be.revertedWithCustomError(roles, "NotEveryArrayElementPasses");
+
+    await expect(
+      invoke([
+        { a: 999, b: true },
+        { a: 1000, b: true },
+      ])
+    ).to.be.revertedWithCustomError(roles, "NotEveryArrayElementPasses");
   });
 
   it.skip("Tracks the resulting trace");
