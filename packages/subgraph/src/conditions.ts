cca37f79bd60752257f2cf7044abd209b0846b94
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-03-31 17:22:39 +0200

    fix subgraph compile issues

--- packages/subgraph/src/conditions.ts
+++ packages/subgraph/src/conditions.ts
@@ -1,9 +1,9 @@
-import { Address, Bytes, crypto } from "@graphprotocol/graph-ts"
+import { Address, ByteArray, Bytes, crypto } from "@graphprotocol/graph-ts"
 import { ScopeFunctionConditionsStruct } from "../generated/PermissionBuilder/PermissionBuilder"
 import { Condition } from "../generated/schema"
-import { Operator, ParameterType } from "./enums"
+import { Operator, OperatorKeys, ParameterType, ParameterTypeKeys } from "./enums"
 
-export const storeConditions = (conditions: ScopeFunctionConditionsStruct[]) => {
+export const storeConditions = (conditions: ScopeFunctionConditionsStruct[]): Condition => {
   assert(conditions.length > 0, "Conditions array is empty")
 
   const rootId = getRootConditionId(conditions)
@@ -14,15 +14,15 @@ export const storeConditions = (conditions: ScopeFunctionConditionsStruct[]) => 
   }
 
   for (let i = 0; i < conditions.length; i++) {
-    const id = i == 0 ? rootId : rootId + "-" + i
-    const parentId = i == 0 ? null : rootId + "-" + (conditions[i].parent - 1)
-    assert(i == 0 || parentId != null, "Parent missing")
+    const id = i == 0 ? rootId : rootId + "-" + i.toString()
+    const parentId: string | null = i == 0 ? null : rootId + "-" + (conditions[i].parent - 1).toString()
+    assert(i == 0 || parentId != "", "Parent missing")
 
     const condition = new Condition(id)
     condition.index = i
     condition.parent = parentId
-    condition.operator = Operator[conditions[i].operator]
-    condition.paramType = ParameterType[conditions[i].paramType]
+    condition.operator = OperatorKeys[conditions[i].operator]
+    condition.paramType = ParameterTypeKeys[conditions[i].paramType]
     condition.compValue = conditions[i].compValue
     condition.save()
 
@@ -38,15 +38,17 @@ export const storeConditions = (conditions: ScopeFunctionConditionsStruct[]) => 
   return rootCondition
 }
 
-export const ERC2470_SINGLETON_FACTORY_ADDRESS = Address.fromHexString("0xce0042b868300000d44a59004da54a005ffdcf9f")
-export const CREATE2_SALT = new Bytes(32).fill(0)
+export const ERC2470_SINGLETON_FACTORY_ADDRESS = Address.fromString("0xce0042b868300000d44a59004da54a005ffdcf9f")
+export const CREATE2_SALT = Bytes.fromUint8Array(new Uint8Array(32).fill(0))
 
-export const getRootConditionId = (conditions: ScopeFunctionConditionsStruct[]) => {
+export function getRootConditionId(conditions: ScopeFunctionConditionsStruct[]): string {
   const packed = conditions
-    .map((condition) => packCondition(condition))
+    .map<Bytes>((condition) => packCondition(condition))
     .reduce((acc, item) => acc.concat(item), new Bytes(0))
     .concat(
-      conditions.map((condition) => packCompValue(condition)).reduce((acc, item) => acc.concat(item), new Bytes(0)),
+      conditions
+        .map<Bytes>((condition) => packCompValue(condition))
+        .reduce((acc, item) => acc.concat(item), new Bytes(0)),
     )
 
   const initCode = creationCodeFor(packed)
@@ -60,23 +62,30 @@ const offsetParent = 8
 const offsetParamType = 5
 const offsetOperator = 0
 
-const packCondition = (condition: ScopeFunctionConditionsStruct) =>
-  Bytes.fromI32(
+function packCondition(condition: ScopeFunctionConditionsStruct): Bytes {
+  return Bytes.fromI32(
     (condition.parent << offsetParent) |
       (condition.paramType << offsetParamType) |
       (condition.operator << offsetOperator),
   )
+}
 
-const packCompValue = (condition: ScopeFunctionConditionsStruct) =>
-  condition.operator == Operator.EqualTo ? crypto.keccak256(condition.compValue) : bytes32(condition.compValue)
+function packCompValue(condition: ScopeFunctionConditionsStruct): Bytes {
+  return condition.operator == Operator.EqualTo
+    ? Bytes.fromByteArray(crypto.keccak256(condition.compValue))
+    : bytes32(condition.compValue)
+}
 
-const bytes32 = (value: Bytes) => Bytes.fromUint8Array(value.slice(0, 32))
+function bytes32(value: Bytes): Bytes {
+  return Bytes.fromUint8Array(value.slice(0, 32))
+}
 
-const creationCodeFor = (bytecode: Bytes) =>
-  Bytes.fromHexString("0x63")
+function creationCodeFor(bytecode: Bytes): Bytes {
+  return Bytes.fromHexString("0x63")
     .concat(Bytes.fromI32(bytecode.length + 1))
     .concat(Bytes.fromHexString("0x80600E6000396000F300"))
     .concat(bytecode)
+}
 
 /**
  * Generates an address for a contract created using CREATE2.
@@ -85,11 +94,18 @@ const creationCodeFor = (bytecode: Bytes) =>
  * @param salt A salt
  * @param initCode The init code of the contract being created
  */
-export const generateAddress2 = function (from: Address, salt: Bytes, initCode: Bytes): Address {
+export function generateAddress2(from: Address, salt: Bytes, initCode: Bytes): Address {
   assert(salt.length == 32)
-  return Address.fromUint8Array(
-    crypto
-      .keccak256(Bytes.fromHexString("0xff").concat(from).concat(salt).concat(crypto.keccak256(initCode)))
-      .slice(-20),
+  return Address.fromBytes(
+    Bytes.fromUint8Array(
+      crypto
+        .keccak256(
+          Bytes.fromHexString("0xff")
+            .concat(from)
+            .concat(salt)
+            .concat(Bytes.fromByteArray(crypto.keccak256(initCode))),
+        )
+        .slice(-20),
+    ),
   )
 }
