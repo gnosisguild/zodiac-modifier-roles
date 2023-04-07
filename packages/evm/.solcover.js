6770bb9a602449f84f4e224ee545181ffe4fb2fe
cristovaoth <cristovao.honorato@gmail.com>
2023-04-07 02:47:04 +0100

    update config file

--- packages/evm/.solcover.js
+++ packages/evm/.solcover.js
@@ -3,7 +3,6 @@ module.exports = {
     "test/*",
     "test/MockConsumptions.sol",
     "test/MockDecoder.sol",
-    "test/MockScopeConfig.sol",
     "test/MockTopology.sol",
     "test/MultiSend.sol",
     "test/TestAvatar.sol",
@@ -11,7 +10,6 @@ module.exports = {
     "test/TestCustomChecker.sol",
     "test/TestEncoder.sol",
     "test/TestFactory.sol",
-    "test/TestGas.sol",
   ],
   mocha: {
     grep: "@skip-on-coverage", // Find everything with this tag
