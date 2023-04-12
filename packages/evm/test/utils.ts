7503234f043b0905a679bdf0d4f4eb8798c0cc85
cristovaoth <cristovao.honorato@gmail.com>
2023-04-12 12:47:49 +0300

    Removing deprecated helpers

--- packages/evm/test/utils.ts
+++ packages/evm/test/utils.ts
@@ -38,83 +38,6 @@ export interface SafeTransaction extends MetaTransaction {
   nonce: string | number;
 }
 
-const encodeMetaTransaction = (tx: MetaTransaction): string => {
-  const data = utils.arrayify(tx.data);
-  const encoded = utils.solidityPack(
-    ["uint8", "address", "uint256", "uint256", "bytes"],
-    [tx.operation, tx.to, tx.value, data.length, data]
-  );
-  return encoded.slice(2);
-};
-
-export const encodeMultiSend = (txs: MetaTransaction[]): string => {
-  return "0x" + txs.map((tx) => encodeMetaTransaction(tx)).join("");
-};
-
-export const buildMultiSendSafeTx = (
-  multiSend: Contract,
-  txs: MetaTransaction[],
-  nonce: number,
-  overrides?: Partial<SafeTransaction>
-): SafeTransaction => {
-  return buildContractCall(
-    multiSend,
-    "multiSend",
-    [encodeMultiSend(txs)],
-    nonce,
-    true,
-    overrides
-  );
-};
-
-export const buildContractCall = (
-  contract: Contract,
-  method: string,
-  params: any[],
-  nonce: number,
-  delegateCall?: boolean,
-  overrides?: Partial<SafeTransaction>
-): SafeTransaction => {
-  const data = contract.interface.encodeFunctionData(method, params);
-  return buildSafeTransaction(
-    Object.assign(
-      {
-        to: contract.address,
-        data,
-        operation: delegateCall ? 1 : 0,
-        nonce,
-      },
-      overrides
-    )
-  );
-};
-
-export const buildSafeTransaction = (template: {
-  to: string;
-  value?: BigNumber | number | string;
-  data?: string;
-  operation?: number;
-  safeTxGas?: number | string;
-  baseGas?: number | string;
-  gasPrice?: number | string;
-  gasToken?: string;
-  refundReceiver?: string;
-  nonce: number;
-}): SafeTransaction => {
-  return {
-    to: template.to,
-    value: template.value || 0,
-    data: template.data || "0x",
-    operation: template.operation || 0,
-    safeTxGas: template.safeTxGas || 0,
-    baseGas: template.baseGas || 0,
-    gasPrice: template.gasPrice || 0,
-    gasToken: template.gasToken || AddressZero,
-    refundReceiver: template.refundReceiver || AddressZero,
-    nonce: template.nonce,
-  };
-};
-
 export enum ParameterType {
   None = 0,
   Static,
@@ -257,7 +180,7 @@ export async function deployRolesMod(
   return modifier;
 }
 
-export const encodeMultisend = (txs: MetaTransaction[]): string => {
+export const encodeMultisendPayload = (txs: MetaTransaction[]): string => {
   return (
     "0x" +
     txs
