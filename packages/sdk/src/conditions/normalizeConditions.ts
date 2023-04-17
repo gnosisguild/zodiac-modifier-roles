0558d01161e7a16ce692f57e9f9d85feae8d6603
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-04-17 16:13:46 +0200

    improve conditions merging

--- packages/sdk/src/conditions/normalizeConditions.ts
+++ packages/sdk/src/conditions/normalizeConditions.ts
@@ -3,7 +3,8 @@ import { Condition } from "../types"
 // TODO - this is a placeholder for the normalizeCondition function, it should:
 // - flatten nested AND conditions
 // - flatten nested OR conditions
-// - remove redundant conditions (empty AND/OR, trailing PASS conditions)
+// - remove redundant conditions (empty AND/OR, trailing PASS conditions?)
+// - prune equal branches in logical expressions
 // - enforce a canonical order for children (e.g. AND/OR conditions should be sorted by their hash)
 // - maybe: bool formula minimization, for example move OR conditions as far down as possible, e.g.: or(and(a, b), and(a, c)) -> and(a, or(b, c))
 export const normalizeCondition = (condition: Condition) => {
