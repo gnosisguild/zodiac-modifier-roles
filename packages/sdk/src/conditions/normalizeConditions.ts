b16b78dfaf6167e7f8820d30256d491253e765aa
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-04-18 17:06:28 +0200

    condition normalization

--- packages/sdk/src/conditions/normalizeConditions.ts
+++ packages/sdk/src/conditions/normalizeConditions.ts
@@ -1,14 +1,19 @@
+import { BigNumber } from "ethers"
+
 import { Condition, Operator } from "../types"
 
-// TODO - this is a placeholder for the normalizeCondition function, it should:
-// - flatten nested AND/OR conditions
-// - remove redundant conditions (single child AND/OR, trailing PASS conditions?)
-// - prune equal branches in logical expressions
-// - enforce a canonical order for children (e.g. AND/OR conditions should be sorted by their hash)
-// - maybe: bool formula minimization, for example move OR conditions as far down as possible, e.g.: or(and(a, b), and(a, c)) -> and(a, or(b, c))
-export const normalizeCondition = (condition: Condition) => {
-  let result = condition
+import { getConditionId } from "./getConditionId"
+
+// maybe add bool formula minimization, for example move OR conditions as far down as possible, e.g.: or(and(a, b), and(a, c)) -> and(a, or(b, c))
+export const normalizeCondition = (condition: Condition): Condition => {
+  const normalizedChildren = condition.children?.map(normalizeCondition)
+  let result: Condition = normalizedChildren
+    ? { ...condition, children: normalizedChildren }
+    : condition
   result = flattenNestedLogicalConditions(result)
+  result = dedupeBranches(result)
+  result = unwrapSingleBranches(result)
+  result = normalizeChildrenOrder(result)
   return result
 }
 
@@ -20,13 +25,72 @@ const flattenNestedLogicalConditions = (condition: Condition): Condition => {
     condition.operator === Operator.And ||
     condition.operator === Operator.Or
   ) {
+    const flattenedChildren = (children || []).flatMap((child) =>
+      child.operator === condition.operator ? child.children || [] : [child]
+    )
     return {
       ...condition,
-      children: (children || []).flatMap((child) =>
-        child.operator === condition.operator ? child.children || [] : [child]
-      ),
+      children: flattenedChildren,
     }
   }
 
   return children ? { ...condition, children } : condition
+}
+
+/** remove duplicate child branches in AND/OR/NOR */
+const dedupeBranches = (condition: Condition): Condition => {
+  if (
+    condition.operator === Operator.And ||
+    condition.operator === Operator.Or ||
+    condition.operator === Operator.Nor
+  ) {
+    const childIds = new Set()
+    const uniqueChildren = condition.children?.filter((child) => {
+      const childId = getConditionId(child, true)
+      const isDuplicate = !childIds.has(childId)
+      childIds.add(childId)
+      return isDuplicate
+    })
+
+    return { ...condition, children: uniqueChildren }
+  }
+
+  return condition
+}
+
+/** remove AND/OR wrapping if they have only a single child */
+const unwrapSingleBranches = (condition: Condition): Condition => {
+  if (
+    condition.operator === Operator.And ||
+    condition.operator === Operator.Or
+  ) {
+    return condition.children?.length === 1 ? condition.children[0] : condition
+  }
+
+  return condition
+}
+
+/** enforce a canonical order of AND/OR/NOR branches */
+const normalizeChildrenOrder = (condition: Condition): Condition => {
+  if (
+    condition.operator === Operator.And ||
+    condition.operator === Operator.Or ||
+    condition.operator === Operator.Nor
+  ) {
+    if (!condition.children) return condition
+
+    const pairs = condition.children.map(
+      (child) => [BigNumber.from(getConditionId(child, true)), child] as const
+    )
+    // sort is in-place
+    pairs.sort(([a], [b]) => (a.lt(b) ? -1 : 1))
+    const orderedChildren = pairs.map(([, child]) => child)
+
+    return {
+      ...condition,
+      children: orderedChildren,
+    }
+  }
+
+  return condition
 }
