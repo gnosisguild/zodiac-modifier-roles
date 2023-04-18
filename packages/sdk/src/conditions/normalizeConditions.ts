31eab6a7cb983c0ed3c6ac76abde7a546b0a596e
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-04-18 15:00:09 +0200

    flatten nested and/or

--- packages/sdk/src/conditions/normalizeConditions.ts
+++ packages/sdk/src/conditions/normalizeConditions.ts
@@ -1,12 +1,32 @@
-import { Condition } from "../types"
+import { Condition, Operator } from "../types"
 
 // TODO - this is a placeholder for the normalizeCondition function, it should:
-// - flatten nested AND conditions
-// - flatten nested OR conditions
+// - flatten nested AND/OR conditions
 // - remove redundant conditions (single child AND/OR, trailing PASS conditions?)
 // - prune equal branches in logical expressions
 // - enforce a canonical order for children (e.g. AND/OR conditions should be sorted by their hash)
 // - maybe: bool formula minimization, for example move OR conditions as far down as possible, e.g.: or(and(a, b), and(a, c)) -> and(a, or(b, c))
 export const normalizeCondition = (condition: Condition) => {
-  return condition
+  let result = condition
+  result = flattenNestedLogicalConditions(result)
+  return result
+}
+
+/** flatten nested AND/OR conditions */
+const flattenNestedLogicalConditions = (condition: Condition): Condition => {
+  const children = condition.children?.map(flattenNestedLogicalConditions)
+
+  if (
+    condition.operator === Operator.And ||
+    condition.operator === Operator.Or
+  ) {
+    return {
+      ...condition,
+      children: (children || []).flatMap((child) =>
+        child.operator === condition.operator ? child.children || [] : [child]
+      ),
+    }
+  }
+
+  return children ? { ...condition, children } : condition
 }
