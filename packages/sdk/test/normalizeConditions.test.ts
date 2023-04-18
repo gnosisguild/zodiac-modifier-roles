e6bbbb5339f436efa41b7e9bba6525d627f3fc08
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-04-18 14:59:59 +0200

    fix test watcher

--- packages/sdk/test/normalizeConditions.test.ts
+++ packages/sdk/test/normalizeConditions.test.ts
@@ -1,7 +1,14 @@
 import { expect } from "chai"
+import { defaultAbiCoder } from "ethers/lib/utils"
 
 import { normalizeCondition } from "../src/conditions/normalizeConditions"
-import { Operator, ParameterType } from "../src/types"
+import { Condition, Operator, ParameterType } from "../src/types"
+
+const DUMMY_COMP = (id: number): Condition => ({
+  paramType: ParameterType.Static,
+  operator: Operator.Custom,
+  compValue: defaultAbiCoder.encode(["uint256"], [id]),
+})
 
 describe.only("normalizeConditions()", () => {
   it("should flatten nested AND conditions", () => {
@@ -14,18 +21,48 @@ describe.only("normalizeConditions()", () => {
             paramType: ParameterType.None,
             operator: Operator.And,
             children: [
-              { paramType: ParameterType.None, operator: Operator.Pass },
+              DUMMY_COMP(0),
+              {
+                paramType: ParameterType.None,
+                operator: Operator.And,
+                children: [DUMMY_COMP(1), DUMMY_COMP(2)],
+              },
             ],
           },
-          { paramType: ParameterType.None, operator: Operator.Pass },
+          DUMMY_COMP(3),
+        ],
+      })
+    ).to.deep.equal({
+      paramType: ParameterType.None,
+      operator: Operator.And,
+      children: [DUMMY_COMP(0), DUMMY_COMP(1), DUMMY_COMP(2), DUMMY_COMP(3)],
+    })
+  })
+
+  it("should not flatten ORs in ANDs", () => {
+    expect(
+      normalizeCondition({
+        paramType: ParameterType.None,
+        operator: Operator.And,
+        children: [
+          {
+            paramType: ParameterType.None,
+            operator: Operator.Or,
+            children: [DUMMY_COMP(0), DUMMY_COMP(1)],
+          },
+          DUMMY_COMP(3),
         ],
       })
     ).to.deep.equal({
       paramType: ParameterType.None,
       operator: Operator.And,
       children: [
-        { paramType: ParameterType.None, operator: Operator.Pass },
-        { paramType: ParameterType.None, operator: Operator.Pass },
+        {
+          paramType: ParameterType.None,
+          operator: Operator.Or,
+          children: [DUMMY_COMP(0), DUMMY_COMP(1)],
+        },
+        DUMMY_COMP(3),
       ],
     })
   })
