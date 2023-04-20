f9430e80267d6a1ac9f2beabde93e369e1e68a26
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-04-20 17:52:59 +0200

    improve condition type safety

--- packages/sdk/src/presets/authoring/conditions/comparison.ts
+++ packages/sdk/src/presets/authoring/conditions/comparison.ts
@@ -1,61 +1,69 @@
+import { BigNumberish } from "ethers"
 import { ParamType } from "ethers/lib/utils"
 
 import { Operator, ParameterType } from "../../../types"
 import { Placeholder } from "../../types"
 
+import { ConditionFunction } from "./types"
 import { describeStructure, parameterType, encodeValue } from "./utils"
 
 /**
  * Asserts that the value from calldata is equal to the given value
  * @param value The reference value to encode or a placeholder
  */
-export const eq = (value: Placeholder<any> | any) => (abiType: ParamType) => {
-  const type = ParamType.from(abiType)
-  const structure = describeStructure(type)
-  return {
-    paramType: parameterType(type),
-    operator: Operator.EqualTo,
-    compValue: encodeValue(value, type),
-    children: structure.children,
+export const eq =
+  (value: Placeholder<any> | any): ConditionFunction<any> =>
+  (abiType: ParamType) => {
+    const type = ParamType.from(abiType)
+    const structure = describeStructure(type)
+    return {
+      paramType: parameterType(type),
+      operator: Operator.EqualTo,
+      compValue: encodeValue(value, type),
+      children: structure.children,
+    }
   }
-}
 
 /**
  * Asserts that the value from calldata is greater than the given value
  * @param value The reference value to encode or a placeholder
  */
-export const gt = (value: Placeholder<any> | any) => (abiType: ParamType) => {
-  const type = ParamType.from(abiType)
-  if (!type.type.startsWith("uint") || !type.type.startsWith("int")) {
-    console.warn(
-      `Using a gt condition on non-numeric type ${type.type}, bytes will be interpreted as a uint256`
-    )
+export const gt =
+  (
+    value: Placeholder<BigNumberish> | BigNumberish
+  ): ConditionFunction<BigNumberish> =>
+  (abiType: ParamType) => {
+    const type = ParamType.from(abiType)
+    if (!type.type.startsWith("uint") || !type.type.startsWith("int")) {
+      throw new Error("`gt` is only supported for uint and int params")
+    }
+    return {
+      paramType: ParameterType.Static,
+      operator: type.type.startsWith("uint")
+        ? Operator.GreaterThan
+        : Operator.SignedIntGreaterThan,
+      compValue: encodeValue(value, abiType),
+    }
   }
-  return {
-    paramType: ParameterType.Static,
-    operator: type.type.startsWith("uint")
-      ? Operator.GreaterThan
-      : Operator.SignedIntGreaterThan,
-    compValue: encodeValue(value, abiType),
-  }
-}
 
 /**
  * Asserts that the value from calldata is greater than the given value
  * @param value The reference value to encode or a placeholder
  */
-export const lt = (value: Placeholder<any> | any) => (abiType: ParamType) => {
-  const type = ParamType.from(abiType)
-  if (!type.type.startsWith("uint") || !type.type.startsWith("int")) {
-    console.warn(
-      `Using a lt condition on non-numeric type ${type.type}, bytes will be interpreted as a uint256`
-    )
+export const lt =
+  (
+    value: Placeholder<BigNumberish> | BigNumberish
+  ): ConditionFunction<BigNumberish> =>
+  (abiType: ParamType) => {
+    const type = ParamType.from(abiType)
+    if (!type.type.startsWith("uint") || !type.type.startsWith("int")) {
+      throw new Error("`lt` is only supported for uint and int params")
+    }
+    return {
+      paramType: ParameterType.Static,
+      operator: type.type.startsWith("uint")
+        ? Operator.LessThan
+        : Operator.SignedIntLessThan,
+      compValue: encodeValue(value, abiType),
+    }
   }
-  return {
-    paramType: ParameterType.Static,
-    operator: type.type.startsWith("uint")
-      ? Operator.LessThan
-      : Operator.SignedIntLessThan,
-    compValue: encodeValue(value, abiType),
-  }
-}
--- packages/sdk/test/typings.ts
+++ packages/sdk/test/typings.ts
@@ -1,5 +1,6 @@
 import { BigNumberish } from "ethers"
 
+import * as c from "../src/presets/authoring/conditions"
 import { Scoping } from "../src/presets/authoring/conditions/types"
 
 // These are tests of the typing system, not the runtime behavior.
@@ -16,3 +17,6 @@ const _t03: Scoping<any[]> = {}
 
 // @ts-expect-error - It should not be allowed to use promises as a scopings
 const _t04: Scoping<{ a: Promise<string> }> = { a: Promise.resolve("a") }
+
+// @ts-expect-error - It should only be allowed to use gt on BigNumberish scopings
+const _t05: Scoping<string[]> = c.gt(0)
