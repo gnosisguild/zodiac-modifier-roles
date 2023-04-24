33b55c12e82c16a7513f74c73e012891e20bd5e1
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-04-24 18:31:02 +0200

    don't implement keys encoding
    we'll just use formatBytes32String / parseBytes32String

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,7 +1,6 @@
 export * from "./conditions"
 export * from "./presets"
 
-export * from "./keys"
 export { fetchRole } from "./fetchRole"
 export { applyMembers } from "./applyMembers"
 export { applyPermissions } from "./applyPermissions"
