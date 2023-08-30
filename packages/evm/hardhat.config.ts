9900c5a27cc81cad0659d71c25b874db4b4176a2
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-08-30 12:54:26 +0200

    adding more networks to hardhat config

--- packages/evm/hardhat.config.ts
+++ packages/evm/hardhat.config.ts
@@ -34,7 +34,10 @@ if (PK) {
   };
 }
 
-if (["goerli", "mainnet"].includes(argv.network) && INFURA_KEY === undefined) {
+if (
+  ["goerli", "sepolia", "mainnet"].includes(argv.network) &&
+  INFURA_KEY === undefined
+) {
   throw new Error(
     `Could not find Infura key in env, unable to connect to network ${argv.network}`
   );
@@ -71,7 +74,11 @@ const config: HardhatUserConfig = {
       ...sharedNetworkConfig,
       url: `https://goerli.infura.io/v3/${INFURA_KEY}`,
     },
-    xdai: {
+    sepolia: {
+      ...sharedNetworkConfig,
+      url: `https://sepolia.infura.io/v3/${INFURA_KEY}`,
+    },
+    gnosis: {
       ...sharedNetworkConfig,
       url: "https://rpc.gnosischain.com/",
     },
