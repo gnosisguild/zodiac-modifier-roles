a6c9b63863d6fead5d85f66d2f7002cbdc78d00a
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-08-30 14:53:20 +0200

    upgrade solidity

--- packages/evm/hardhat.config.ts
+++ packages/evm/hardhat.config.ts
@@ -49,7 +49,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.17",
+        version: "0.8.21",
         settings: {
           optimizer: {
             enabled: true,
