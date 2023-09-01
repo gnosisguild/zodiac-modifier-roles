3fff5b6c136ff896ee74436368619194056ff4c1
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-09-01 10:55:31 +0200

    update subgraph urls

--- packages/sdk/src/subgraph.ts
+++ packages/sdk/src/subgraph.ts
@@ -1,16 +1,12 @@
 const SUBGRAPH = {
-  [1]: "https://api.thegraph.com/subgraphs/name/samepant/zodiac-roles-mod-mainnet",
-  [5]: "https://api.thegraph.com/subgraphs/name/samepant/zodiac-roles-mod-goerli",
-  // [10]: "https://api.thegraph.com/subgraphs/name/samepant/zodiac-roles-mod-optimism",
-  [56]: "https://api.thegraph.com/subgraphs/name/samepant/zodiac-roles-mod-bsc",
-  [100]:
-    "https://api.thegraph.com/subgraphs/name/samepant/zodiac-roles-mod-gnosis-chain",
-  [137]:
-    "https://api.thegraph.com/subgraphs/name/samepant/zodiac-roles-mod-polygon",
-  [42161]:
-    "https://api.thegraph.com/subgraphs/name/samepant/zodiac-roles-mod-arbitrum",
-  // [43114]:
-  //   "https://api.thegraph.com/subgraphs/name/samepant/zodiac-roles-mod-avalanche",
+  [1]: "",
+  [5]: "https://api.studio.thegraph.com/query/23167/zodiac-roles-goerli/version/latest",
+  [10]: "",
+  [56]: "",
+  [100]: "",
+  [137]: "",
+  [42161]: "",
+  [43114]: "",
 }
 
 export default SUBGRAPH
