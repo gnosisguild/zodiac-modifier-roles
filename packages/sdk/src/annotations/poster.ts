470eda8630f805d178e6c69573a6b8389bad628b
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-09-06 15:25:49 +0200

    export poster abi

--- packages/sdk/src/annotations/poster.ts
+++ packages/sdk/src/annotations/poster.ts
@@ -1,20 +1,11 @@
 import { Interface } from "@ethersproject/abi"
 
+import { posterAbi } from "../abi"
+
 // EIP-3722 Poster contract
 export const POSTER_ADDRESS = "0x000000000000cd17345801aa8147b8D3950260FF"
 
-const posterInterface = new Interface([
-  {
-    inputs: [
-      { internalType: "string", name: "content", type: "string" },
-      { internalType: "string", name: "tag", type: "string" },
-    ],
-    name: "post",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-])
+const posterInterface = new Interface(posterAbi)
 
 export const encodePost = (content: string, tag: string) => {
   return posterInterface.encodeFunctionData("post", [
