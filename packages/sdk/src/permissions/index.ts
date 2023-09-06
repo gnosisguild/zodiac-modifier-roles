3492a80ea7755e4e237808a20e3ad5c88497d73c
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-09-06 14:05:15 +0200

    add type export

--- packages/sdk/src/permissions/index.ts
+++ packages/sdk/src/permissions/index.ts
@@ -2,4 +2,4 @@ export * from "./authoring"
 export { processPermissions } from "./processPermissions"
 export { reconstructPermissions } from "./reconstructPermissions"
 
-export type { Permission } from "./types"
+export type { Permission, PermissionSet } from "./types"
