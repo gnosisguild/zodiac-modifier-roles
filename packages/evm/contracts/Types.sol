e6c2beb527ba742efea1ad90c80b1a1b38bc6b11
Auryn Macmillan <hi@auryn.xyz>
2023-09-18 10:41:13 +1000

    feat: natspec on Allowance struct

--- packages/evm/contracts/Types.sol
+++ packages/evm/contracts/Types.sol
@@ -7,6 +7,7 @@ pragma solidity >=0.8.17 <0.9.0;
  * @author Cristóvão Honorato - <cristovao.honorato@gnosis.io>
  * @author Jan-Felix Schwarz  - <jan-felix.schwarz@gnosis.io>
  */
+
 enum ParameterType {
     None,
     Static,
@@ -122,29 +123,18 @@ struct Role {
     mapping(bytes32 => bytes32) scopeConfig;
 }
 
+/// @notice The order of members in the `Allowance` struct is significant; members updated during accrual (`balance` and `timestamp`) should be stored in the same word.
+/// @custom:member refill Amount added to balance after each period elapses.
+/// @custom:member maxBalance Maximum allowable accumulated allowance. Refill stops when unused allowance reaches this value.
+/// @custom:member perdio Duration, in seconds, before a refill occurs. If set to 0, the allowance is for one-time use and won't be replenished.
+/// @custom:member balance Remaining allowance available for use. Decreases with usage and increases after each refill by the specified refill amount.
+/// @custom:member timestamp Timestamp when the last refill occurred.
 struct Allowance {
-    /*
-     * period:     Duration, in seconds, before a refill occurs. If set to 0, the allowance is
-     *             for one-time use and won't be replenished.
-     *
-     * refill:     Amount added to balance after each period elapses.
-     *
-     * timestamp:  Records the moment when the last refill occurred.
-     *
-     * maxBalance: Maximum allowable accumulated allowance. Refill stops when unused allowance
-     *             reaches this value.
-     *
-     * balance:    Remaining, unused allowance available for use. It decreases with usage and
-     *             increases after each refill according to the specified refill amount.
-     */
-
-    // The order is significant; fields updated during accrual are on the second word.
-    uint128 refill; // Amount added per accrual.
-    uint128 maxBalance; // Maximum allowable accumulated amount.
-    uint64 period; // Duration before a refill occurs.
-    // These two fields are the only ones updated during accrual and should reside in the same word.
-    uint128 balance; // Remaining unused amount.
-    uint64 timestamp; // Timestamp of the last refilled period.
+    uint128 refill;
+    uint128 maxBalance;
+    uint64 period;
+    uint128 balance;
+    uint64 timestamp;
 }
 
 struct Consumption {
