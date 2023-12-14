fbb6f7a992cfae50badafc71ec0c8942e992461b
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-12-14 14:34:54 +0100

    fix typing issue

--- packages/sdk/src/subgraph.ts
+++ packages/sdk/src/subgraph.ts
@@ -1,14 +1,14 @@
 const SUBGRAPH = {
   [1]: "https://api.studio.thegraph.com/query/23167/zodiac-roles-mainnet/v2.1.0",
   [5]: "https://api.studio.thegraph.com/query/23167/zodiac-roles-goerli/v2.1.0",
-  [10]: "",
-  [56]: "",
+  // [10]: "",
+  // [56]: "",
   [100]:
     "https://api.studio.thegraph.com/query/23167/zodiac-roles-gnosis/v2.1.0",
   [137]:
     "https://api.studio.thegraph.com/query/23167/zodiac-roles-polygon/v2.1.0",
-  [42161]: "",
-  [43114]: "",
+  // [42161]: "",
+  // [43114]: "",
   [80001]:
     "https://api.studio.thegraph.com/query/23167/zodiac-roles-mumbai/v2.1.0",
   [11155111]:
