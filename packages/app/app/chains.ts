fabd6847d5f8c32e35066ca297b6313419a534a9
Jan-Felix <jan-felix.schwarz@gnosis.io>
2023-12-14 14:01:19 +0100

    update chains in app

--- packages/app/app/chains.ts
+++ packages/app/app/chains.ts
@@ -1,16 +1,15 @@
 import {
   mainnet,
   goerli,
-  // sepolia,
-  optimism,
-  avalanche,
-  arbitrum,
-  bsc,
+  sepolia,
+  // optimism,
+  // avalanche,
+  // arbitrum,
+  // bsc,
   polygon,
-  // polygonMumbai,
+  polygonMumbai,
   gnosis,
 } from "@wagmi/core/chains" // cannot import from wagmi/chains because that one declares "use client;"
-import { polygonMumbai } from "viem/chains"
 
 export const CHAINS = {
   [mainnet.id]: {
@@ -25,36 +24,36 @@ export const CHAINS = {
     blockExplorerAbiUrl: "https://api.gnosisscan.io/api",
     blockExplorerApiKey: "8ENCUFT4D3XVJS7N9ZFS5Z9XQPNUGRKSN5",
   },
-  [optimism.id]: {
-    ...optimism,
-    prefix: "oeth",
-    blockExplorerAbiUrl: "https://api-optimistic.etherscan.io/api",
-    blockExplorerApiKey: "6RJ8KT4B1S9V7E3CIYECNY7HFW8IPWQ3C4",
-  },
+  // [optimism.id]: {
+  //   ...optimism,
+  //   prefix: "oeth",
+  //   blockExplorerAbiUrl: "https://api-optimistic.etherscan.io/api",
+  //   blockExplorerApiKey: "6RJ8KT4B1S9V7E3CIYECNY7HFW8IPWQ3C4",
+  // },
   [polygon.id]: {
     ...polygon,
     prefix: "matic",
     blockExplorerAbiUrl: "https://api.polygonscan.com/api",
     blockExplorerApiKey: "NM937M1IZXVQ6QVDXS73XMF8JSAB677JWQ",
   },
-  [avalanche.id]: {
-    ...avalanche,
-    prefix: "avax",
-    blockExplorerAbiUrl: "https://api.snowtrace.io/api",
-    blockExplorerApiKey: "IAST9REKWMIW1QSE2M7K2IKKAZVNQPHC1U",
-  },
-  [arbitrum.id]: {
-    ...arbitrum,
-    prefix: "arb1",
-    blockExplorerAbiUrl: "https://api.arbiscan.io/api",
-    blockExplorerApiKey: "CSITWCYI9UDAJ7QS92FNVJ2XQP5B23P4J9",
-  },
-  [bsc.id]: {
-    ...bsc,
-    prefix: "bnb",
-    blockExplorerAbiUrl: "https://api.bscscan.com/api",
-    blockExplorerApiKey: "AMXEAU3N9P7RJHFSZ7KAJDRY5MFJ1N29D6",
-  },
+  // [avalanche.id]: {
+  //   ...avalanche,
+  //   prefix: "avax",
+  //   blockExplorerAbiUrl: "https://api.snowtrace.io/api",
+  //   blockExplorerApiKey: "IAST9REKWMIW1QSE2M7K2IKKAZVNQPHC1U",
+  // },
+  // [arbitrum.id]: {
+  //   ...arbitrum,
+  //   prefix: "arb1",
+  //   blockExplorerAbiUrl: "https://api.arbiscan.io/api",
+  //   blockExplorerApiKey: "CSITWCYI9UDAJ7QS92FNVJ2XQP5B23P4J9",
+  // },
+  // [bsc.id]: {
+  //   ...bsc,
+  //   prefix: "bnb",
+  //   blockExplorerAbiUrl: "https://api.bscscan.com/api",
+  //   blockExplorerApiKey: "AMXEAU3N9P7RJHFSZ7KAJDRY5MFJ1N29D6",
+  // },
 
   [goerli.id]: {
     ...goerli,
@@ -62,7 +61,12 @@ export const CHAINS = {
     blockExplorerAbiUrl: "https://api-goerli.etherscan.io/api",
     blockExplorerApiKey: "N53BKW6ABNX7CNUK8QIXGRAQS2NME92YAN",
   },
-  // [sepolia.id]: { ...sepolia, prefix: "sep", blockExplorerApiKey: '6RJ8KT4B1S9V7E3CIYECNY7HFW8IPWQ3C4' },
+  [sepolia.id]: {
+    ...sepolia,
+    prefix: "sep",
+    blockExplorerAbiUrl: "https://api-sepolia.etherscan.io/api",
+    blockExplorerApiKey: "6RJ8KT4B1S9V7E3CIYECNY7HFW8IPWQ3C4",
+  },
   [polygonMumbai.id]: {
     ...polygonMumbai,
     prefix: "maticmum",
