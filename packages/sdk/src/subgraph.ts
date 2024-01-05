50ca42f9ae1ef0783991483eacebdcf8f6a197f8
Jan-Felix <jan-felix.schwarz@gnosis.io>
2024-01-05 10:58:36 +0100

    sdk: upgrade subgraphs

--- packages/sdk/src/subgraph.ts
+++ packages/sdk/src/subgraph.ts
@@ -1,18 +1,18 @@
 const SUBGRAPH = {
-  [1]: "https://api.studio.thegraph.com/query/23167/zodiac-roles-mainnet/v2.1.0",
-  [5]: "https://api.studio.thegraph.com/query/23167/zodiac-roles-goerli/v2.1.0",
+  [1]: "https://api.studio.thegraph.com/query/23167/zodiac-roles-mainnet/v2.1.1",
+  [5]: "https://api.studio.thegraph.com/query/23167/zodiac-roles-goerli/v2.1.1",
   // [10]: "",
   // [56]: "",
   [100]:
-    "https://api.studio.thegraph.com/query/23167/zodiac-roles-gnosis/v2.1.0",
+    "https://api.studio.thegraph.com/query/23167/zodiac-roles-gnosis/v2.1.1",
   [137]:
-    "https://api.studio.thegraph.com/query/23167/zodiac-roles-polygon/v2.1.0",
+    "https://api.studio.thegraph.com/query/23167/zodiac-roles-polygon/v2.1.1",
   // [42161]: "",
   // [43114]: "",
   [80001]:
-    "https://api.studio.thegraph.com/query/23167/zodiac-roles-mumbai/v2.1.0",
+    "https://api.studio.thegraph.com/query/23167/zodiac-roles-mumbai/v2.1.1",
   [11155111]:
-    "https://api.studio.thegraph.com/query/23167/zodiac-roles-sepolia/v2.1.0",
+    "https://api.studio.thegraph.com/query/23167/zodiac-roles-sepolia/v2.1.1",
 }
 
 export default SUBGRAPH
