860d59b1b79feede55aaa89895ef3b3139b1a7d4
samepant <sampanter@gmail.com>
2024-02-21 13:45:16 -0500

    permissions view update

--- packages/app/app/permissions/[chain]/[hash]/page.tsx
+++ packages/app/app/permissions/[chain]/[hash]/page.tsx
@@ -1,7 +1,7 @@
 import { Annotation, Target } from "zodiac-roles-sdk"
 import { MdOutlinePolicy } from "react-icons/md"
 
-import styles from "./page.module.css"
+import classes from "./page.module.css"
 import { notFound } from "next/navigation"
 import Box from "@/ui/Box"
 import PermissionsList from "@/components/permissions/PermissionsList"
@@ -9,6 +9,8 @@ import Layout, { Breadcrumb } from "@/components/Layout"
 import { kv } from "@vercel/kv"
 import { CHAINS, ChainId } from "@/app/chains"
 import Flex from "@/ui/Flex"
+import LabeledData from "@/ui/LabeledData"
+import CopyButton from "@/ui/CopyButton"
 
 const chains = Object.values(CHAINS)
 
@@ -36,24 +38,30 @@ export default async function PermissionPage({
     <Layout
       head={
         <>
-          <Breadcrumb href={`/permissions/${chainId}/${hash}`}>
+          <Breadcrumb>
             <Flex gap={2} alignItems="center">
-              <MdOutlinePolicy />
-              <code className={styles.hash}>{hash}</code>
+              <LabeledData label="Permission Hash">
+                <div className={classes.hash}>{hash}</div>
+              </LabeledData>
             </Flex>
           </Breadcrumb>
         </>
       }
     >
-      <main className={styles.main}>
-        <Box p={3} className={styles.permissions}>
-          <h5>Permissions</h5>
-          <PermissionsList
-            targets={targets}
-            annotations={annotations}
-            chainId={chainId}
-          />
-        </Box>
+      <main className={classes.main}>
+        <div className={classes.header}>
+          <LabeledData label="Permission Hash">
+            <div className={classes.headerHash}>
+              {hash}
+              <CopyButton value={hash} />
+            </div>
+          </LabeledData>
+        </div>
+        <PermissionsList
+          targets={targets}
+          annotations={annotations}
+          chainId={chainId}
+        />
       </main>
     </Layout>
   )
