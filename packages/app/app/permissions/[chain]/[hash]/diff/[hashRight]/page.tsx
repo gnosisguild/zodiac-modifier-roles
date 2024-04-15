5e51f6ae65ee6b2cba660e108fc46e573945e83c
Jan-Felix <jfschwarz@users.noreply.github.com>
2024-04-15 11:27:26 +0200

    add annotations toggle to permissions diff

--- packages/app/app/permissions/[chain]/[hash]/diff/[hashRight]/page.tsx
+++ packages/app/app/permissions/[chain]/[hash]/diff/[hashRight]/page.tsx
@@ -4,16 +4,21 @@ import { kv } from "@vercel/kv"
 
 import Layout from "@/components/Layout"
 import PermissionsDiff from "@/components/permissions/PermissionsDiff"
+import LabeledData from "@/ui/LabeledData"
+import Flex from "@/ui/Flex"
 import PageBreadcrumbs from "./breadcrumbs"
 import styles from "./page.module.css"
 import { CHAINS } from "@/app/chains"
+import Link from "next/link"
 
 const chains = Object.values(CHAINS)
 
 export default async function DiffPage({
   params,
+  searchParams,
 }: {
   params: { chain: string; hash: string; hashRight: string }
+  searchParams: { annotations?: string }
 }) {
   const chainId = chains.find(
     (c) => c.prefix === params.chain.toLowerCase()
@@ -38,21 +43,37 @@ export default async function DiffPage({
     notFound()
   }
 
+  const hasAnnotations =
+    left.annotations.length > 0 || right.annotations.length > 0
+  const shallShowAnnotations = searchParams.annotations !== "false"
+
   return (
     <Layout head={<PageBreadcrumbs {...params} />}>
+      {hasAnnotations && (
+        <Flex
+          direction="row"
+          gap={1}
+          justifyContent="end"
+          className={styles.toolbar}
+        >
+          <Link href={{ query: { annotations: !shallShowAnnotations } }}>
+            {shallShowAnnotations ? "Hide annotations" : "Show annotations"}
+          </Link>
+        </Flex>
+      )}
       <main className={styles.main}>
         <PermissionsDiff
           left={{
             targets: left.targets.filter(
               (target) => !isEmptyFunctionScoped(target)
             ),
-            annotations: left.annotations,
+            annotations: shallShowAnnotations ? left.annotations : [],
           }}
           right={{
             targets: right.targets.filter(
               (target) => !isEmptyFunctionScoped(target)
             ),
-            annotations: right.annotations,
+            annotations: shallShowAnnotations ? right.annotations : [],
           }}
           chainId={chainId}
         />
