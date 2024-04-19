fd7d988c59cb99d6e4c4e458a6fdf8e757c8e95b
Jan-Felix <jfschwarz@users.noreply.github.com>
2024-04-19 11:01:11 +0200

    sdk: export supported chains and their subgraphs and prefixes

---
+++ packages/sdk/src/chains.ts
@@ -0,0 +1,37 @@
+export const chains = {
+  [1]: {
+    prefix: "eth",
+    subgraph:
+      "https://api.studio.thegraph.com/query/23167/zodiac-roles-mainnet/v2.1.3",
+  },
+  [100]: {
+    prefix: "gno",
+    subgraph:
+      "https://api.studio.thegraph.com/query/23167/zodiac-roles-gnosis/v2.1.3",
+  },
+  [137]: {
+    prefix: "matic",
+    subgraph:
+      "https://api.studio.thegraph.com/query/23167/zodiac-roles-polygon/v2.1.3",
+  },
+  [42161]: {
+    prefix: "arb1",
+    subgraph:
+      "https://api.studio.thegraph.com/query/23167/zodiac-roles-arbitrum-one/v2.1.3",
+  },
+  [43114]: {
+    prefix: "avax",
+    subgraph:
+      "https://api.studio.thegraph.com/query/23167/zodiac-roles-avalanche/v2.1.3",
+  },
+  [80001]: {
+    prefix: "maticmum",
+    subgraph:
+      "https://api.studio.thegraph.com/query/23167/zodiac-roles-mumbai/v2.1.3",
+  },
+  [11155111]: {
+    prefix: "sep",
+    subgraph:
+      "https://api.studio.thegraph.com/query/23167/zodiac-roles-sepolia/v2.1.3",
+  },
+}
