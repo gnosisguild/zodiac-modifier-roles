0d6a8eb6fb9c025a123ae1e327f7d4fcf7c50d02
Jan-Felix <jfschwarz@users.noreply.github.com>
2024-04-24 14:10:57 +0200

    sdk: upgrade subgraphs

--- packages/sdk/src/chains.ts
+++ packages/sdk/src/chains.ts
@@ -3,43 +3,43 @@ export const chains = {
     name: "mainnet",
     prefix: "eth",
     subgraph:
-      "https://api.studio.thegraph.com/query/23167/zodiac-roles-mainnet/v2.1.3",
+      "https://api.studio.thegraph.com/query/23167/zodiac-roles-mainnet/v2.1.4",
   },
   [100]: {
     name: "gnosis",
     prefix: "gno",
     subgraph:
-      "https://api.studio.thegraph.com/query/23167/zodiac-roles-gnosis/v2.1.3",
+      "https://api.studio.thegraph.com/query/23167/zodiac-roles-gnosis/v2.1.4",
   },
   [137]: {
     name: "polygon",
     prefix: "matic",
     subgraph:
-      "https://api.studio.thegraph.com/query/23167/zodiac-roles-polygon/v2.1.3",
+      "https://api.studio.thegraph.com/query/23167/zodiac-roles-polygon/v2.1.4",
   },
   [42161]: {
     name: "arbitrumOne",
     prefix: "arb1",
     subgraph:
-      "https://api.studio.thegraph.com/query/23167/zodiac-roles-arbitrum-one/v2.1.3",
+      "https://api.studio.thegraph.com/query/23167/zodiac-roles-arbitrum-one/v2.1.4",
   },
   [43114]: {
     name: "avalanche",
     prefix: "avax",
     subgraph:
-      "https://api.studio.thegraph.com/query/23167/zodiac-roles-avalanche/v2.1.3",
+      "https://api.studio.thegraph.com/query/23167/zodiac-roles-avalanche/v2.1.4",
   },
   [80001]: {
     name: "polygonMumbai",
     prefix: "maticmum",
     subgraph:
-      "https://api.studio.thegraph.com/query/23167/zodiac-roles-mumbai/v2.1.3",
+      "https://api.studio.thegraph.com/query/23167/zodiac-roles-mumbai/v2.1.4",
   },
   [11155111]: {
     name: "sepolia",
     prefix: "sep",
     subgraph:
-      "https://api.studio.thegraph.com/query/23167/zodiac-roles-sepolia/v2.1.3",
+      "https://api.studio.thegraph.com/query/23167/zodiac-roles-sepolia/v2.1.4",
   },
 } as const
 
