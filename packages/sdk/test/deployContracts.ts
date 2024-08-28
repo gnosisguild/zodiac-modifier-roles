ffb95e9ab955ca5de09b244f038d3de918555a6f
Jan-Felix <jfschwarz@users.noreply.github.com>
2024-08-28 21:35:21 +0200

    migrate sdk tests to ethers 6

--- packages/sdk/test/deployContracts.ts
+++ packages/sdk/test/deployContracts.ts
@@ -1,5 +1,7 @@
-import { parseEther } from "ethers"
+import { deployFactories, EIP1193Provider } from "@gnosis-guild/zodiac-core"
+import { Signer } from "ethers"
 import hre from "hardhat"
+import { EthereumProvider } from "hardhat/types"
 
 import { Roles } from "../../evm/typechain-types"
 
@@ -8,10 +10,14 @@ export const deployContracts = async (
   avatar: string,
   target: string
 ) => {
-  const [signer] = await hre.ethers.getSigners()
-  const deployer = hre.ethers.provider.getSigner(signer.address)
+  const [deployer] = await hre.ethers.getSigners()
 
-  await deployERC2470SingletonFactory()
+  await deployFactories({
+    provider: createAdapter({
+      provider: hre.network.provider,
+      signer: deployer,
+    }),
+  })
 
   // deploy libs
   const Packer = await hre.ethers.getContractFactory("Packer")
@@ -23,27 +29,30 @@ export const deployContracts = async (
   // deploy Roles
   const Roles = await hre.ethers.getContractFactory("Roles", {
     libraries: {
-      Integrity: integrity.address,
-      Packer: packer.address,
+      Integrity: await integrity.getAddress(),
+      Packer: await packer.getAddress(),
     },
   })
 
   const roles = await Roles.deploy(owner, avatar, target)
-  return roles as Roles
+  return roles as unknown as Roles
 }
 
-/**
- * Deploy the ERC-2470 singleton factory on networks where it doesn't exist yet.
- */
-const deployERC2470SingletonFactory = async () => {
-  const [signer] = await hre.ethers.getSigners()
-  await signer.sendTransaction({
-    to: "0xBb6e024b9cFFACB947A71991E386681B1Cd1477D",
-    value: parseEther("0.0247"),
-  })
-  if (!signer.provider) throw new Error("No provider")
+function createAdapter({
+  provider,
+  signer,
+}: {
+  provider: EthereumProvider
+  signer: Signer
+}): EIP1193Provider {
+  return {
+    request: async ({ method, params }) => {
+      if (method == "eth_sendTransaction") {
+        const { hash } = await signer.sendTransaction((params as any[])[0])
+        return hash
+      }
 
-  await signer.provider?.sendTransaction(
-    "0xf9016c8085174876e8008303c4d88080b90154608060405234801561001057600080fd5b50610134806100206000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c80634af63f0214602d575b600080fd5b60cf60048036036040811015604157600080fd5b810190602081018135640100000000811115605b57600080fd5b820183602082011115606c57600080fd5b80359060200191846001830284011164010000000083111715608d57600080fd5b91908080601f016020809104026020016040519081016040528093929190818152602001838380828437600092019190915250929550509135925060eb915050565b604080516001600160a01b039092168252519081900360200190f35b6000818351602085016000f5939250505056fea26469706673582212206b44f8a82cb6b156bfcc3dc6aadd6df4eefd204bc928a4397fd15dacf6d5320564736f6c634300060200331b83247000822470"
-  )
+      return provider.request({ method, params })
+    },
+  }
 }
