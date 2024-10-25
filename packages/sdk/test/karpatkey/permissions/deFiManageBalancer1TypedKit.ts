8b6a99e98a3f5e5279cfd85cf7add22533b6f26e
Jan-Felix <jfschwarz@users.noreply.github.com>
2024-10-25 22:09:52 +0200

    add failing test

--- packages/sdk/test/karpatkey/permissions/deFiManageBalancer1TypedKit.ts
+++ packages/sdk/test/karpatkey/permissions/deFiManageBalancer1TypedKit.ts
@@ -324,6 +324,24 @@ const preset = [
       toInternalBalance: false,
     }
   ),
+
+  //Aura claim zap
+  allow.aura.claim_zap.claimRewards(
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    {
+      claimCvxCrv: undefined,
+      claimLockedCvx: undefined,
+      lockCvxCrv: false,
+      lockCrvDeposit: false,
+      useAllWalletFunds: false,
+      useCompounder: false,
+      lockCvx: true,
+    }
+  ),
 ] satisfies Permission[]
 
 export default preset
