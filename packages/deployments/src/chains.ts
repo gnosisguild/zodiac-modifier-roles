2c13903e9b3d289585f65d6aee2c1e045495ae77
Jan-Felix <jfschwarz@users.noreply.github.com>
2024-11-01 12:27:32 +0100

    update subgraph dev endpoints

--- packages/deployments/src/chains.ts
+++ packages/deployments/src/chains.ts
@@ -3,54 +3,54 @@ export const chains = {
     name: "mainnet",
     prefix: "eth",
     subgraph:
-      "https://api.studio.thegraph.com/query/23167/zodiac-roles-mainnet/v2.3.0",
+      "https://api.studio.thegraph.com/query/93263/zodiac-roles-mainnet/v2.3.0",
   },
   [10]: {
     name: "optimism",
     prefix: "oeth",
     subgraph:
-      "https://api.studio.thegraph.com/query/23167/zodiac-roles-optimism/v2.3.0",
+      "https://api.studio.thegraph.com/query/93263/zodiac-roles-optimism/v2.3.0",
   },
   [100]: {
     name: "gnosis",
     prefix: "gno",
     subgraph:
-      "https://api.studio.thegraph.com/query/23167/zodiac-roles-gnosis/v2.3.0",
+      "https://api.studio.thegraph.com/query/93263/zodiac-roles-gnosis/v2.3.0",
   },
   [137]: {
     name: "polygon",
     prefix: "matic",
     subgraph:
-      "https://api.studio.thegraph.com/query/23167/zodiac-roles-polygon/v2.3.0",
+      "https://api.studio.thegraph.com/query/93263/zodiac-roles-polygon/v2.3.0",
   },
   [1101]: {
     name: "zkevm",
     prefix: "zkevm",
     subgraph:
-      "https://api.studio.thegraph.com/query/23167/zodiac-roles-zkevm/v2.3.0",
+      "https://api.studio.thegraph.com/query/93263/zodiac-roles-zkevm/v2.3.0",
   },
   [42161]: {
     name: "arbitrumOne",
     prefix: "arb1",
     subgraph:
-      "https://api.studio.thegraph.com/query/23167/zodiac-roles-arbitrum-one/v2.3.0",
+      "https://api.studio.thegraph.com/query/93263/zodiac-roles-arbitrum-one/v2.3.0",
   },
   [43114]: {
     name: "avalanche",
     prefix: "avax",
     subgraph:
-      "https://api.studio.thegraph.com/query/23167/zodiac-roles-avalanche/v2.3.0",
+      "https://api.studio.thegraph.com/query/93263/zodiac-roles-avalanche/v2.3.0",
   },
   [8453]: {
     name: "base",
     prefix: "base",
     subgraph:
-      "https://api.studio.thegraph.com/query/23167/zodiac-roles-base/v2.3.0",
+      "https://api.studio.thegraph.com/query/93263/zodiac-roles-base/v2.3.0",
   },
   [11155111]: {
     name: "sepolia",
     prefix: "sep",
     subgraph:
-      "https://api.studio.thegraph.com/query/23167/zodiac-roles-sepolia/v2.3.0",
+      "https://api.studio.thegraph.com/query/93263/zodiac-roles-sepolia/v2.3.0",
   },
 } as const
