8343ce62081b67d3bfb3748fae639a4b0f19f301
Jan-Felix <jfschwarz@users.noreply.github.com>
2024-12-08 22:05:30 +0100

    export some additional typescript interfaces

--- packages/sdk/src/types.ts
+++ packages/sdk/src/types.ts
@@ -1,3 +1,10 @@
+export {
+  Permission,
+  PermissionCoerced,
+  PermissionSet,
+} from "./permissions/types"
+export { Scoping } from "./permissions/authoring/conditions/types"
+
 export interface Allowance {
   key: `0x${string}`
   period: number
