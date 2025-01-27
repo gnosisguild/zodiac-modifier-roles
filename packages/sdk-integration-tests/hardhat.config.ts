0dc0d81f9f87a863fb5c265c7a4836f633a70860
Cristóvão Honorato <cristovao.honorato@gmail.com>
2025-01-27 09:57:23 +0100

    extract sdk-integration-tests

---
+++ packages/sdk-integration-tests/hardhat.config.ts
@@ -0,0 +1,20 @@
+import { HardhatUserConfig } from "hardhat/types"
+import "@nomicfoundation/hardhat-ethers"
+
+import evmConfig from "../evm/hardhat.config"
+
+const config: HardhatUserConfig = {
+  ...evmConfig,
+  paths: {
+    root: "../evm",
+    artifacts: "build/artifacts",
+    cache: "build/cache",
+    sources: "contracts",
+    tests: "./test",
+  },
+  mocha: {
+    timeout: 2000000,
+  },
+}
+
+export default config
