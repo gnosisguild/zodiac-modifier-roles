ad13e56f8fbf39a2d56374b2f198e3f97565eee1
Cristóvão Honorato <cristovao.honorato@gmail.com>
2025-02-04 22:03:53 +0100

    diffMembers tests

---
+++ packages/sdk/src/diff/diffMembers.test.ts
@@ -0,0 +1,89 @@
+import { expect, it, suite } from "vitest"
+import { ZeroHash } from "ethers"
+
+import { diffMembers } from "./diffMembers"
+
+const roleKey = ZeroHash
+const AddressOne = `0x0000000000000000000000000000000000000001`
+const AddressTwo = `0x0000000000000000000000000000000000000002`
+const AddressThree = `0x0000000000000000000000000000000000000003`
+const AddressFour = `0x0000000000000000000000000000000000000004`
+
+suite("diffMembers", () => {
+  it("works for both prev and next", () => {
+    const prev: `0x${string}`[] = [AddressOne, AddressTwo, AddressThree]
+
+    const next: `0x${string}`[] = [AddressTwo, AddressThree, AddressFour]
+
+    const { minus, plus } = diffMembers({ roleKey, prev, next })
+    expect(minus).toEqual([
+      {
+        call: "assignRoles",
+        roleKey,
+        member: AddressOne,
+        join: false,
+      },
+    ])
+    expect(plus).toEqual([
+      {
+        call: "assignRoles",
+        roleKey,
+        member: AddressFour,
+        join: true,
+      },
+    ])
+  })
+
+  it("only prev", () => {
+    const prev: `0x${string}`[] = [AddressOne, AddressTwo, AddressThree]
+
+    const { minus, plus } = diffMembers({ roleKey, prev })
+    expect(minus).toEqual([
+      {
+        call: "assignRoles",
+        roleKey,
+        member: AddressOne,
+        join: false,
+      },
+      {
+        call: "assignRoles",
+        roleKey,
+        member: AddressTwo,
+        join: false,
+      },
+      {
+        call: "assignRoles",
+        roleKey,
+        member: AddressThree,
+        join: false,
+      },
+    ])
+  })
+
+  it("only next", () => {
+    const next: `0x${string}`[] = [AddressTwo, AddressThree, AddressFour]
+
+    const { minus, plus } = diffMembers({ roleKey, next })
+    expect(minus).toEqual([])
+    expect(plus).toEqual([
+      {
+        call: "assignRoles",
+        roleKey,
+        member: AddressTwo,
+        join: true,
+      },
+      {
+        call: "assignRoles",
+        roleKey,
+        member: AddressThree,
+        join: true,
+      },
+      {
+        call: "assignRoles",
+        roleKey,
+        member: AddressFour,
+        join: true,
+      },
+    ])
+  })
+})
