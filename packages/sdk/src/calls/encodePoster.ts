c82619e2e1b99df1d2b0bead2a3e5b9e960bd83e
Cristóvão Honorato <cristovao.honorato@gmail.com>
2025-02-06 14:41:52 +0100

    adjust encoding to account for some messages being sent to POSTER

---
+++ packages/sdk/src/calls/encodePoster.ts
@@ -0,0 +1,36 @@
+import { Interface } from "ethers"
+
+import { posterAbi } from "../abi"
+
+interface AnnotationsPost {
+  addAnnotations?: {
+    uris: string[]
+    schema: string
+  }[]
+  removeAnnotations?: string[] // referenced by `uri` field
+}
+
+// EIP-3722 Poster contract
+export const POSTER_ADDRESS =
+  "0x000000000000cd17345801aa8147b8D3950260FF" as `0x${string}`
+
+const ROLES_ANNOTATION_POSTER_TAG = "ROLES_PERMISSION_ANNOTATION"
+
+const posterInterface = new Interface(posterAbi)
+
+export const encodeAnnotationsPost = (
+  rolesMod: string,
+  roleKey: string,
+  payload: AnnotationsPost
+) => {
+  const content = JSON.stringify({
+    rolesMod,
+    roleKey,
+    ...payload,
+  })
+
+  return posterInterface.encodeFunctionData("post", [
+    content,
+    ROLES_ANNOTATION_POSTER_TAG,
+  ]) as `0x${string}`
+}
