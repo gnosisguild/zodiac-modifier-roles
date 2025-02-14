ec92b56751234b8c060fa74d4b0dfca2d1bd9d88
Cristóvão Honorato <cristovao.honorato@gmail.com>
2025-02-14 13:02:23 +0100

    add merge conditions test placeholders

---
+++ packages/sdk/src/permissions/mergeConditions.test.ts
@@ -0,0 +1,13 @@
+import { it, suite } from "vitest"
+
+suite("mergeConditions()", () => {
+  it.todo("with condition + without condition - gets merged as no condition")
+
+  it.todo("both no condition")
+
+  it.todo("both with condition - joined via OR")
+
+  it.todo("both with condition - joined via OR, left gets hoisted")
+
+  it.todo("both with condition - joined via OR, right gets hoisted")
+})
