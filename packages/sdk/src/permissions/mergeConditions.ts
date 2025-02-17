2cc3d3917255bd30caf831450044fad22830e23e
Cristóvão Honorato <cristovao.honorato@gmail.com>
2025-02-17 14:48:48 +0100

    Adjust merge Condition to receive only conditions as input

--- packages/sdk/src/permissions/mergeConditions.ts
+++ packages/sdk/src/permissions/mergeConditions.ts
@@ -1,31 +1,12 @@
 import { Condition, Operator, ParameterType } from "zodiac-roles-deployments"
 
-import { targetId } from "./utils"
-
-import { FunctionPermissionCoerced } from "./types"
-
 /**
  * @dev Merges two conditions using a logical OR, flattening nested OR conditions. If the conditions are equal, it will still create separate OR branches.
  * These will be pruned later in sanitizeCondition().
  */
-export const mergeConditions = (
-  a: FunctionPermissionCoerced,
-  b: FunctionPermissionCoerced
-): Condition | undefined => {
-  if (!!a.condition !== !!b.condition) {
-    const id = targetId(a)
-    console.warn(
-      `Target ${id} is allowed with and without conditions. It will be allowed without conditions.`
-    )
-    return undefined
-  }
-
-  if (!a.condition || !b.condition) return undefined
-
-  const aBranches =
-    a.condition.operator === Operator.Or ? a.condition.children : [a.condition]
-  const bBranches =
-    b.condition.operator === Operator.Or ? b.condition.children : [b.condition]
+export const mergeConditions = (a: Condition, b: Condition): Condition => {
+  const aBranches = a.operator === Operator.Or ? a.children : [a]
+  const bBranches = b.operator === Operator.Or ? b.children : [b]
 
   return {
     paramType: ParameterType.None,
