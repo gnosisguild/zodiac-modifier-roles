e4b2ce66d046701c006212ece2bc0e69c2f66814
Cristóvão Honorato <cristovao.honorato@gmail.com>
2025-02-20 13:09:18 +0100

    Write merge conditions using normalizeConditions

--- packages/sdk/src/permissions/mergeConditions.ts
+++ packages/sdk/src/permissions/mergeConditions.ts
@@ -1,16 +1,24 @@
 import { Condition, Operator, ParameterType } from "zodiac-roles-deployments"
+import { normalizeCondition } from "../conditions"
 
 /**
  * @dev Merges two conditions using a logical OR, flattening nested OR conditions. If the conditions are equal, it will still create separate OR branches.
  * These will be pruned later in sanitizeCondition().
  */
 export const mergeConditions = (a: Condition, b: Condition): Condition => {
-  const aBranches = a.operator === Operator.Or ? a.children : [a]
-  const bBranches = b.operator === Operator.Or ? b.children : [b]
+  return trimIds(
+    normalizeCondition({
+      paramType: ParameterType.None,
+      operator: Operator.Or,
+      children: [a, b],
+    })
+  )
+}
 
+function trimIds(condition: any): Condition {
+  const { $$id, children, ...rest } = condition
   return {
-    paramType: ParameterType.None,
-    operator: Operator.Or,
-    children: [...(aBranches || []), ...(bBranches || [])],
+    ...rest,
+    ...(children ? { children: children.map(trimIds) } : {}),
   }
 }
