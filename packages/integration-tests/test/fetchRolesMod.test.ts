ef106b3d25c1801af75499a6e670a4b4a4a6ec64
Cristóvão <cris@gnosisguild.org>
2025-02-24 18:26:48 +0100

    fetch roles no longer returns a summary. fix tests

--- packages/integration-tests/test/fetchRolesMod.test.ts
+++ packages/integration-tests/test/fetchRolesMod.test.ts
@@ -23,7 +23,7 @@ describe("fetchRolesMod", () => {
     expect(rolesMod).to.have.property("unwrapAdapters")
   })
 
-  it("fetches all roles but omits conditions while indicating if the target function is wildcarded", async () => {
+  it("fetches all roles while indicating if the target function is wildcarded", async () => {
     const rolesMod = await fetchRolesMod({
       address: "0x13c61a25db73e7a94a244bd2205adba8b4a60f4a",
       chainId: 1,
@@ -40,6 +40,6 @@ describe("fetchRolesMod", () => {
 
     const funcWithCondition = firstRole.targets[1].functions[0]
     expect(funcWithCondition).to.have.property("wildcarded", false)
-    expect(funcWithCondition).to.not.have.property("condition")
+    // expect(funcWithCondition).to.not.have.property("condition")
   })
 })
