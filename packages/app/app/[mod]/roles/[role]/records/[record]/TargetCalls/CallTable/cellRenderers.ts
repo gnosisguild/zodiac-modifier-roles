6c1fffb7dc325653d526768dcccccc2866c6da7a
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-02-27 22:10:47 +0100

    adjust NestedValuesRenderer

--- packages/app/app/[mod]/roles/[role]/records/[record]/TargetCalls/CallTable/cellRenderers.ts
+++ packages/app/app/[mod]/roles/[role]/records/[record]/TargetCalls/CallTable/cellRenderers.ts
@@ -4,105 +4,50 @@ import {
   type ICellRendererComp,
   type ICellRendererParams,
 } from "ag-grid-community"
-import { Row } from "./types"
+import { NestedArrayValues, Row } from "./types"
 
-type PrimitiveCellValue = string | number | boolean | bigint
-// type ArrayCellValue = PrimitiveCellValue[]
-
-export interface NestedCellValue {
-  value: PrimitiveCellValue[] | NestedCellValue[]
-  span: number[]
-  totalSpan: number
-}
-
-// export interface NestedCellValue {
-//   value: PrimitiveCellValue[] | ArrayCellValue[]
-//   span: number[]
-//   totalSpan: number
-// }
-
-export class NestedCellRenderer implements ICellRendererComp<Row> {
+export class NestedValuesRenderer implements ICellRendererComp<Row> {
   eGui!: HTMLDivElement
 
-  init(params: ICellRendererParams<Row, NestedCellValue>) {
+  init(params: ICellRendererParams<Row, NestedArrayValues>) {
     invariant(params.value != null, "unexpected empty cell value")
 
     this.eGui = document.createElement("div")
-    this.eGui.dataset.span = params.value.totalSpan.toString()
+    this.eGui.dataset.span = params.value.span.toString()
 
-    const html = this.renderValues(
-      params.value.value.map((element) =>
-        this.formatElementValue(element, params)
-      ),
-      params.value.span
-    )
+    const valueFormatter =
+      params.formatValue ??
+      ((val: any) => (val != null ? val.toString() : undefined))
+
+    const html = this.render(params.value, valueFormatter)
     this.eGui.innerHTML = html
   }
 
   render(
-    value: NestedCellValue,
-    params: ICellRendererParams<Row, NestedCellValue>
-  ) {
-    const isNested =
-      Array.isArray(value.value) &&
-      value.value.length > 0 &&
-      !Array.isArray(value.value[0])
-
-    if (!isNested) {
-      if (Array.isArray(value.value)) {
-        return (
-          '<ol start="0">' +
-          value.value
-            .map((element) => `<li data-span="1">${element}</li>`)
-            .join("") +
-          "</ol>"
-        )
-      } else {
-        return this.formatElementValue(value.value, params)
-      }
+    value: NestedArrayValues,
+    valueFormatter: (value: any | null | undefined) => string | undefined
+  ): string {
+    if ("value" in value) {
+      return this.formatElementValue(value.value, valueFormatter)
     }
 
-    // is nested
-    return (
-      '<ol start="0">' +
-      value
-        .map((element) => `<li data-span="${span}">${element}</li>`)
-        .join("") +
-      "</ol>"
-    )
+    const lis = value.children.map((child) => {
+      return (
+        `<li data-span="${child.span}">` +
+        this.render(child, valueFormatter) +
+        `</li>`
+      )
+    })
+
+    return '<ol start="0">' + lis.join("") + "</ol>"
   }
 
-  renderNested(elements: string[], span: number[]) {}
-
-  renderPrimitive(elements: string[], span: number[]) {
-    return (
-      '<ol start="0">' +
-      elements
-        .map((element) => `<li data-span="${span}">${element}</li>`)
-        .join("") +
-      "</ol>"
-    )
-  }
-
-  renderValues(elements: string[], span: number[]) {
-    return (
-      '<ol start="0">' +
-      elements
-        .map((element) => `<li data-span="${span}">${element}</li>`)
-        .join("") +
-      "</ol>"
-    )
-  }
-
+  // Format the value of an element according to the column definition.
+  // ATTENTION: It's crucial to properly escape the value to prevent XSS attacks.
   formatElementValue(
     element: any,
-    params: ICellRendererParams<Row, NestedCellValue>
+    valueFormatter: (value: any | null | undefined) => string | undefined
   ) {
-    const valueFormatter =
-      typeof params.colDef?.valueFormatter === "function"
-        ? params.colDef?.valueFormatter
-        : (val: any) => val.toString()
-
     const escaped = _escapeString(valueFormatter(element))
     invariant(escaped != null, "unexpected empty formatted value")
     return escaped
@@ -116,49 +61,3 @@ export class NestedCellRenderer implements ICellRendererComp<Row> {
     return false
   }
 }
-
-interface ArrayElements {
-  value: any[]
-  span: number[]
-  totalSpan: number
-}
-
-/** Renders (nested) array elements as a lists. */
-// export class ArrayElementsRenderer implements ICellRendererComp<Row> {
-//   eGui!: HTMLDivElement
-
-//   init(params: ICellRendererParams<Row, ArrayElements[]>) {
-//     params.invariant(Array.isArray(params.value), "Expected array")
-
-//     this.eGui = document.createElement("div")
-//     // this.eGui.setAttribute("start", "0")
-
-//     const ols = params.value.map((indices) =>
-//       this.renderArray(indices.value.map(String), indices.span)
-//     )
-//     const html = this.renderArray(
-//       ols,
-//       ols.map((_, i) => i)
-//     )
-
-//     this.eGui.innerHTML = html
-//   }
-
-//   renderArray(elements: string[], span: number[]) {
-//     return (
-//       '<ol start="0">' +
-//       elements
-//         .map((element) => `<li data-span="${span}">${element}</li>`)
-//         .join("") +
-//       "</ol>"
-//     )
-//   }
-
-//   getGui() {
-//     return this.eGui
-//   }
-
-//   refresh(params: ICellRendererParams): boolean {
-//     return false
-//   }
-// }
