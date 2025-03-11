80015061e98dc895a260694aa4945effff39b769
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-03-11 22:37:10 +0100

    wip: custom header rendering

--- packages/app/app/[mod]/roles/[role]/records/[record]/TargetCalls/CallTable/index.tsx
+++ packages/app/app/[mod]/roles/[role]/records/[record]/TargetCalls/CallTable/index.tsx
@@ -22,7 +22,7 @@ import classes from "./style.module.css"
 import { theme } from "./theme"
 import { Row, StructRowValue } from "./types"
 import { distributeArrayElements, totalSpan } from "./distributeArrayElements"
-import { CustomInnerHeader } from "./customInnerHeader"
+import { CustomHeader } from "./customHeader"
 
 ModuleRegistry.registerModules([AllCommunityModule])
 
@@ -100,13 +100,28 @@ const inputColumnDefs = (
         // spanRows: true,
         suppressMovable: true,
         headerClass: cn(
-          !!carryComponentNameToValuesColumn && "agx-header-array-values",
+          // !!carryComponentNameToValuesColumn && "agx-header-array-values",
           isLastChild && "agx-header-cell-last-child"
         ),
 
+        headerComponent: CustomHeader,
         headerComponentParams: {
-          innerHeaderComponent: CustomInnerHeader,
+          // template: `<div class="ag-cell-label-container" role="presentation">
+          //   <span data-ref="eMenu" class="ag-header-icon ag-header-cell-menu-button" aria-hidden="true"></span>
+          //   <span data-ref="eFilterButton" class="ag-header-icon ag-header-cell-filter-button" aria-hidden="true"></span>
+          //   <div data-ref="eLabel" class="ag-header-cell-label" role="presentation">
+          //     <span data-ref="eText" class="ag-header-cell-text"></span>
+          //     <span data-ref="eFilter" class="ag-header-icon ag-header-label-icon ag-filter-icon" aria-hidden="true"></span>
+          //     <span data-ref="eSortOrder" class="ag-header-icon ag-header-label-icon ag-sort-order" aria-hidden="true"></span>
+          //     <span data-ref="eSortAsc" class="ag-header-icon ag-header-label-icon ag-sort-ascending-icon" aria-hidden="true"></span>
+          //     <span data-ref="eSortDesc" class="ag-header-icon ag-header-label-icon ag-sort-descending-icon" aria-hidden="true"></span>
+          //     <span data-ref="eSortNone" class="ag-header-icon ag-header-label-icon ag-sort-none-icon" aria-hidden="true"></span>
+          //   </div>
+          // </div>`,
         },
+        // headerComponentParams: {
+        //   innerHeaderComponent: CustomInnerHeader,
+        // },
       }
 
       const componentName = input.name ?? `[${index}]`
@@ -222,6 +237,7 @@ const metadataColumns: ColDef<Row>[] = [
     cellClass: "agx-label-column",
     cellRenderer: EditableCellRenderer,
     editable: true,
+    headerComponent: CustomHeader,
   },
   {
     headerName: "recorded",
@@ -229,6 +245,7 @@ const metadataColumns: ColDef<Row>[] = [
     cellClass: "agx-recorded-column",
     cellRenderer: RecordedCellRenderer,
     width: 120,
+    headerComponent: CustomHeader,
   },
 ]
 
