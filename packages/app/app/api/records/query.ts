5d2814eaacab94f95b8c0ae1b38e5fa224e911ee
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-03-19 22:28:40 +0100

    taint auth token

--- packages/app/app/api/records/query.ts
+++ packages/app/app/api/records/query.ts
@@ -1,6 +1,7 @@
 import { kv } from "@vercel/kv"
 import { notFound } from "next/navigation"
 import { zRecord } from "./types"
+import { experimental_taintUniqueValue } from "react"
 
 export async function getRecordById(recordId: string) {
   const storedRecord = await kv.get(recordId)
@@ -13,7 +14,14 @@ export async function getRecordById(recordId: string) {
   const record = zRecord.parse(storedRecord)
 
   // Omit the authToken property so that the token isn’t exposed.
-  const { authToken, ...publicRecord } = record
+  // const { authToken, ...publicRecord } = record
 
-  return publicRecord
+  // Prevent the authToken from ever being passed to the client
+  experimental_taintUniqueValue(
+    "Never pass the authToken to the client",
+    record,
+    record.authToken
+  )
+
+  return record
 }
--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
       fullUrl: true,
     },
   },
+  experimental: {
+    taint: true,
+  },
   async headers() {
     return [
       {
