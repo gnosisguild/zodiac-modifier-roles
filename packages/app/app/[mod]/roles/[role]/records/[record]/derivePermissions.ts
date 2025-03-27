6db4801933a5819c6980e2e799019109a3bdc705
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-03-27 22:03:50 +0100

    derivePermissions implementation

--- packages/app/app/[mod]/roles/[role]/records/[record]/derivePermissions.ts
+++ packages/app/app/[mod]/roles/[role]/records/[record]/derivePermissions.ts
@@ -1,9 +1,16 @@
-import { AbiFunction, toFunctionSelector, decodeFunctionData } from "viem"
-import { FunctionPermission } from "zodiac-roles-sdk"
+import {
+  AbiFunction,
+  toFunctionSelector,
+  decodeFunctionData,
+  AbiParameter,
+  encodeAbiParameters,
+} from "viem"
+import { c, Condition, FunctionPermission } from "zodiac-roles-sdk"
+import { ParamType } from "ethers"
+import { invariant } from "@epic-web/invariant"
 import { fetchContractInfo } from "@/app/abi"
 import { ChainId } from "@/app/chains"
 import { Call, Operation, Record } from "@/app/api/records/types"
-import { mapAbiInputs, StructAbiInput } from "./abi"
 
 export async function derivePermissionsFromRecord(
   record: Record,
@@ -34,7 +41,10 @@ export async function derivePermissionsFromRecord(
       throw new Error(`Unable to decode function data for call ${call.id}`)
     }
 
-    const wildcards = record.wildcards[call.to + ":" + selector] || {}
+    const wildcardsObj = record.wildcards[call.to + ":" + selector] || {}
+    const wildcards = Object.entries(wildcardsObj)
+      .filter(([, active]) => !!active)
+      .map(([paramPath]) => paramPath)
     return derivePermissionFromCall({ call, abi: functionAbi, wildcards })
   })
 
@@ -48,29 +58,43 @@ const derivePermissionFromCall = ({
 }: {
   call: Call
   abi: AbiFunction
-  wildcards: { [paramPath: string]: boolean | undefined }
+  wildcards: string[]
 }): FunctionPermission => {
   const selector = call.data.slice(0, 10) as `0x${string}`
   const { args } = decodeFunctionData({ abi: [abi], data: call.data })
-  const abiInputs = mapAbiInputs(abi.inputs, args)
 
   return {
     targetAddress: call.to,
     selector,
     delegatecall: call.operation === Operation.DelegateCall,
     send: BigInt(call.value || "0") > 0,
-    condition: deriveConditionFromWildcards(abiInputs, wildcards),
+    condition: deriveConditionFromWildcards(args, abi.inputs, wildcards),
   }
 }
 
+/**
+ * Derives a condition from the wildcards:
+ *
+ * Initially invoked with the set of function inputs, then recurses for struct parameters.
+ */
 const deriveConditionFromWildcards = (
-  abiInputs: StructAbiInput,
-  wildcards: { [paramPath: string]: boolean | undefined },
-  isTopLevelCalldata = true
-) => {
-  const noWildcards = Object.values(wildcards).every((v) => !v)
-  const allWildcarded = Object.keys(abiInputs).every(
-    (paramName) => wildcards[paramName]
+  values: readonly unknown[],
+  abi: readonly AbiParameter[],
+  wildcards: string[],
+  pathPrefix = ""
+): Condition | undefined => {
+  const isTopLevelCalldata = pathPrefix === ""
+
+  const filteredWildcards = wildcards.filter((wildcard) =>
+    abi.some((input, index) =>
+      wildcard.startsWith(pathPrefix + (input.name ?? `[${index}]`))
+    )
+  )
+
+  const noWildcards = filteredWildcards.length === 0
+
+  const allWildcarded = abi.every((component, index) =>
+    filteredWildcards.includes(pathPrefix + (component.name ?? `[${index}]`))
   )
 
   if (allWildcarded) {
@@ -78,7 +102,51 @@ const deriveConditionFromWildcards = (
     return undefined
   }
 
-  if (noWildcards) {
-    // full equals match
+  // some weird incompatibility between ParamType from different ethers instances forces use of any :/
+  const paramTypes = abi.map((abiParam) => ParamType.from(abiParam)) as any[]
+
+  if (noWildcards && isTopLevelCalldata) {
+    // use calldata matches on top-level
+    return c.calldataMatches(
+      values.map((value, index) => c.eq(value)(paramTypes[index])),
+      paramTypes
+    )()
   }
+
+  const tupleType = ParamType.from({
+    type: "tuple",
+    components: abi,
+  })
+
+  if (noWildcards && !isTopLevelCalldata) {
+    // full equals match on tuple
+
+    return c.eq(encodeAbiParameters(abi, values))(tupleType as any)
+  }
+
+  // use matches structure on the tuple and recurse into tuple components
+  const components = abi.map((component, index) => {
+    const componentPath = pathPrefix + (component.name ?? `[${index}]`)
+    const isWildcarded = filteredWildcards.includes(componentPath)
+
+    if (isWildcarded) {
+      return undefined
+    }
+
+    if (component.type === "tuple") {
+      invariant("components" in component, "tuple components not found")
+      invariant(Array.isArray(values[index]), "invalid tuple value")
+      return deriveConditionFromWildcards(
+        values[index],
+        component.components,
+        filteredWildcards,
+        componentPath + "."
+      )
+    }
+
+    // some weird incompatibility between ParamType from different ethers instances :/
+    return c.eq(values[index])(ParamType.from(component) as any)
+  })
+
+  return c.matches(components)(tupleType as any)
 }
