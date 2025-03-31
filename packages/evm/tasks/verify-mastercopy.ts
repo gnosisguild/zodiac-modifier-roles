01f562eda024089c1e0253fe2128195deff4fc3b
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-03-31 11:43:46 +0200

    cleanup

--- packages/evm/tasks/verify-mastercopy.ts
+++ packages/evm/tasks/verify-mastercopy.ts
@@ -1,8 +1,6 @@
 import { task, types } from "hardhat/config";
 import { readMastercopies, verifyMastercopy } from "@gnosis-guild/zodiac-core";
 
-const { ETHERSCAN_API_KEY } = process.env;
-
 task(
   "verify:mastercopy",
   "Verifies all mastercopies from the artifacts file in the block explorer corresponding to the current network"
