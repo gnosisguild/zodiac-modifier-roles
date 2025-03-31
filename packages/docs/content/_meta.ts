4f3953d79d6061a84535b71c3c8182aac533f792
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-03-31 17:10:53 +0200

    upgrade next, react, nextra

---
+++ packages/docs/content/_meta.ts
@@ -0,0 +1,20 @@
+import type { MetaRecord } from "nextra";
+
+const meta: MetaRecord = {
+  "app-link": {
+    title: "Go to app",
+    type: "page",
+    href: "https://roles.gnosisguild.org",
+  },
+  index: {
+    title: "Introduction",
+  },
+  general: {
+    title: "General Concepts",
+  },
+  sdk: {
+    title: "SDK",
+  },
+};
+
+export default meta;
---
+++ packages/docs/content/sdk/_meta.ts
@@ -0,0 +1,15 @@
+import type { MetaRecord } from "nextra";
+
+const meta: MetaRecord = {
+  "getting-started": {
+    title: "Getting Started",
+  },
+  architecture: {
+    title: "Architecture",
+  },
+  conditions: {
+    title: "Conditions",
+  },
+};
+
+export default meta;
