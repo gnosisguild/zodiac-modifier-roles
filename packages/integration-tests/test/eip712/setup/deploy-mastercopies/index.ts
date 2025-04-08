77895737b4492a0e531334c2afdea689e6e40312
Cristóvão <cris@gnosisguild.org>
2025-04-08 15:49:24 +0200

    scopeSignTypedMessage tests

--- packages/integration-tests/test/eip712/setup/deploy-mastercopies/index.ts
+++ packages/integration-tests/test/eip712/setup/deploy-mastercopies/index.ts
@@ -1,21 +1,28 @@
-import hre from "hardhat";
+import hre from "hardhat"
 
-import { deployFactory as deploy2470Factory } from "./eip2470";
-import { deployFallbackHandler } from "./fallbackHandler";
-import { deployModuleProxyFactory } from "./moduleProxyFactory";
-import { deployRolesMastercopy } from "./rolesMastercopy";
-import { deploySafeMastercopy } from "./safeMastercopy";
-import { deploySafeProxyFactory } from "./safeProxyFactory";
-import { deployFactory } from "./singletonFactory";
+import { deployFactory as deploy2470Factory } from "./eip2470"
+import { deployFallbackHandler } from "./fallbackHandler"
+import { deployModuleProxyFactory } from "./moduleProxyFactory"
+import { deployRolesMastercopy } from "./rolesMastercopy"
+import { deploySafeMastercopy } from "./safeMastercopy"
+import {
+  deploySafeProxyFactory,
+  address as rolesMastercopyAddress,
+} from "./safeProxyFactory"
+import { deployFactory } from "./singletonFactory"
 
 export default async function deployMastercopies() {
-  const [, , , , , , deployer] = await hre.ethers.getSigners();
+  const [, , , , , , deployer] = await hre.ethers.getSigners()
 
-  await deployFactory(deployer);
-  await deploy2470Factory(deployer);
-  await deployModuleProxyFactory(deployer);
-  await deployFallbackHandler(deployer);
-  await deploySafeMastercopy(deployer);
-  await deploySafeProxyFactory(deployer);
-  await deployRolesMastercopy(deployer);
+  if ((await deployer.provider.getCode(rolesMastercopyAddress)) != "0x") {
+    return
+  }
+
+  await deployFactory(deployer)
+  await deploy2470Factory(deployer)
+  await deployModuleProxyFactory(deployer)
+  await deployFallbackHandler(deployer)
+  await deploySafeMastercopy(deployer)
+  await deploySafeProxyFactory(deployer)
+  await deployRolesMastercopy(deployer)
 }
--- packages/integration-tests/test/eip712/setup/deployEIP712Encoder.ts
+++
@@ -1,6 +0,0 @@
-import { ethers } from "hardhat";
-
-export async function deployEIP712Encoder() {
-  const EIP7127Encoder = await ethers.getContractFactory("EIP712Encoder");
-  return EIP7127Encoder.deploy();
-}
