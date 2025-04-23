08f37514a5a82e740928dfa09022155f4e9fe8c5
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-04-23 12:49:00 +0200

    open the diff page in safe wallet

--- packages/app/app/[mod]/roles/[role]/records/[record]/Apply.tsx
+++ packages/app/app/[mod]/roles/[role]/records/[record]/Apply.tsx
@@ -6,6 +6,9 @@ import { serverApplyPermissions } from "./serverActions"
 import { useRouter } from "next/navigation"
 import { useParams } from "next/navigation"
 import { parseModParam } from "@/app/params"
+import { ChainId } from "zodiac-roles-sdk"
+import { createPublicClient, getAddress, http } from "viem"
+import { CHAINS } from "@/app/chains"
 
 const Apply: React.FC<{}> = ({}) => {
   const [isLoading, setIsLoading] = useState(false)
@@ -14,12 +17,13 @@ const Apply: React.FC<{}> = ({}) => {
     role: string
     record: string
   }>()
-  const chainId = parseModParam(mod)?.chainId
   const router = useRouter()
 
-  if (!chainId) {
+  const parsed = parseModParam(mod)
+  if (!parsed) {
     return null
   }
+  const { chainId, chainPrefix, address } = parsed
 
   const apply = async () => {
     setIsLoading(true)
@@ -27,6 +31,20 @@ const Apply: React.FC<{}> = ({}) => {
       recordId: record,
       chainId,
     })
+    const path = `/${mod}/roles/${role}/diff/${permissionsHash}`
+
+    const isInIframe = window.self !== window.top
+    if (!isInIframe) {
+      // If owned by a Safe redirect to the Safe app
+      const ownerSafe = await fetchOwnerSafe(chainId, address)
+      if (ownerSafe) {
+        window.location.href = `https://app.safe.global/apps/open?safe=${chainPrefix}:${ownerSafe}&appUrl=${encodeURIComponent(
+          window.location.origin + path
+        )}`
+        return
+      }
+    }
+
     router.push(`/${mod}/roles/${role}/diff/${permissionsHash}`)
   }
 
@@ -38,3 +56,57 @@ const Apply: React.FC<{}> = ({}) => {
 }
 
 export default Apply
+
+/**
+ * Fetch the owner of the given rolesMod.
+ * Returns undefined if the rolesMod is not a Safe.
+ *
+ * This function is memoized to avoid unnecessary RPC calls.
+ */
+async function fetchOwnerSafe(
+  chainId: ChainId,
+  roleMod: `0x${string}`
+): Promise<`0x${string}` | undefined> {
+  const publicClient = createPublicClient({
+    chain: CHAINS[chainId],
+    transport: http(),
+  })
+  const owner = await publicClient.readContract({
+    address: roleMod,
+    abi: [
+      {
+        inputs: [],
+        name: "owner",
+        outputs: [
+          {
+            internalType: "address",
+            name: "",
+            type: "address",
+          },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+    ] as const,
+    functionName: "owner",
+  })
+  const result = (await isSafeAccount(chainId, owner)) ? owner : undefined
+
+  return result
+}
+
+const isSafeAccount = async (
+  chainId: ChainId,
+  address: `0x${string}`
+): Promise<boolean> => {
+  try {
+    const response = await fetch(
+      `https://safe-client.safe.global/v1/chains/${chainId}/safes/${getAddress(
+        address
+      )}`
+    )
+    return response.ok
+  } catch (error) {
+    return false
+  }
+}
