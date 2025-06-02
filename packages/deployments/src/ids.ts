689934177dfbeedf0c889d9f9e451ef8ce9885a0
Cristóvão <cris@gnosisguild.org>
2025-06-02 11:08:34 +0200

    adjust id generation

--- packages/deployments/src/ids.ts
+++ packages/deployments/src/ids.ts
@@ -2,10 +2,10 @@ import { chains } from "./chains"
 import { ChainId } from "./types"
 
 export const getRolesModId = (chainId: ChainId, address: `0x${string}`) =>
-  `rolesmod:${chains[chainId].prefix}:${address.toLowerCase()}`
+  `${chains[chainId].prefix}:${address.toLowerCase()}`
 
 export const getRoleId = (
   chainId: ChainId,
   address: `0x${string}`,
   roleKey: `0x${string}`
-) => `role:${chains[chainId].prefix}:${address.toLowerCase()}:${roleKey}`
+) => `${chains[chainId].prefix}:${address.toLowerCase()}:${roleKey}`
