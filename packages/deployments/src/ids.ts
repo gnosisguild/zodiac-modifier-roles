25d6b0fab8b76834e45f84d6d032181e2ad5375c
Jan-Felix <jfschwarz@users.noreply.github.com>
2025-06-02 09:57:36 +0200

    adust id scheme

--- packages/deployments/src/ids.ts
+++ packages/deployments/src/ids.ts
@@ -2,10 +2,10 @@ import { chains } from "./chains"
 import { ChainId } from "./types"
 
 export const getRolesModId = (chainId: ChainId, address: `0x${string}`) =>
-  `${chains[chainId].prefix}:rolesmod:${address.toLowerCase()}`
+  `rolesmod:${chains[chainId].prefix}:${address.toLowerCase()}`
 
 export const getRoleId = (
   chainId: ChainId,
   address: `0x${string}`,
   roleKey: `0x${string}`
-) => `${chains[chainId].prefix}:role:${address.toLowerCase()}:${roleKey}`
+) => `role:${chains[chainId].prefix}:${address.toLowerCase()}:${roleKey}`
