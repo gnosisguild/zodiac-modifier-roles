3d6962b91b7733ac7ede6a6bb7bf559252a3f766
Cristóvão <cris@gnosisguild.org>
2025-06-27 12:58:02 +0200

    delete hoistCondition function

--- packages/sdk/src/main/target/condition/index.ts
+++ packages/sdk/src/main/target/condition/index.ts
@@ -1,4 +1,3 @@
 export { conditionAddress, conditionId, conditionHash } from "./conditionId"
 export { normalizeCondition } from "./normalize"
 export { subtractCondition } from "./subtractCondition"
-export { hoistCondition, hoistTopOrs } from "./hoistCondition"
