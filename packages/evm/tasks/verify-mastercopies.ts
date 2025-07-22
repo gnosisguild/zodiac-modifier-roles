4cbe3948a978c1a424ef08319e968b660c97a15d
Cris <cristovaoth@users.noreply.github.com>
2025-07-22 14:50:16 +0200

    adjust verify script

--- packages/evm/tasks/verify-mastercopies.ts
+++ packages/evm/tasks/verify-mastercopies.ts
@@ -5,38 +5,26 @@ const { ETHERSCAN_API_KEY } = process.env;
 
 task(
   "verify:mastercopies",
-  "Verifies all mastercopies from the artifacts file, in the block explorer corresponding to the current network"
+  "Verifies all mastercopies from the artifacts file, in the block explorer corresponding to the current network",
 ).setAction(async (_, hre) => {
-  const apiKey = (hre.config.etherscan.apiKey as any)[hre.network.name] as
-    | string
-    | undefined;
-  if (!apiKey) {
-    throw new Error(
-      "Missing etherscan api key for network " + hre.network.name
-    );
-  }
-
-  const chainId = String((await hre.ethers.provider.getNetwork()).chainId);
+  const chainId = Number((await hre.ethers.provider.getNetwork()).chainId);
 
   for (const artifact of readMastercopies()) {
     const { noop } = await verifyMastercopy({
+      chainId,
       artifact,
-      customChainConfig: hre.config.etherscan.customChains.find(
-        (chain: any) => chain.network === hre.network.name
-      ),
-      apiUrlOrChainId: chainId,
-      apiKey,
+      apiKey: ETHERSCAN_API_KEY as string,
     });
 
     const { contractName, contractVersion, address } = artifact;
 
     if (noop) {
       console.log(
-        `🔄 ${contractName}@${contractVersion}: Already verified at ${address}`
+        `🔄 ${contractName}@${contractVersion}: Already verified at ${address}`,
       );
     } else {
       console.log(
-        `🚀 ${contractName}@${contractVersion}: Successfully verified at ${address}`
+        `🚀 ${contractName}@${contractVersion}: Successfully verified at ${address}`,
       );
     }
   }
