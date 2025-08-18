8da08cf0be34c236e6ae50be1ce7ddc78d3da2f0
Philipp Giese <giese.philipp+git@gmail.com>
2025-08-18 16:02:07 +0200

    adjust spec import

--- packages/sdk/src/swaps/allowCowOrderSigning.test.ts
+++ packages/sdk/src/swaps/allowCowOrderSigning.test.ts
@@ -1,9 +1,6 @@
 import { it, describe, expect } from "vitest"
 import { allowCowOrderSigning } from "./allowCowOrderSigning"
-import {
-  processPermissions,
-  targetIntegrity,
-} from "../../build/esm/sdk/src/main"
+import { processPermissions, targetIntegrity } from "zodiac-roles-sdk"
 
 const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2" as const
 const COW = "0xDEf1CA1fb7FBcDC777520aa7f396b4E015F497aB" as const
