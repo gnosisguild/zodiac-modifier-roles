e3f52267b775ad545eef7c6cc4a0a37eed95ecf6
Jan-Felix <524089+jfschwarz@users.noreply.github.com>
2025-08-28 12:08:46 +0200

    fix: types exports (#378)
    Co-authored-by: Jan-Felix <jfschwarz@users.noreply.github.com>

--- packages/sdk/src/typechain.ts
+++ packages/sdk/src/typechain.ts
@@ -1,1 +1,2 @@
-export * from "@gnosis-guild/zodiac-core-modifier-roles"
+// keep this a relative path (importing from @gnosis-guild/zodiac-core-modifier-roles doesn't work)
+export * from "../../evm/typechain-types"
