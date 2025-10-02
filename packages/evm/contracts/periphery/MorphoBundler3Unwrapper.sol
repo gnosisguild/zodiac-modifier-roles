5172606beacf6f586c8013291274a42a8269f2c0
Cris <cristovaoth@users.noreply.github.com>
2025-10-02 13:41:51 +0200

    include validation on the unwrapping

--- packages/evm/contracts/periphery/MorphoBundler3Unwrapper.sol
+++ packages/evm/contracts/periphery/MorphoBundler3Unwrapper.sol
@@ -21,15 +21,45 @@ contract MorphoBundler3Unwrapper is ITransactionUnwrapper {
 
     function unwrap(
         address,
-        uint256,
+        uint256 value,
         bytes calldata data,
         Operation operation
     ) external pure override returns (UnwrappedTransaction[] memory) {
+        _validate(value, data, operation);
+        return _unwrapEntries(data);
+    }
+
+    function _validate(
+        uint256 value,
+        bytes calldata data,
+        Operation operation
+    ) internal pure {
+        if (value != 0) {
+            revert UnsupportedMode();
+        }
         if (operation != Operation.Call) {
             revert UnsupportedMode();
         }
 
-        return _unwrapEntries(data);
+        // first 4 bytes are the selector for multicall
+        if (bytes4(data) != IBundler3.multicall.selector) {
+            revert MalformedHeader();
+        }
+
+        // the following 32 bytes are the offset to the bytes param (always 0x20)
+        if (bytes32(data[4:]) != bytes32(uint256(0x20))) {
+            revert MalformedHeader();
+        }
+
+        uint256 count = uint256(bytes32(data[36:]));
+        uint256 length = 68;
+        for (uint256 i; i < count; i++) {
+            length += 32 + _entrySize(data, _tail(data, 68, i));
+        }
+
+        if (length != data.length) {
+            revert MalformedBody();
+        }
     }
 
     function _unwrapEntries(
@@ -65,12 +95,30 @@ contract MorphoBundler3Unwrapper is ITransactionUnwrapper {
         result.value = uint256(bytes32(data[location + 64:]));
     }
 
+    function _entrySize(
+        bytes calldata data,
+        uint256 location
+    ) private pure returns (uint256) {
+        uint256 headSize = 5 * 32;
+        uint256 tailSize = 32 +
+            _ceil32(uint256(bytes32(data[_tail(data, location, 1):])));
+        return headSize + tailSize;
+    }
+
     function _tail(
         bytes calldata data,
         uint256 headLocation,
         uint256 slot
     ) private pure returns (uint256 location) {
-        uint offset = uint256(bytes32(data[headLocation + 32 * slot:]));
+        uint256 slotLocation = headLocation + 32 * slot;
+        if (slotLocation + 32 > data.length) {
+            revert MalformedBody();
+        }
+        uint offset = uint256(bytes32(data[slotLocation:]));
         location = headLocation + offset;
+    }
+
+    function _ceil32(uint256 length) private pure returns (uint256) {
+        return ((length + 32 - 1) / 32) * 32;
     }
 }
