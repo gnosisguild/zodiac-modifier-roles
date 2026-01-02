3b4ff84f49383f8f910517a1b002b86e01e515de
Cristóvão <12870300+cristovaoth@users.noreply.github.com>
2026-01-02 08:13:46 +0100

    Fix CI test flakiness (#421)
    * Downgrade hardhat to 2.27.0 and solidity-coverage for ci test stability
    
    * Bump hardhat tooling and ethers dependencies
    
    ---------
    
    Co-authored-by: Cristóvão <cristovaoth@users.noreply.github.com>

--- packages/integration-tests/hardhat.config.ts
+++ packages/integration-tests/hardhat.config.ts
@@ -6,7 +6,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.29",
+        version: "0.8.30",
         settings: {
           evmVersion: "cancun",
           optimizer: {
