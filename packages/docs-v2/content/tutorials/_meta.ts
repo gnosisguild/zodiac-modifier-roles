857ef7cb6c870e2b65b06babd2619c6f0faa4c44
Jan-Felix <jfschwarz@users.noreply.github.com>
2026-01-30 18:49:03 +0100

    clean out v2 docs

--- packages/docs-v2/content/tutorials/_meta.ts
+++
@@ -1,9 +0,0 @@
-import type { MetaRecord } from "nextra";
-
-const meta: MetaRecord = {
-  "lower-threshold-routine-transactions": {
-    title: "Lower Safe Threshold for Routine Transactions",
-  },
-};
-
-export default meta;
