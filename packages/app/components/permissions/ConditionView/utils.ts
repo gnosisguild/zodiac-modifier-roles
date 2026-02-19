71c21cdad2504c5b8226b726ea8daf29ad4a33dc
Jan-Felix <jfschwarz@users.noreply.github.com>
2026-02-19 12:12:18 +0100

    code cleanup

--- packages/app/components/permissions/ConditionView/utils.ts
+++ packages/app/components/permissions/ConditionView/utils.ts
@@ -63,7 +63,7 @@ export const matchesAbi = (
 // }
 
 export const isLogicalOperator = (operator: Operator) =>
-  operator >= Operator.And && operator <= Operator.Nor
+  operator >= Operator.And && operator <= Operator.Or
 
 export const isArrayOperator = (operator: Operator) =>
-  operator >= Operator.ArrayEvery && operator <= Operator.ArraySubset
+  operator == Operator.ArrayEvery
