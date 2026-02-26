8df984ad4f00883926f15880960dcf5e81f8063f
Jan-Felix <jfschwarz@users.noreply.github.com>
2026-02-26 15:46:36 +0100

    add SPDX license identifiers to contracts

--- packages/evm/contracts/common/ImmutableStorage.sol
+++ packages/evm/contracts/common/ImmutableStorage.sol
@@ -1,4 +1,7 @@
-// SPDX-License-Identifier: LGPL-3.0-only
+// SPDX-License-Identifier: BUSL-1.1
+// Copyright (c) 2026 GG DAO LLC
+// Zodiac Roles Modifier v3
+// Converts to LGPL-3.0-or-later on 2030-03-01
 pragma solidity >=0.8.17 <0.9.0;
 
 interface ISingletonFactory {
