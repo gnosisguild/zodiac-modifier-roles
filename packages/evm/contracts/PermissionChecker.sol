1ccb04285d1ac5d5d261b78a8dca60026b62ca34
Cristóvão <cristovaoth@users.noreply.github.com>
2026-06-05 16:47:33 +0200

    Fix Bitmask dynamic length to exclude ABI padding

--- packages/evm/contracts/PermissionChecker.sol
+++ packages/evm/contracts/PermissionChecker.sol
@@ -601,7 +601,9 @@ abstract contract PermissionChecker is Core, Periphery {
         bytes calldata value = AbiDecoder.pluck(
             data,
             payload.location + (isInline ? 0 : 32),
-            payload.size - (isInline ? 0 : 32)
+            isInline
+                ? payload.size
+                : uint256(AbiDecoder.word(data, payload.location))
         );
 
         uint256 shift = uint16(bytes2(compValue));
